a0bca6fb5ecef1530a2831798bfa7a1d50ae3326
Hua <binhua.tein@gmail.com>
2018-03-10 10:31:58 +0800

    Update

--- webpack.plugins.config.js
+++ webpack.plugins.config.js
@@ -1,25 +1,27 @@
-var path = require('path')
-var webpack = require('webpack')
+const path = require('path')
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 // Phaser webpack config
-var phaserModule = path.join(__dirname, '/node_modules/phaser/')
-var phaser = path.join(phaserModule, 'src/phaser.js')
+const phaserModule = path.join(__dirname, '/node_modules/phaser/')
+const phaser = path.join(phaserModule, 'src/phaser.js')
 
 module.exports = {
     entry: {
-        gashapon: ['./plugins/gashapon-plugin.js'],
-        dragcursor: ['./plugins/dragcursor-plugin.js'],
-        dragdrop: ['./plugins/dragdrop-plugin.js'],
-        canvas: ['./plugins/canvas-plugin.js']
+        'gashapon': './plugins/gashapon-plugin.js',
+        'dragcursor': './plugins/dragcursor-plugin.js',
+        'dragdrop': './plugins/dragdrop-plugin.js',
+        'canvas': './plugins/canvas-plugin.js'
     },
     output: {
         pathinfo: true,
         path: path.resolve(__dirname, './plugins/dist'),
         filename: '[name].js',
-        //chunkFilename: '[name].min.js',
-        libraryTarget: 'umd',
-        library: "rexPlugins"
+        libraryTarget: 'umd'
     }, 
+    plugins: [
+        new CleanWebpackPlugin(['./plugins/dist'])
+    ],        
     externals: {
         phaser: 'phaser'
     }
