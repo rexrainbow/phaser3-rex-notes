f8e8fd796158153780d88f002097ebc30fda706f
Hua <binhua.tein@gmail.com>
2018-03-11 01:19:15 +0800

    Update

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,23 +1,24 @@
-var path = require('path')
-var webpack = require('webpack')
-var CleanWebpackPlugin = require('clean-webpack-plugin')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
-var CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path')
+const webpack = require('webpack')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 // Phaser webpack config
-var phaserModule = path.join(__dirname, '/node_modules/phaser/')
-var phaser = path.join(phaserModule, 'src/phaser.js')
+const phaserModule = path.join(__dirname, '/node_modules/phaser/')
+const phaser = path.join(phaserModule, 'src/phaser.js')
 
-var definePlugin = new webpack.DefinePlugin({
+const definePlugin = new webpack.DefinePlugin({
   __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'false')),
   WEBGL_RENDERER: true, // I did this to make webpack work, but I'm not really sure it should always be true
   CANVAS_RENDERER: true // I did this to make webpack work, but I'm not really sure it should always be true
 })
 
-var projectName = process.env.myprojname;
-var projectMain = process.env.main;
-var distFolder = path.resolve(__dirname, 'app/' + projectName);
-var assetsFolder = process.env.assets;
+const projectName = process.env.myprojname;
+const projectMain = process.env.main;
+const distFolder = path.resolve(__dirname, 'app/' + projectName);
+const assetsFolder = process.env.assets;
 
 module.exports = {
   entry: {
@@ -36,13 +37,22 @@ module.exports = {
     definePlugin,
     new CleanWebpackPlugin([distFolder]),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    /*new webpack.optimize.UglifyJsPlugin({
-      drop_console: true,
-      minimize: true,
-      output: {
-        comments: false
-      }
-    }),*/
+    new UglifyJSPlugin({
+      include: /.js$/,
+      parallel: true,
+      sourceMap: false,
+      uglifyOptions: {
+        compress: true,
+        ie8: false,
+        ecma: 5,
+        output: {
+          comments: false
+        },
+        warnings: false
+      },
+      warningsFilter: (src) => false
+    }),
+
     new webpack.optimize.CommonsChunkPlugin({ name: 'vendor' /* chunkName= */, filename: 'js/vendor.bundle.js' /* filename= */ }),
     new HtmlWebpackPlugin({
       filename: distFolder + '/index.html',
