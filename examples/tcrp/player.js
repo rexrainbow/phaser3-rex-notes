b7a0dc02629325b6b1be682bec99a882501a2335
Hua <binhua.tein@gmail.com>
2018-03-27 11:55:41 +0800

    Add time unit, dt mode config

--- examples/tcrp/player.js
+++ examples/tcrp/player.js
@@ -29,7 +29,7 @@ class Demo extends Phaser.Scene {
     create() {
         var myCmds = new CmdKlass(this);
         var commands = [
-            ['// comment'],               // [NaN, ...] -> ignored
+            ['// ??'],               // [NaN, ...] -> ignored
             [0, 'print', 'hello'],        // [dt, fnName, param0, param1, ...]
             [1000, ['print', 'world']],   // [dt, [fnName, param0, param1, ...]]
             [3000, [                      // [dt, [command0, command1, ...]]
@@ -39,7 +39,10 @@ class Demo extends Phaser.Scene {
         ];
         var player = new PlayerPlugin(this);
         player
-            .load(commands, myCmds)
+            .load(commands, myCmds, {
+                // timeUnit: 0,        // 'ms'|0|'s'|'sec'|1
+                // dtMode: 0           // 'abs'|'absolute'|0|'acc'|'accumulation'|1
+            })
             .start()
             .on('complete', function(){
                 console.log(player.now * 0.001);
--- plugins/tcrp/PlayerPlugin.js
+++ plugins/tcrp/PlayerPlugin.js
@@ -28,8 +28,10 @@ class PlayerPlugin extends EE {
         this.clock.resetFromJSON(clockConfig);
         this.commands = GetFastValue(o, 'commands', []); // [[dt, cmds], [dt, cmds], ...]
         this.scope = GetFastValue(o, 'scope', undefined);
+        this.timeUnit = GetFastValue(o, 'timeUnit', 0);
+        this.dtMode = GetFastValue(o, 'dtMode', 0);
         this.index = GetFastValue(o, 'index', 0);
-        this.nextDt = undefined;
+        this.nextDt = GetFastValue(o, 'nextDt', 0);
         return this;
     }
 
@@ -42,7 +44,10 @@ class PlayerPlugin extends EE {
             clock: this.clock.toJSON(),
             commands: this.commands,
             scope: this.scope,
-            index: this.index
+            timeUnit: this.timeUnit,
+            dtMode: this.dtMode,
+            index: this.index,
+            nextDt: this.nextDt
         };
     }
 
@@ -64,17 +69,29 @@ class PlayerPlugin extends EE {
         this.shutdown();
     }
 
-    load(commands, scope) {
-        commands = commands.filter(function(item){
+    load(commands, scope, config) {
+        this.timeUnit = GetFastValue(config, 'timeUnit', this.timeUnit);
+        if (typeof (this.timeUnit) === 'string') {
+            this.timeUnit = TIMEUNITMODE[this.timeUnit];
+        }
+        this.dtMode = GetFastValue(config, 'dtMode', this.dtMode);
+        if (typeof (this.dtMode) === 'string') {
+            this.dtMode = DTMODE[this.dtMode];
+        }
+
+        commands = commands.filter(function (item) {
             var dt = item[0];
             return !isNaN(dt);
         });
-        commands.sort(function (itemA, itemB) {
-            var dtA = itemA[0],
-                dtB = itemB[0];
-            return (dtA > dtB) ? 1 :
-                (dtA < dtB) ? -1 : 0;
-        });
+
+        if (this.dtMode === 0) {
+            commands.sort(function (itemA, itemB) {
+                var dtA = itemA[0],
+                    dtB = itemB[0];
+                return (dtA > dtB) ? 1 :
+                    (dtA < dtB) ? -1 : 0;
+            });
+        }
 
         this.commands = commands;
         this.scope = scope;
@@ -83,7 +100,7 @@ class PlayerPlugin extends EE {
 
     start(startAt) {
         this.index = 0;
-        this.nextDt = undefined;
+        this.nextDt = this.getNextDt(0);
         this.clock.start(startAt);
         return this;
     }
@@ -129,51 +146,72 @@ class PlayerPlugin extends EE {
     }
 
     runNextCommands() {
-        if (!this.clock.isRunning) {
+        if (!this.clock.isRunning ||
+            (this.nextDt > this.clock.now)) {
             return;
         }
-        if ((this.nextDt !== undefined) &&
-            (this.nextDt > this.now)) {
-            return;
+
+        while (1) {
+            // complete
+            if (this.isComplete) {
+                this.clock.stop();
+                this.emit('complete');
+                return;
+            }
+            // complete            
+
+            // run a row
+            var item = this.commands[this.index];
+            var command = item[1];
+            if (typeof (command) === 'string') { // [dt, fnName, param0, param1, ...]
+                CMD.length = item.length - 1;
+                for (var i = 0, len = CMD.length; i < len; i++) {
+                    CMD[i] = item[i + 1];
+                }
+                command = CMD;
+            }
+            runCommands(command, this.scope);
+            this.emit('runcommand', command, this.scope);
+            // run a row
+
+            // next dt
+            this.index++; // point to next item
+            this.nextDt = this.getNextDt(this.nextDt);
+            if (this.nextDt > this.clock.now) {
+                return;
+            }
+            // next dt
         }
-        while (this.runCommand()) {}
     }
 
-    runCommand() {
+    getNextDt(currentDt) {
         if (this.isComplete) {
-            this.clock.stop();
-            this.emit('complete');
-            return false;
+            return currentDt;
+        }
+        var dt = this.commands[this.index][0];
+        if (this.timeUnit === 1) { // sec mode
+            dt = dt * 1000;
         }
 
-        var item = this.commands[this.index];
-        var dt = item[0];
-        if (isNaN(dt)) {
-            // skip current command
-            this.index++; // point to next item
-            return true; // continue
-        }
-        if (typeof (dt) === 'string') {
-            dt = parseFloat(dt);
-        }
-        if (dt > this.clock.now) {
-            this.nextDt = dt;
-            return false; // exit
+        if (this.dtMode === 1) {
+            dt += currentDt;
         }
 
-        var command = item[1];
-        if (typeof (command) === 'string') { // [dt, fnName, param0, param1, ...]
-            CMD.length = item.length - 1;
-            for (var i = 0, len = CMD.length; i < len; i++) {
-                CMD[i] = item[i + 1];
-            }
-            command = CMD;
-        }
-        runCommands(command, this.scope);
-        this.emit('runcommand', command, this.scope);
-        this.index++; // point to next item
-        return true; // continue
+        return dt;
     }
 }
+
+const TIMEUNITMODE = {
+    ms: 0,
+    s: 1,
+    sec: 1
+};
+
+const DTMODE = {
+    abs: 0,
+    absolute: 0,
+    acc: 1,
+    accumulation: 1
+};
 
 export default PlayerPlugin;
