6f59d7f5c159a888f035f2b5c556a5dea7be56d6
Hua <binhua.tein@gmail.com>
2018-04-18 13:05:52 +0800

    Try export canvas plugin

--- webpack.plugins.config.js
+++ webpack.plugins.config.js
@@ -1,28 +1,36 @@
 const path = require('path')
 const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-
-// Phaser webpack config
-const phaserModule = path.join(__dirname, '/node_modules/phaser/')
-const phaser = path.join(phaserModule, 'src/phaser.js')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: {
-        'gashapon': './plugins/gashapon-plugin.js',
-        'dragcursor': './plugins/dragcursor-plugin.js',
-        'dragdrop': './plugins/dragdrop-plugin.js',
-        'canvas': './plugins/canvas-plugin.js'
+        'canvas': './plugins/canvas-plugin.js',
+        'canvas.min': './plugins/canvas-plugin.js',
     },
     output: {
         pathinfo: true,
         path: path.resolve(__dirname, './plugins/dist'),
         filename: '[name].js',
-        libraryTarget: 'umd'
-    }, 
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
     plugins: [
-        new CleanWebpackPlugin(['./plugins/dist'])
-    ],        
-    externals: {
-        phaser: 'phaser'
-    }
+        new CleanWebpackPlugin(['./plugins/dist']),
+        new UglifyJSPlugin({
+            include: /\.min\.js$/,
+            parallel: true,
+            sourceMap: false,
+            uglifyOptions: {
+                compress: true,
+                ie8: false,
+                ecma: 5,
+                output: {
+                    comments: false
+                },
+                warnings: false
+            },
+            warningsFilter: (src) => false
+        })
+    ]
 }
