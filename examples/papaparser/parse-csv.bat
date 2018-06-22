59848cced6ca328f55e27142539dfc56c313f592
Hua <binhua.tein@gmail.com>
2018-06-22 17:40:33 +0800

    Add papaparser example
    papaparser is a csv parser

---
+++ examples/papaparser/parse-csv.bat
@@ -0,0 +1,4 @@
+set myproj=./examples/papaparser/parse-csv.js
+cd ..
+cd ..
+webpack
---
+++ examples/papaparser/parse-csv.js
@@ -0,0 +1,44 @@
+'use strict'
+
+import Papa from 'rexPlugins/utils/papaparser/papaparse.js';
+
+class Demo extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'examples'
+        })
+    }
+
+    preload() {}
+
+    create() {
+        var csvString = `,2,3
+4,5,6
+7,8,9`;
+        var data = Papa.parse(csvString, {
+            dynamicTyping: true
+        });
+        console.log(data);  // a 3x3 array
+
+        csvString = `name,hp,mp
+4,5,6
+7,8,9`;
+        data = Papa.parse(csvString, {
+            dynamicTyping: true,
+            header: true
+        });
+        console.log(data);  // an object array with 2 rows
+    }
+
+    update() {}
+}
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scene: Demo
+};
+
+var game = new Phaser.Game(config);
