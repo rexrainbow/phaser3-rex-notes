72b8ea014ab13efeb8b038aacc299f2201ab4a29
Rex <binhua.tein@gmail.com>
2018-07-13 23:57:15 +0800

    Update plugins with v3.11

--- plugins/gameobjects/canvas/CanvasWebGLRenderer.js
+++ plugins/gameobjects/canvas/CanvasWebGLRenderer.js
@@ -1,6 +1,7 @@
 // copy from Phaser.GameObjects.Text
 
 const GameObject = Phaser.GameObjects.GameObject;
+const Utils = Phaser.Renderer.WebGL.Utils;
 
 var WebGLRenderer = function (renderer, src, interpolationPercentage, camera, parentMatrix) {
     if (GameObject.RENDER_MASK !== src.renderFlags || (src.cameraFilter > 0 && (src.cameraFilter & camera._id))) {
@@ -12,7 +13,29 @@ var WebGLRenderer = function (renderer, src, interpolationPercentage, camera, pa
         src.dirty = false;
     }
 
-    this.pipeline.batchText(this, camera, parentMatrix);
+    var getTint = Utils.getTintAppendFloatAlpha;
+
+    this.pipeline.batchTexture(
+        src,
+        src.canvasTexture,
+        src.canvasTexture.width, src.canvasTexture.height,
+        src.x, src.y,
+        src.canvasTexture.width, src.canvasTexture.height,
+        src.scaleX, src.scaleY,
+        src.rotation,
+        src.flipX, src.flipY,
+        src.scrollFactorX, src.scrollFactorY,
+        src.displayOriginX, src.displayOriginY,
+        0, 0, src.canvasTexture.width, src.canvasTexture.height,
+        getTint(src._tintTL, camera.alpha * src._alphaTL),
+        getTint(src._tintTR, camera.alpha * src._alphaTR),
+        getTint(src._tintBL, camera.alpha * src._alphaBL),
+        getTint(src._tintBR, camera.alpha * src._alphaBR),
+        (src._isTinted && src.tintFill),
+        0, 0,
+        camera,
+        parentMatrix
+    );
 };
 
 export default WebGLRenderer;
--- plugins/utils/text/TextWebGLRenderer.js
+++ plugins/utils/text/TextWebGLRenderer.js
@@ -5,6 +5,7 @@
  */
 
 const GameObject = Phaser.GameObjects.GameObject;
+const Utils = Phaser.Renderer.WebGL.Utils;
 
 /**
  * Renders this Game Object with the WebGL Renderer to the given Camera.
@@ -21,20 +22,39 @@ const GameObject = Phaser.GameObjects.GameObject;
  * @param {Phaser.Cameras.Scene2D.Camera} camera - The Camera that is rendering the Game Object.
  * @param {Phaser.GameObjects.Components.TransformMatrix} parentMatrix - This transform matrix is defined if the game object is nested
  */
-var WebGLRenderer = function (renderer, src, interpolationPercentage, camera, parentMatrix)
-{
-    if (GameObject.RENDER_MASK !== src.renderFlags || (src.cameraFilter > 0 && (src.cameraFilter & camera._id)) || src.text === '')
-    {
+var WebGLRenderer = function (renderer, src, interpolationPercentage, camera, parentMatrix) {
+    if (GameObject.RENDER_MASK !== src.renderFlags || (src.cameraFilter > 0 && (src.cameraFilter & camera._id)) || src.text === '') {
         return;
     }
-    
-    if (src.dirty)
-    {
+
+    if (src.dirty) {
         src.canvasTexture = renderer.canvasToTexture(src.canvas, src.canvasTexture);
         src.dirty = false;
     }
 
-    this.pipeline.batchText(this, camera, parentMatrix);
+    var getTint = Utils.getTintAppendFloatAlpha;
+
+    this.pipeline.batchTexture(
+        src,
+        src.canvasTexture,
+        src.canvasTexture.width, src.canvasTexture.height,
+        src.x, src.y,
+        src.canvasTexture.width, src.canvasTexture.height,
+        src.scaleX, src.scaleY,
+        src.rotation,
+        src.flipX, src.flipY,
+        src.scrollFactorX, src.scrollFactorY,
+        src.displayOriginX, src.displayOriginY,
+        0, 0, src.canvasTexture.width, src.canvasTexture.height,
+        getTint(src._tintTL, camera.alpha * src._alphaTL),
+        getTint(src._tintTR, camera.alpha * src._alphaTR),
+        getTint(src._tintBL, camera.alpha * src._alphaBL),
+        getTint(src._tintBR, camera.alpha * src._alphaBR),
+        (src._isTinted && src.tintFill),
+        0, 0,
+        camera,
+        parentMatrix
+    );
 };
 
 export default WebGLRenderer;
