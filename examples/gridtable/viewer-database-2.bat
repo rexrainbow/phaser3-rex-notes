2cc370c334ff4c7681e4cfb3b5b8acd8c6d4d47d
Hua <binhua.tein@gmail.com>
2018-08-21 13:59:47 +0800

    Add swapping database example

---
+++ examples/gridtable/viewer-database-2.bat
@@ -0,0 +1,5 @@
+@echo off
+set myproj=./examples/gridtable/viewer-database-2.js
+cd ..
+cd ..
+npm run dev
---
+++ examples/gridtable/viewer-database-2.js
@@ -0,0 +1,146 @@
+'use strict'
+
+import Phaser from 'phaser';
+import GridTablePlugin from 'rexPlugins/gridtable-plugin.js';
+import TouchStatePlugin from 'rexPlugins/touchstate-plugin.js';
+import ContainerLitePlugin from 'rexPlugins/containerlite-plugin.js';
+
+const GetValue = Phaser.Utils.Objects.GetValue;
+
+class Demo extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'examples'
+        })
+    }
+
+    preload() {
+        this.load.image('bg', 'assets/images/white-dot.png');
+    }
+
+    create() {
+        var db0 = getDataBase(30);
+        var db1 = getDataBase(40);
+        var db2 = getDataBase(50);
+
+
+        var config = {
+            x: 400,
+            y: 300,
+            width: 250,
+            height: 400,
+            depth: -1,
+
+            cellHeight: 60,
+            cellWidth: 60,
+            cellsCount: 0,
+            columns: 1
+        };
+        var table = newTable(this, config);
+
+        var setDataBase = function (database) {
+            table.database = database;
+            table.setCellsCount(database.length).updateTable(true).setTableOY(0);
+        };
+
+        this.add.text(0, 0, 'Db0')
+            .setInteractive()
+            .on('pointerup', function () {
+                setDataBase(db0);
+            });
+
+        this.add.text(0, 30, 'Db1')
+            .setInteractive()
+            .on('pointerup', function () {
+                setDataBase(db1);
+            });
+
+        this.add.text(0, 60, 'Db2')
+            .setInteractive()
+            .on('pointerup', function () {
+                setDataBase(db2);
+            });
+
+        setDataBase(db0);
+    }
+
+    update() {}
+}
+
+var getDataBase = function (count) {
+    var data = [];
+    var startIdx = Math.floor(Math.random() * 100);
+    for (var i = 0; i < count; i++) {
+        data.push({
+            id: i + startIdx
+        });
+    }
+    return data;
+}
+
+var newTable = function (scene, config) {
+    var table;
+
+    var newCellObject = function (scene, cell) {
+        var database = table.database;
+        var data = database[cell.index];
+        var bg = scene.add.image(30, 30, 'bg')
+            .setName('background')
+            .setDisplaySize(56, 56)
+            .setTint(0x333333);
+        var txt = scene.add.text(5, 5, data.id)
+            .setName('id');
+        cell.setData('id', data.id);
+
+        var container = scene.add.rexContainerLite(0, 0)
+            .add(bg)
+            .add(txt);
+        return container;
+    };
+
+    config.cellVisibleCallback = function (cell) {
+        cell.setContainer(newCellObject(scene, cell));
+        //console.log('Cell ' + cell.index + ' visible');
+    };
+    table = scene.make.rexGridTable(config);
+    table.database = GetValue(config, 'database', []);
+    addDragContentBehavior(table);
+
+    return table;
+}
+
+var addDragContentBehavior = function (table) {
+    table.touchState = table.scene.plugins.get('rexTouchState').add(table)
+        .on('touchmove', function (pointer) {
+            table.addTableOXY(this.dx, this.dy).updateTable();
+        });
+    return table;
+}
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scene: Demo,
+    plugins: {
+        global: [{
+                key: 'rexGridTable',
+                plugin: GridTablePlugin,
+                start: true
+            },
+            {
+                key: 'rexTouchState',
+                plugin: TouchStatePlugin,
+                start: true
+            },
+            {
+                key: 'rexContainerLite',
+                plugin: ContainerLitePlugin,
+                start: true
+            }
+        ]
+    }
+};
+
+var game = new Phaser.Game(config);
