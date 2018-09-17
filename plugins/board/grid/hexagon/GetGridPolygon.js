195c4b28cba96663b8704eecbce465356a4a9308
Hua <binhua.tein@gmail.com>
2018-09-17 13:36:43 +0800

    Return new polygon object if input poly is undefined

--- plugins/board/grid/hexagon/GetGridPolygon.js
+++ plugins/board/grid/hexagon/GetGridPolygon.js
@@ -7,16 +7,15 @@ var GetGridPolygon = function (tileX, tileY, poly) {
     if (tileY === undefined) {
         tileY = 0;
     }
-    if (poly === undefined) {
-        poly = tmpPoly;
-    }    
     var worldX = this.getWorldX(tileX, tileY);
     var worldY = this.getWorldY(tileX, tileY);
     var size = (this.staggeraxis === 0) ? (this.width / 2) : (this.height / 2);
-    poly.setTo(worldX, worldY, size, this.staggeraxis);
+    if (poly === undefined) {
+        poly = new HexagonPolygon(worldX, worldY, size, this.staggeraxis);
+    } else {
+        poly.setTo(worldX, worldY, size, this.staggeraxis);
+    }
     return poly;
 }
-
-var tmpPoly = new HexagonPolygon(0, 0, 0, 0);
 
 export default GetGridPolygon;
--- plugins/board/grid/quad/GetGridPolygon.js
+++ plugins/board/grid/quad/GetGridPolygon.js
@@ -7,18 +7,17 @@ var GetGridPolygon = function (tileX, tileY, poly) {
     if (tileY === undefined) {
         tileY = 0;
     }
-    if (poly === undefined) {
-        poly = tmpPoly;
-    }    
     var worldX = this.getWorldX(tileX, tileY);
     var worldY = this.getWorldY(tileX, tileY);
     var quadType = (this.mode === 0) ? 0 : 1;
-    poly.setTo(0, 0, this.width, this.height, quadType);
+    if (poly === undefined) {
+        poly = new QuadPolygon(0, 0, this.width, this.height, quadType);
+    } else {
+        poly.setTo(0, 0, this.width, this.height, quadType);
+    }
     poly.centerX = worldX;
     poly.centerY = worldY;
     return poly;
 }
-
-var tmpPoly = new QuadPolygon(0, 0, 0, 0, 0);
 
 export default GetGridPolygon;
