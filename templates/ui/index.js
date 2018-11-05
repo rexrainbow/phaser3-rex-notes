2c42873eb29730c05c200300bdde0b133f410db6
Hua <binhua.tein@gmail.com>
2018-11-05 19:38:00 +0800

    Export plugins

--- templates/ui/index.js
+++ templates/ui/index.js
@@ -1,7 +1,20 @@
+import RoundRectangle from './roundrectangle/RoundRectangle.js';
+
+import Sizer from './sizer/Sizer.js';
 import Label from './label/Label.js';
-import List from './list/List.js';
+import Dialog from './dialog/Dialog.js';
+import Slider from './slider/Slider.js';
+import GridTable from './gridtable/GridTable.js';
+import GridSizer from './gridsizer/GridSizer.js';
+import Tabs from './tabs/Tabs.js';
 
 export default {
+    RoundRectangle: RoundRectangle,
+    Sizer: Sizer,
     Label: Label,
-    List: List,
+    Dialog: Dialog,
+    Slider: Slider,
+    GridTable: GridTable,
+    GridSizer: GridSizer,
+    Tabs: Tabs,
 };
