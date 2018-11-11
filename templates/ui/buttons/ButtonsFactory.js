622978de39e7b9537b0fc10a587f674169d4506f
Rex <binhua.tein@gmail.com>
2018-11-12 00:48:52 +0800

    Fix name error

--- templates/ui/buttons/ButtonsFactory.js
+++ templates/ui/buttons/ButtonsFactory.js
@@ -2,7 +2,7 @@ import Buttons from './Buttons.js';
 import ObjectFactory from '../ObjectFactory.js';
 import SetValue from 'rexPlugins/utils/object/SetValue.js';
 
-ObjectFactory.register('dialog', function (config) {
+ObjectFactory.register('buttons', function (config) {
     return new Buttons(this.scene, config);
 });
 
