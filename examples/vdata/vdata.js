0adcd87179e06bd0ca001d38991f539a764eadb3
Hua <binhua.tein@gmail.com>
2018-11-21 17:50:36 +0800

    Add data manager with version rollback feature

---
+++ examples/vdata/vdata.js
@@ -0,0 +1,59 @@
+import DataManager from 'rexPlugins/data/vdata/DataManager.js';
+
+const Random = Phaser.Math.Between;
+
+class Demo extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'examples'
+        })
+    }
+
+    preload() {}
+
+    create() {
+        var data = new DataManager(this, new Phaser.Events.EventEmitter());
+        data
+            .set('a', 10)
+            .set('b', 100)
+            .commit() // version: 1
+            .set('a', 20)
+            .commit() // version: 2
+            .set('a', 30)
+            .set('b', 300)
+            .commit() // version: 3
+            .set('b', 400)
+
+        var rollback = function (version) {
+            console.log('Rollback to version: ' + version);
+            data
+                .rollback(version)
+                .each(function (parent, key, data) {
+                    console.log(key + ':' + data)
+                })
+            console.log('----');
+        }
+
+        rollback(1);
+        rollback(2);
+        rollback(1);
+        rollback(3);
+        rollback(0);
+        rollback(2);
+
+        console.log('Current version: ' + data.version);
+        console.log('Last version: ' + data.lastVersion);
+    }
+
+    update() {}
+}
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scene: Demo
+};
+
+var game = new Phaser.Game(config);
---
+++ plugins/data/vdata/DataManager.js
@@ -0,0 +1,101 @@
+const Base = Phaser.Data.DataManager;
+
+class DataManager extends Base {
+    constructor(parent, eventEmitter) {
+        super(parent, eventEmitter);
+
+        this._version = 0;
+        this.changeList = {};
+        this.repository = [];
+        this.rollbackFlag = false;
+
+        this.events.on('setdata', this.onValueChange, this);
+        this.events.on('changedata', this.onValueChange, this);
+    }
+
+    get version() {
+        return this._version;
+    }
+
+    set version(value) {
+        if (this.repository.length === 0) {
+            return;
+        }
+
+        value = Math.min(value, this.repository.length);
+        if (this._version === value) {
+            return;
+        }
+
+        var changeList, merged = {};
+        if (this._version < value) {
+            // Forward
+            for (var key in this.changeList) {
+                merged[key] = this.changeList[key][0];
+                delete this.changeList[key];
+            }
+            for (var i = this._version; i < value; i++) {
+                changeList = this.repository[i];
+                for (var key in changeList) {
+                    merged[key] = changeList[key][0];
+                }
+            }
+        } else {
+            // Backward
+            for (var key in this.changeList) {
+                merged[key] = this.changeList[key][1];
+                delete this.changeList[key];
+            }
+            for (var i = this._version - 1; i >= value; i--) {
+                changeList = this.repository[i];
+                for (var key in changeList) {
+                    merged[key] = changeList[key][1];
+                }
+            }
+        }
+
+        this._version = value;
+        var value;
+        this.rollbackFlag = true;
+        for (var key in merged) {
+            value = merged[key];
+            if (value === undefined) {
+                this.remove(key);
+            } else {
+                this.setValue(key, value);
+            }
+        }
+        this.rollbackFlag = false;
+    }
+
+    get lastVersion() {
+        return this.repository.length;
+    }
+
+    commit() {
+        this.repository.length = this._version;
+        this.repository.push(this.changeList);
+        this.changeList = {};
+        this._version++;
+        return this;
+    }
+
+    rollback(value) {
+        this.version = value;
+        return this;
+    }
+
+    onValueChange(parent, key, value, previousValue) {
+        if (this.rollbackFlag) {
+            return;
+        }
+
+        if (this.changeList.hasOwnProperty(key)) {
+            this.changeList[key][0] = value;
+        } else {
+            this.changeList[key] = [value, previousValue];
+            // [newData, previousData]
+        }
+    }
+}
+export default DataManager;
