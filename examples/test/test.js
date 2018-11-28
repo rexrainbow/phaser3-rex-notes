39c077cae453739039f532780af016146904ad2e
Hua <binhua.tein@gmail.com>
2018-11-28 17:32:26 +0800

    Add some textures of characters

--- examples/test/test.js
+++ examples/test/test.js
@@ -5,46 +5,42 @@ class Demo extends Phaser.Scene {
         })
     }
 
-    preload() {}
+    preload() {
+        var key, url;
+        for (var i = 0, icnt = names.length; i < icnt; i++) {
+            for (var j = 0, jcnt = states.length; j < jcnt; j++) {
+                key = getKey(names[i], states[j]);
+                url = 'assets/images/characters/' + key + '.png';
+                this.load.image(key, url);
+            }
+        }
+    }
 
     create() {
-        var graphics = this.add.graphics({
-            lineStyle: {
-                width: 1,
-                color: 0xff0000,
-                alpha: 1
-            },
-            fillStyle: {
-                color: 0xff0000,
-                alpha: 1
-            },
-        });
-
-        var print = this.add.text(0, 0, '');
-
-        var zone1 = this.add.zone(50, 350, 100, 100)
-            .setInteractive().on('pointerdown', function (pointer, localX, localY, event) {
-                print.text += 'click zone1\n';
-            });
-        var zone2 = this.add.zone(150, 350, 10, 10)
-            .setInteractive().on('pointerdown', function (pointer, localX, localY, event) {
-                print.text += 'click zone2\n';
-            });
-        var zone3 = this.add.zone(250, 350, 10, 10)
-            .setInteractive().on('pointerdown', function (pointer, localX, localY, event) {
-                print.text += 'click zone3\n';
-            });
-
-        zone2.setSize(100, 100).updateDisplayOrigin();
-        zone3.setSize(100, 100);
-
-        graphics.strokeRectShape(zone1.getBounds());
-        graphics.strokeRectShape(zone2.getBounds());
-        graphics.strokeRectShape(zone3.getBounds());
+        this.add.image(400, 300, getKey('A', 'smile'));
     }
 
     update() {}
 }
+
+var getKey = function (name, state) {
+    return name + '-' + state;
+}
+
+var names = [
+    'A',
+    'B',
+    'C'
+];
+var states = [
+    'anger',
+    'confuse',
+    'dizzy',
+    'happy',
+    'none',
+    'shock',
+    'smile'
+];
 
 var config = {
     type: Phaser.AUTO,
