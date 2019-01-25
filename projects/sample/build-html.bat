a50cb240f8b51733b13e4a7e5c8d95793e39939a
Hua <binhua.tein@gmail.com>
2019-01-25 17:23:21 +0800

    Add html template parameter

--- projects/sample/build-html.bat
+++ projects/sample/build-html.bat
@@ -1,5 +1,6 @@
 @echo off
 set myprojname=export-html-test
+set htmlTemplate=./projects/sample/index.tmpl
 set main=./projects/sample/main.js
 set assets=./projects/sample/assets
 cd ..
--- projects/sample/preview.bat
+++ projects/sample/preview.bat
@@ -1,4 +1,5 @@
 @echo off
+set htmlTemplate=./projects/sample/index.tmpl
 set main=./projects/sample/main.js
 set assets=./projects/sample/assets
 cd ..
