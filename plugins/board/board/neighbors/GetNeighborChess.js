d29842659009eda89052cb277b25b49f3650fa0f
Hua <binhua.tein@gmail.com>
2019-03-06 17:08:04 +0800

    Fix bug and export plugin

--- plugins/board/board/neighbors/GetNeighborChess.js
+++ plugins/board/board/neighbors/GetNeighborChess.js
@@ -1,12 +1,12 @@
 var GetNeighborChess = function (tileXYZ, directions, neighborTileZ, out) {
+    if (tileXYZ === null) {
+        return null;
+    }
     if (tileXYZ.rexChess) { // tileXYZ is a Chess game object
         var chess = tileXYZ;
         tileXYZ = this.chessToTileXYZ(chess);
     }
 
-    if (tileXYZ === null) {
-        return null;
-    }
     if (neighborTileZ == null) {
         neighborTileZ = tileXYZ.z;
     }
