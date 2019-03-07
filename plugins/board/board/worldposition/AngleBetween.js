66fe3c4fa9f7f684851e6b642424cc6bb1a5b37b
Hua <binhua.tein@gmail.com>
2019-03-07 17:40:41 +0800

    Remane variables

--- plugins/board/board/worldposition/AngleBetween.js
+++ plugins/board/board/worldposition/AngleBetween.js
@@ -1,10 +1,10 @@
 import GetAngle from '../../../utils/math/angle/Between.js';
 
-var AngleBetween = function (tileXY0, tileXY1) {
-    var x0 = this.tileXYToWorldX(tileXY0.x, tileXY0.y);
-    var y0 = this.tileXYToWorldY(tileXY0.x, tileXY0.y);
-    var x1 = this.tileXYToWorldX(tileXY1.x, tileXY1.y);
-    var y1 = this.tileXYToWorldY(tileXY1.x, tileXY1.y);
+var AngleBetween = function (tileA, tileB) {
+    var x0 = this.tileXYToWorldX(tileA.x, tileA.y);
+    var y0 = this.tileXYToWorldY(tileA.x, tileA.y);
+    var x1 = this.tileXYToWorldX(tileB.x, tileB.y);
+    var y1 = this.tileXYToWorldY(tileB.x, tileB.y);
     return GetAngle(x0, y0, x1, y1);
 }
 
