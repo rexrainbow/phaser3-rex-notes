3f58e8b086d18c216e5bedcff71ad002ad6f7333
Hua <binhua.tein@gmail.com>
2019-03-07 14:07:44 +0800

    Swap condition

--- plugins/board/board/tileposition/ChessToTileXYZ.js
+++ plugins/board/board/tileposition/ChessToTileXYZ.js
@@ -3,16 +3,16 @@ import IsChess from '../../chess/IsChess';
 
 var ChessToTileXYZ = function (chess) {
     // chess: chess object, UID, or tileXYZ
-    if (IsUID(chess)) { // UID
-        var uid = chess;
-        return this.boardData.getXYZ(uid);
-    } else if (IsChess(chess)) { // chess
+    if (IsChess(chess)) { // chess
         var chessData = chess.rexChess;
         if (chessData.board === this) {
             return chessData.tileXYZ;
         } else {
             return null;
         }
+    } else if (IsUID(chess)) { // UID
+        var uid = chess;
+        return this.boardData.getXYZ(uid);
     } else if (chess != null) { // tileXYZ
         var tileXYZ = chess;
         return tileXYZ;
