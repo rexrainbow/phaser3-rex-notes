beba86127e9a5dea7922ae9540567bd47dd659b1
Hua <binhua.tein@gmail.com>
2019-04-11 11:46:57 +0800

    Don't wrap content again if wrapping parameters are not changed

--- templates/ui/textblock/ResizeText.js
+++ templates/ui/textblock/ResizeText.js
@@ -8,16 +8,21 @@ var ResizeText = function (textObject, width, height) {
 
     var maxLines = Math.ceil(TextHeightToLinesCount.call(this, height)) + 1;
 
+    var isChanged;
     if (this.textObjectType === 0) {
+        isChanged = (style.wordWrapWidth !== wrapWidth) || (style.maxLines !== maxLines);
         style.wordWrapWidth = wrapWidth;
         style.maxLines = maxLines;
     } else {
+        isChanged = (style.wrapWidth !== wrapWidth) || (style.maxLines !== maxLines);
         style.wrapWidth = wrapWidth;
         style.maxLines = maxLines;
     }
 
     // Render content again
-    this.setText();
+    if (isChanged) {
+        this.setText();
+    }
 }
 
 export default ResizeText;
