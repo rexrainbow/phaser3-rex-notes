8de690e2ad7922e82904a3011b78ddfe8906fe6b
Hua <binhua.tein@gmail.com>
2019-05-16 14:33:46 +0800

    Remove pre-installed gestrue objects

--- examples/gesture/one-pointer.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set main=./examples/gesture/one-pointer.js
-cd ..
-cd ..
-webpack --config webpack.config.js
--- plugins/gestures-plugin.js
+++ plugins/gestures-plugin.js
@@ -7,31 +7,12 @@ import SwipeFactory from './input/gestures/swipe/SwipeFactory.js';
 import PinchFactory from './input/gestures/pinch/PinchFactory.js';
 import RotateFactory from './input/gestures/rotate/RotateFactory.js';
 
-import EventEmitterMethods from './utils/eventemitter/EventEmitterMethods.js';
-
 class GesturesPlugin extends Phaser.Plugins.ScenePlugin {
     constructor(scene, pluginManager) {
         super(scene, pluginManager);
 
         this.add = new ObjectFactory(scene);
-        // Event emitter
-        this.setEventEmitter();
-
-        var config = {
-            eventEmitter: this.getEventEmitter()
-        }
-        this.tap = this.add.tap(config);
-        this.press = this.add.press(config);
-        this.pan = this.add.pan(config);
-        this.swipe = this.add.swipe(config);
-        this.pinch = this.add.pinch(config);
-        this.rotate = this.add.rotate(config);
     }
 }
-
-Object.assign(
-    GesturesPlugin.prototype,
-    EventEmitterMethods
-);
 
 export default GesturesPlugin;
