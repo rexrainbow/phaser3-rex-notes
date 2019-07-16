89ad21f4ffd535d113838000c51f27a4b44295b5
Hua <binhua.tein@gmail.com>
2019-07-16 11:02:03 +0800

    Fix bug

--- examples/canvas-data/texture-to-color-map.js
+++ examples/canvas-data/texture-to-color-map.js
@@ -3,6 +3,7 @@ import CanvasDataPlugin from '../../plugins/canvasdata-plugin.js';
 const COLOR_PRIMARY = 0x4e342e;
 const COLOR_LIGHT = 0x7b5e57;
 const COLOR_DARK = 0x260e04;
+const RandomXY = Phaser.Math.RandomXY;
 
 class Demo extends Phaser.Scene {
     constructor() {
@@ -18,16 +19,31 @@ class Demo extends Phaser.Scene {
 
     create() {
         this.add.image(0, 0, 'mushroom').setOrigin(0);
+
+        var startX = 250, startY = 100, width = 6, height = 6,
+            offsetXY = { x: 0, y: 0 };
         this.plugins.get('rexCanvasData').textureTColorMap('mushroom')
-            .forEach(function (value, x, y, colorMap) {
-                this.add.rectangle(
-                    100 + (x * 4),
-                    100 + (y * 4),
-                    4,
-                    4,
+            .forEachNonZero(function (value, x, y, colorMap) {
+                var destinationX = startX + (x * width);
+                var destinationY = startY + (y * height);
+                offsetXY = RandomXY(offsetXY, 300);
+                var gameObject = this.add.rectangle(
+                    (destinationX + offsetXY.x),
+                    (destinationY + offsetXY.y),
+                    width,
+                    height,
                     colorMap.color32ToColorInt(value),
                     (colorMap.color32ToAlpha(value) / 255)
                 )
+                this.tweens.add({
+                    targets: gameObject,
+                    x: destinationX,
+                    y: destinationY,
+                    ease: 'Linear',       // 'Cubic', 'Elastic', 'Bounce', 'Back'
+                    duration: 1000,
+                    repeat: 0,            // -1: infinity
+                    yoyo: false
+                });
             }, this)
     }
 
