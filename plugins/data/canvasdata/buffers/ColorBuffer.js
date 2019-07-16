749a5469fc115f7eb4465692b5fe09fcecf13250
Hua <binhua.tein@gmail.com>
2019-07-16 13:55:46 +0800

    Format code

--- plugins/data/canvasdata/buffers/ColorBuffer.js
+++ plugins/data/canvasdata/buffers/ColorBuffer.js
@@ -30,10 +30,10 @@ class ColorBuffer {
 }
 
 ColorBuffer.FillCallback = function (imgData, imgDataIndex) {
-    return (imgData[imgDataIndex + 3] << 24) | 
-    (imgData[imgDataIndex + 0] << 16) | 
-    (imgData[imgDataIndex + 1] << 8) | 
-    imgData[imgDataIndex + 2];
+    return (imgData[imgDataIndex + 3] << 24) |
+        (imgData[imgDataIndex + 0] << 16) |
+        (imgData[imgDataIndex + 1] << 8) |
+        imgData[imgDataIndex + 2];
 }
 
 export default ColorBuffer;
