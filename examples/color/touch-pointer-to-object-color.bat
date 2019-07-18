f5a16143d7ed948b975bdbf26d3369b39ac9fb52
Hua <binhua.tein@gmail.com>
2019-07-18 10:18:01 +0800

    Move file

--- examples/color/touch-pointer-to-object-color.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set main=./examples/color/touch-pointer-to-object-color.js
-cd ..
-cd ..
-webpack --config webpack.config.js
---
+++ examples/texture/touch-pointer-to-object-color.bat
@@ -0,0 +1,5 @@
+@echo off
+set main=./examples/texture/touch-pointer-to-object-color.js
+cd ..
+cd ..
+webpack --config webpack.config.js
