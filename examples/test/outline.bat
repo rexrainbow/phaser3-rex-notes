683b944df9c87b23f4016c64f1bf04e4ac1540bc
Hua <binhua.tein@gmail.com>
2019-08-06 15:33:03 +0800

    Remove outline plugin

--- examples/test/outline.bat
+++ examples/test/outline.bat
@@ -1,6 +1,6 @@
 @echo off
 set testmode=2
-set main=./examples/outline/outline.js
+set main=./examples/test/outline.js
 cd ..
 cd ..
 webpack --config webpack.config.js
--- plugins/gameobjects/outline/Factory.js
+++
@@ -1,7 +0,0 @@
-import Outline from './Outline.js';
-
-export default function (target, config) {
-    var gameObject = new Outline(target, config);
-    this.scene.add.existing(gameObject);
-    return gameObject;
-};
