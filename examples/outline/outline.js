3a7417771d2b35e5ce5d62dae491471ccac29ec7
Hua <binhua.tein@gmail.com>
2019-08-06 15:11:45 +0800

    More testing

--- examples/outline/outline.js
+++ examples/outline/outline.js
@@ -1,4 +1,31 @@
-import Outline from '../../plugins/gameobjects/outline/Outline.js';
+// import Outline from '../../plugins/gameobjects/outline/Outline.js';
+
+const frag = `
+precision mediump float;
+
+uniform vec2 resolution;
+uniform sampler2D iChannel0;
+
+varying vec2 fragCoord;
+
+void rawImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    vec2 uv = fragCoord.xy / resolution.xy;
+
+    vec4 tex = texture2D(iChannel0, uv);
+
+    if (tex.w > 0.0)
+    {
+        fragColor = tex;
+    }
+
+    fragColor = vec4(tex.x + 0.5, tex.y + 0.5, tex.z + 0.5, tex.w + 0.5);
+}
+
+void main () {
+    rawImage(gl_FragColor, fragCoord.xy);
+}
+`;
 
 class Demo extends Phaser.Scene {
     constructor() {
@@ -13,14 +40,25 @@ class Demo extends Phaser.Scene {
     }
 
     create() {
-        var img = this.add.image(400, 300, 'mushroom');
+        // var img = this.add.image(400, 300, 'mushroom');
         var rt = this.make.renderTexture({
             x: 0, y: 0,
-            width: 1024, height: 1024
-        }).draw(img);
+            width: 1024, height: 1024,
+            // add: false
+        })
+            .setOrigin(0)
+            .fill(0xff0000, 1)
+
+        var baseShader = new Phaser.Display.BaseShader('outline', frag);
+        var shader = this.add.shader(baseShader, rt.x, rt.y, rt.width, rt.height)
+            //.setSampler2DBuffer('iChannel0', rt.glTexture, rt.width, rt.height, 0)
+            //.setOrigin(0)
+
+
+        //.draw(img)
         // It maybe non-power-of-2 and have incompatible texture filtering
-        var outLine = new Outline(rt);
-        //this.add.existing(outLine);
+
+        // img.setPosition(400, 200);
     }
 
     update(time) { }
--- plugins/gameobjects/outline/Outline.js
+++ plugins/gameobjects/outline/Outline.js
@@ -1,8 +1,9 @@
 import frag from './outline-frag.js';
 import GetGLTexture from '../../utils/system/GetGLTexture.js';
-import Sync from './Sync.js';
 
 const Shader = Phaser.GameObjects.Shader;
+const RenderTexture = Phaser.GameObjects.RenderTexture;
+const Image = Phaser.GameObjects.Image;
 const GetValue = Phaser.Utils.Objects.GetValue;
 const IsPlainObject = Phaser.Utils.Objects.IsPlainObject;
 const baseShader = new Phaser.Display.BaseShader('outline', frag);
@@ -12,21 +13,12 @@ class Outline extends Shader {
         if (IsPlainObject(target)) {
             target = GetValue(config, 'target');
         }
-        debugger
-        super(target.scene, baseShader, target.x, target.y, target.width, target.height);
+        var scene = target.scene;
+        super(scene, baseShader, target.x, target.y, target.width, target.height);
 
-        this.setSampler2DBuffer('iChannel0', GetGLTexture(target), target.width, target.height, 0);
+        this
+            .setSampler2DBuffer('iChannel0', GetGLTexture(target), target.width, target.height, 0)
         this.target = target;
-    }
-
-    renderWebGL(renderer, src, interpolationPercentage, camera, parentMatrix) {
-        Sync(this.target, this);
-        super.renderWebGL(renderer, src, interpolationPercentage, camera, parentMatrix);
-    }
-
-    renderCanvas(renderer, src, interpolationPercentage, camera, parentMatrix) {
-        Sync(this.target, this);
-        super.renderCanvas(renderer, src, interpolationPercentage, camera, parentMatrix);
     }
 }
 
