950a74234781d9ed9c832c0816945b5b2ace9b46
Hua <binhua.tein@gmail.com>
2019-08-23 11:27:18 +0800

    Remove perspective-quad plugin

--- examples/perspective-quad/flip.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set main=./examples/perspective-quad/flip.js
-cd ..
-cd ..
-webpack --config webpack.config.js
--- plugins/perspectivequad.js
+++
@@ -1,8 +0,0 @@
-import Factory from './gameobjects/perspectivequad/Factory.js';
-import Creator from './gameobjects/perspectivequad/Creator.js';
-import PerspectiveQuad from './gameobjects/perspectivequad/PerspectiveQuad.js';
-
-Phaser.GameObjects.GameObjectFactory.register('rexPerspectiveQuad', Factory);
-Phaser.GameObjects.GameObjectCreator.register('rexPerspectiveQuad', Creator);
-
-export default PerspectiveQuad;
