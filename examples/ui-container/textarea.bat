cdd75451b26ad83bee069a286ba6bfd8df78dd98
Hua <binhua.tein@gmail.com>
2019-10-01 09:29:43 +0800

    Remove tests

--- examples/ui-container/textarea.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set main=./examples/ui-container/textarea.js
-cd ..
-cd ..
-webpack --config webpack.config.js
--- examples/ui-scrollablepanel/image-panel.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set main=./examples/ui-scrollablepanel/image-panel.js
-cd ..
-cd ..
-webpack --config webpack.config.js
