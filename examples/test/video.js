7fad87a0b076c586ee9f4ff572b6a99bd31e6aca
Hua <binhua.tein@gmail.com>
2019-10-24 14:30:08 +0800

    Update

--- examples/test/video.js
+++ examples/test/video.js
@@ -1,10 +1,59 @@
 import VideoPlugin from '../../plugins/video-plugin.js';
-import LogMaxDelta from '../../plugins/utils/system/LogMaxDelta.js';
 
-class Demo extends Phaser.Scene {
+class Menu extends Phaser.Scene {
     constructor() {
         super({
-            key: 'examples'
+            key: 'menu'
+        })
+    }
+
+    preload() {
+    }
+
+    create() {
+        var textConfig = {
+            backgroundColor: '#40241a'
+        }
+
+        var scene = this;
+        var btn0 = this.add.text(0, 0, 'P3-Video', textConfig)
+            .setInteractive()
+            .on('pointerup', function () {
+                scene.scene.launch('p3video')
+                scene.scene.stop('videocanvas')
+            })
+        var btn1 = this.add.text(120, 0, 'Canvas-Video', textConfig)
+            .setInteractive()
+            .on('pointerup', function () {
+                scene.scene.stop('p3video')
+                scene.scene.launch('videocanvas')
+            })
+
+
+        this.scene.get('p3video').events
+            .on('start', function () {
+                btn0.setBackgroundColor('#6d4c41')
+            })
+            .on('shutdown', function () {
+                btn0.setBackgroundColor('#40241a')
+            })
+        this.scene.get('videocanvas').events
+            .on('start', function () {
+                btn1.setBackgroundColor('#6d4c41')
+            })
+            .on('shutdown', function () {
+                btn1.setBackgroundColor('#40241a')
+            })
+    }
+
+    update(time) {
+    }
+}
+
+class P3Video extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'p3video'
         })
     }
 
@@ -13,26 +62,53 @@ class Demo extends Phaser.Scene {
     }
 
     create() {
-        this.add.video(200, 300, 'test').setDisplaySize(400, 225);
-
-        // var config = {
-        //     width: 400,
-        //     height: 225,
-        //     src: './assets/video/test.mp4',
-        //     autopPlay: true
-        // };
-        // var canvas = this.add.rexVideoCanvas(600, 300, config);
-        // this.input.on('pointerdown', function () {
-        //     if (canvas.isPaused) {
-        //         canvas.play();
-        //     } else {
-        //         canvas.pause();
-        //     }
-        // });
+        console.log('create-p3video-scene')
+        this.add.video(400, 300, 'test')
+            .setDisplaySize(400, 225)
+            .once('destroy', function () {
+                console.log('destroy-p3video-obj')
+            })
     }
 
     update(time) {
-        LogMaxDelta(time);
+    }
+}
+
+
+class VideoCanvas extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'videocanvas'
+        })
+    }
+
+    preload() {
+    }
+
+    create() {
+        console.log('create-videocanvas-scene')
+
+        var canvas = this.add.rexVideoCanvas({
+            x: 400, y: 300,
+            width: 400,
+            height: 225,
+            src: './assets/video/test.mp4',
+            autopPlay: true
+        })
+            .setInteractive()
+            .on('pointerdown', function () {
+                if (canvas.isPaused) {
+                    canvas.play();
+                } else {
+                    canvas.pause();
+                }
+            })
+            .once('destroy', function () {
+                console.log('destroy-videocanvas-obj')
+            })
+    }
+
+    update(time) {
     }
 }
 
@@ -45,7 +121,7 @@ var config = {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
     },
-    scene: Demo,
+    scene: [Menu, P3Video, VideoCanvas],
     plugins: {
         global: [
             {
