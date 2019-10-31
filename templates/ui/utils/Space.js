83f17378e08f0f0972acc74261151f7b196a06bc
Hua <binhua.tein@gmail.com>
2019-10-31 13:32:23 +0800

    Fix layout bug

--- templates/ui/utils/Space.js
+++ templates/ui/utils/Space.js
@@ -1,9 +1,9 @@
 var Space = function (scene, width, height) {
-    if (width === undefined) {
-        width = 0;
+    if ((width === undefined) || (width <= 1)) {
+        width = 1;
     }
-    if (height === undefined) {
-        height = 0;
+    if ((height === undefined) || (height <= 1)) {
+        height = 1;
     }
     return scene.add.zone(0, 0, width, height);
 }
