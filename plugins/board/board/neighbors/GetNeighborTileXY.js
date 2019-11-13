ff85f180dc769656a163661416c08bf28582c49f
Hua <binhua.tein@gmail.com>
2019-11-13 14:05:43 +0800

    Remove duplicated tileXY checking

--- plugins/board/board/neighbors/GetNeighborTileXY.js
+++ plugins/board/board/neighbors/GetNeighborTileXY.js
@@ -1,48 +1,5 @@
 var GetNeighborTileXY = function (srcTileXY, directions, out) {
-    if (typeof (directions) === 'string') {
-        if (directions.indexOf(',') === -1) {
-            directions = parseInt(directions);
-        } else {
-            directions = directions.split(',');
-        }
-    }
-
-    var isNumberDirection = (typeof (directions) === 'number');
-    if (isNumberDirection) {
-        var dir = directions;
-        if (out === undefined) {
-            out = {};
-        } else if (out === true) {
-            out = globTileXY;
-        }
-
-        this.grid.getNeighborTileXY(srcTileXY.x, srcTileXY.y, dir, out);
-        this.getWrapTileXY(out.x, out.y, out);
-        if ((out.x == null) || (out.y == null)) {
-            out = null;
-        }
-
-    } else {
-        var dir, neighborTileXY;
-
-        // directions array
-        if (directions == null) {
-            directions = this.grid.allDirections;
-        }
-        if (out === undefined) {
-            out = [];
-        }
-        for (var i = 0, cnt = directions.length; i < cnt; i++) {
-            dir = directions[i];
-            neighborTileXY = this.getNeighborTileXY(srcTileXY, dir);
-            if (neighborTileXY !== null) {
-                out.push(neighborTileXY);
-            }
-        }
-    }
-
-    return out;
+    return this.getTileXYAtDirection(srcTileXY, directions, 1, out);
 };
 
-var globTileXY = {};
 export default GetNeighborTileXY;
--- plugins/board/board/neighbors/GetTileXYAtDirection.js
+++ plugins/board/board/neighbors/GetTileXYAtDirection.js
@@ -1,6 +1,5 @@
 import IsPlainObject from '../../../utils/object/IsPlainObject.js';
 import GetValue from '../../../utils/object/GetValue.js';
-import IsTileXYInArray from '../../utils/IsTileXYInArray.js';
 
 var GetTileXYAtDirection = function (chess, directions, distance, out) {
     var srcTileXY = this.chessToTileXYZ(chess);
@@ -38,20 +37,20 @@ var GetTileXYAtDirection = function (chess, directions, distance, out) {
                 var step = GetValue(distance, 'step', ((endIdx >= startIdx) ? 1 : -1));
                 if (startIdx === endIdx) {
                     resultTileXY = this.getTileXYAtDirection(srcTileXY, directions, endIdx); // return a single tileXY
-                    if ((resultTileXY !== null) && (!IsTileXYInArray(resultTileXY, out))) {
+                    if (resultTileXY !== null) {
                         out.push(resultTileXY);
                     }
                 } else if (startIdx < endIdx) {
                     for (var i = startIdx; i <= endIdx; i += step) {
                         resultTileXY = this.getTileXYAtDirection(srcTileXY, directions, i); // return a single tileXY
-                        if ((resultTileXY !== null) && (!IsTileXYInArray(resultTileXY, out))) {
+                        if (resultTileXY !== null) {
                             out.push(resultTileXY);
                         }
                     }
                 } else {
                     for (var i = startIdx; i >= endIdx; i += step) {
                         resultTileXY = this.getTileXYAtDirection(srcTileXY, directions, i); // return a single tileXY
-                        if ((resultTileXY !== null) && (IsTileXYInArray(resultTileXY, out))) {
+                        if (resultTileXY !== null) {
                             out.push(resultTileXY);
                         }
                     }
@@ -59,7 +58,7 @@ var GetTileXYAtDirection = function (chess, directions, distance, out) {
             } else { // Is array
                 for (var i = 0, cnt = distance.length; i < cnt; i++) {
                     resultTileXY = this.getTileXYAtDirection(srcTileXY, directions, distance[i]);
-                    if ((resultTileXY !== null) && (!IsTileXYInArray(resultTileXY, out))) {
+                    if (resultTileXY !== null) {
                         out.push(resultTileXY);
                     }
                 }
@@ -68,7 +67,7 @@ var GetTileXYAtDirection = function (chess, directions, distance, out) {
             for (var i = 0, cnt = directions.length; i < cnt; i++) {
                 if (isNumberDistance) { // return a single tileXY
                     resultTileXY = this.getTileXYAtDirection(srcTileXY, directions[i], distance);
-                    if ((resultTileXY !== null) && (!IsTileXYInArray(resultTileXY, out))) {
+                    if (resultTileXY !== null) {
                         out.push(resultTileXY);
                     }
                 } else { // append an array of tileXY
