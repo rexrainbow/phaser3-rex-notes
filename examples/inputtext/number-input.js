13e3d60eaeaf1571500e9dc71bf9de4db3a9c41a
Rex <binhua.tein@gmail.com>
2019-11-27 20:31:28 +0800

    Add number-input test code

---
+++ examples/inputtext/number-input.js
@@ -0,0 +1,61 @@
+import InputTextPlugin from '../../plugins/inputtext-plugin.js'
+
+class Demo extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'examples'
+        })
+    }
+
+    preload() { }
+
+    create() {
+        var printText = this.add.text(400, 200, '', {
+            fontSize: '12px',
+            fixedWidth: 100,
+            fixedHeight: 100,
+        }).setOrigin(0.5);
+        var inputText = this.add.rexInputText(400, 400, 10, 10, {
+            type: 'number',
+            text: '0',
+            fontSize: '12px',
+        })
+            .resize(100, 100)
+            .setOrigin(0.5)
+            .on('textchange', function (inputText) {
+                printText.text = inputText.text;
+            })
+
+        printText.text = inputText.text;
+
+        // this.input.keyboard.addKey('ENTER').on('down', function (event) {
+        //     console.log('enter');
+        // });
+    }
+
+    update() { }
+}
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    dom: {
+        createContainer: true
+    },
+    scene: Demo,
+    plugins: {
+        global: [{
+            key: 'rexInputText',
+            plugin: InputTextPlugin,
+            start: true
+        }]
+    }
+};
+
+var game = new Phaser.Game(config);
