40316e0828e0c54f79edcfa190b4a399ac483cc4
Rex <binhua.tein@gmail.com>
2019-12-25 15:43:22 +0800

    Refactory QuickLogin

--- plugins/parse/quicklogin/QuickLogin.js
+++ plugins/parse/quicklogin/QuickLogin.js
@@ -5,23 +5,21 @@ var QuickLogin = function (userName, password) {
         .then(function () {
             return Parse.User.logIn(userName, password); // Try login
         })
-        .then(
-            function () {  // Login success 
-                return Promise.resolve();
-            },
-            function () {  // Login fail
-                // Try sign-up, then login again
-                var user = new Parse.User();
-                user
-                    .set('username', userName)
-                    .set('password', password);
+        .catch(function () {  // Login fail, try sign-up, then login again
+            return SignUpThenLogin(userName, password);
+        })
+}
 
-                return user.signUp()
-                    .then(function () {  // Sign up success, try login again                        
-                        return Parse.User.logIn(userName, password);
-                    })
-            }
-        )
+var SignUpThenLogin = function (userName, password) {
+    var user = new Parse.User();
+    user
+        .set('username', userName)
+        .set('password', password);
+
+    return user.signUp()
+        .then(function () {  // Sign up success, try login again                        
+            return Parse.User.logIn(userName, password);
+        })
 }
 
 export default QuickLogin;
