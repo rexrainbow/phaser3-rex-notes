b942702e0f54cabe02bd7bfb8f1d6045a94f8a16
Rex <binhua.tein@gmail.com>
2020-01-08 15:38:07 +0800

    Export minify js files using rollup

---
+++ plugin-list.js
@@ -0,0 +1,161 @@
+module.exports = {
+    // game objects
+    'bbcodetextplugin': './plugins/bbcodetext-plugin.js',
+    'tagtextplugin': './plugins/tagtext-plugin.js',
+    'canvasplugin': './plugins/canvas-plugin.js',
+    'circlemaskimageplugin': './plugins/circlemaskimage-plugin.js',
+    'containerliteplugin': './plugins/containerlite-plugin.js',
+    'gridtableplugin': './plugins/gridtable-plugin.js',
+    'roundrectangleplugin': './plugins/roundrectangle-plugin.js',
+    'inputtextplugin': './plugins/inputtext-plugin.js',
+    'videoplugin': './plugins/video-plugin.js',
+    'youtubeplayerplugin': './plugins/youtubeplayer-plugin.js',
+    'persistenceeffectplugin': './plugins/persistenceeffect-plugin.js',
+    'lineplugin': './plugins/line-plugin.js',
+    'ninepatchplugin': './plugins/ninepatch-plugin.js',
+
+    // custom file loader      
+    'webfontloaderplugin': './plugins/webfontloader-plugin.js',
+    'awaitloaderplugin': './plugins/awaitloader-plugin.js',
+    'scripttagloaderplugin': './plugins/scripttagloader-plugin.js',
+
+    // actions                    
+    'gridalignplugin': './plugins/gridalign-plugin.js',
+    'randomplaceplugin': './plugins/randomplace-plugin.js',
+
+    // input
+    'touchstateplugin': './plugins/touchstate-plugin.js',
+    'dragplugin': './plugins/drag-plugin.js',
+    'dragspeedplugin': './plugins/dragspeed-plugin.js',
+    'sliderplugin': './plugins/slider-plugin.js',
+    'scrollerplugin': './plugins/scroller-plugin.js',
+    'buttonplugin': './plugins/button-plugin.js',
+    'touchcursorplugin': './plugins/touchcursor-plugin.js',
+    'virtualjoystickplugin': './plugins/virtualjoystick-plugin.js',
+    'cursoratboundplugin': './plugins/cursoratbound-plugin.js',
+    'mousewheeltoupdownplugin': './plugins/mousewheeltoupdown-plugin.js',
+    'pinchplugin': './plugins/pinch-plugin.js',
+    'dragrotateplugin': './plugins/dragrotate-plugin.js',
+    'gesturesplugin': './plugins/gestures-plugin.js',
+    'touchhelperplugin': './plugins/touchhelper-plugin.js',
+
+    // member of game object
+    'fadeplugin': './plugins/fade-plugin.js',
+    'scaleplugin': './plugins/scale-plugin.js',
+    'flipplugin': './plugins/flip-plugin.js',
+    'pathfollowerplugin': './plugins/pathfollower-plugin.js',
+    'movetoplugin': './plugins/moveto-plugin.js',
+    'rotatetoplugin': './plugins/rotateto-plugin.js',
+    'flashplugin': './plugins/flash-plugin.js',
+    'shakepositionplugin': './plugins/shakeposition-plugin.js',
+    'interceptionplugin': './plugins/interception-plugin.js',
+    'anchorplugin': './plugins/anchor-plugin.js',
+    // member of game object, arcade behavior
+    'eightdirectionplugin': './plugins/eightdirection-plugin.js',
+    'bulletplugin': './plugins/bullet-plugin.js',
+    'shipplugin': './plugins/ship-plugin.js',
+    'buildarcadeobjectplugin': './plugins/buildarcadeobject-plugin.js',
+
+    // member of text
+    'texttypingplugin': './plugins/texttyping-plugin.js',
+    'textpageplugin': './plugins/textpage-plugin.js',
+    'texteditplugin': './plugins/textedit-plugin.js',
+
+    // member of scene
+    // sound
+    'soundfadeplugin': './plugins/soundfade-plugin.js',
+
+    // control
+    'sequenceplugin': './plugins/sequence-plugin.js',
+    'fsmplugin': './plugins/fsm-plugin.js',
+    'tcrpplugin': './plugins/tcrp-plugin.js',
+    'csvscenarioplugin': './plugins/csvscenario-plugin.js',
+    'waiteventsplugin': './plugins/waitevents-plugin.js',
+    'achievementsplugin': './plugins/achievements-plugin.js',
+    'conditionstableplugin': './plugins/conditionstable-plugin.js',
+    'questplugin': './plugins/quest-plugin.js',
+    'loopinticksplugin': './plugins/loopinticks-plugin.js',
+
+    // time
+    'clockplugin': './plugins/clock-plugin.js',
+    'lifetimeplugin': './plugins/lifetime-plugin.js',
+    'awaytimeplugin': './plugins/awaytime-plugin.js',
+
+    // data structure
+    'csvtoarrayplugin': './plugins/csvtoarray-plugin.js',
+    'csvtohashtableplugin': './plugins/csvtohashtable-plugin.js',
+    'restorabledataplugin': './plugins/restorabledata-plugin.js',
+    'buffdataplugin': './plugins/buffdata-plugin.js',
+    'canvasdataplugin': './plugins/canvasdata-plugin.js',
+
+    // math
+    'gashaponplugin': './plugins/gashapon-plugin.js',
+    'perlinplugin': './plugins/perlin-plugin.js',
+    // geom
+    'rhombusplugin': './plugins/rhombus-plugin.js',
+    'hexagonplugin': './plugins/hexagon-plugin.js',
+
+    // string
+    'xorplugin': './plugins/xor-plugin.js',
+    'lzstringplugin': './plugins/lzstring-plugin.js',
+
+    // shader
+    'swirlpipelineplugin': './plugins/swirlpipeline-plugin.js',
+    'pixelationpipelineplugin': './plugins/pixelationpipeline-plugin.js',
+    'grayscalepipelineplugin': './plugins/grayscalepipeline-plugin.js',
+    'inversepipelineplugin': './plugins/inversepipeline-plugin.js',
+    'hsladjustpipelineplugin': './plugins/hsladjustpipeline-plugin.js',
+    'toonifypipelineplugin': './plugins/toonifypipeline-plugin.js',
+
+    // board
+    'boardplugin': './plugins/board-plugin.js',
+
+    // graph
+    'graphplugin': './plugins/graph-plugin.js',
+
+    // parse
+    'parseplugin': './plugins/parse-plugin.js',
+
+    // firebase
+    'firebaseplugin': './plugins/firebase-plugin.js',
+
+    // templates
+    'bejeweled': './templates/bejeweled/Bejeweled.js',
+    'uiplugin': './templates/ui/ui-plugin.js',
+    'dialogquest': './templates/dialog-quest/DialogQuest.js',
+
+    // Plain class
+    // rex ui
+    'bbcodetext': './templates/ui/bbcodetext/BBCodeText.js',
+    'buttons': './templates/ui/buttons/Buttons.js',
+    'chart': './templates/ui/chart/Chart.js',
+    'circlemaskimage': './templates/ui/circlemaskimage/CircleMaskImage.js',
+    'dialog': './templates/ui/dialog/Dialog.js',
+    'fixwidthsizer': './templates/ui/fixwidthsizer/FixWidthSizer.js',
+    'flip': './templates/ui/flip/Flip.js',
+    'gridsizer': './templates/ui/gridsizer/GridSizer.js',
+    'gridtable': './templates/ui/gridtable/GridTable.js',
+    'label': './templates/ui/label/Label.js',
+    'menu': './templates/ui/menu/Menu.js',
+    'ninepatch': './templates/ui/ninepatch/NinePatch.js',
+    'pages': './templates/ui/pages/Pages.js',
+    'pan': './templates/ui/pan/Pan.js',
+    'pinch': './templates/ui/pinch/Pinch.js',
+    'press': './templates/ui/press/Press.js',
+    'rotate': './templates/ui/rotate/Rotate.js',
+    'roundrectangle': './templates/ui/roundrectangle/RoundRectangle.js',
+    'scrollablepanel': './templates/ui/scrollablepanel/ScrollablePanel.js',
+    'sizer': './templates/ui/sizer/Sizer.js',
+    'slider': './templates/ui/slider/Slider.js',
+    'swipe': './templates/ui/swipe/Swipe.js',
+    'tabs': './templates/ui/tabs/Tabs.js',
+    'tagtext': './templates/ui/tagtext/TagText.js',
+    'tap': './templates/ui/tap/Tap.js',
+    'textarea': './templates/ui/textarea/TextArea.js',
+    'textbox': './templates/ui/textbox/TextBox.js',
+    'toast': './templates/ui/toast/Toast.js',
+    'youtubeplayer': './templates/ui/youtubeplayer/YoutubePlayer.js',
+    // others
+    'firebase': './plugins/firebase.js',
+    'parse': './plugins/parse.js'
+}
