a55abd9d62dc7266cde484869bd8922dd3df719f
Rex <binhua.tein@gmail.com>
2020-01-13 16:06:35 +0800

    Use npmcds as default parse api url

--- plugins/parse/utils/preload/Preload.js
+++ plugins/parse/utils/preload/Preload.js
@@ -1,7 +1,7 @@
 import LoadScriptPromise from '../../../utils/loader/LoadScriptPromise.js';
 
-const VERSION = '2.9.1';
-const CDNURL = `https://cdnjs.cloudflare.com/ajax/libs/parse/${VERSION}/parse.min.js`;
+const VERSION = '2.11.0';
+const CDNURL = `https://npmcdn.com/parse@${VERSION}/dist/parse.min.js`;
 
 var Preload = function (url) {
     if (url === undefined) {
