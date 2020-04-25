cb421e8e1309985c4ce8ebda79e2f9be8f9fd513
Rex <binhua.tein@gmail.com>
2020-04-25 11:14:44 +0800

    Set stroke thiinkness to 1 if has stoke color but no stroke thiinkness

--- export-plugins.bat
+++ export-plugins.bat
@@ -1,3 +1,2 @@
 @echo off
-rem webpack --config webpack.plugins.config.js
 npm run build
--- plugins/gameobjects/text/textbase/Text.js
+++ plugins/gameobjects/text/textbase/Text.js
@@ -42,6 +42,10 @@ class Text extends GameObject {
                 delete style.align;
                 style.halign = halign;
             }
+            // Has Stroke color but stroke thinkness, set stroke thinkness to 1
+            if (style.hasOwnProperty('stroke') && !style.hasOwnProperty('strokeThickness')) {
+                style.strokeThickness = 1;
+            }
         }
         this.style = new TextStyle(this, style);
 
