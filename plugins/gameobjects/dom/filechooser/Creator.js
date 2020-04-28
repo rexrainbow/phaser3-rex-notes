ecf602a0fcd5f5126d6d7a1e2d906b3d969838f4
Rex <binhua.tein@gmail.com>
2020-04-28 14:33:50 +0800

    Grab file chooser Game object class back

---
+++ plugins/gameobjects/dom/filechooser/Creator.js
@@ -0,0 +1,15 @@
+import FileChooser from './FileChooser.js';
+
+const GetAdvancedValue = Phaser.Utils.Objects.GetAdvancedValue;
+const BuildGameObject = Phaser.GameObjects.BuildGameObject;
+
+export default function (config, addToScene) {
+    var width = GetAdvancedValue(config, 'width', undefined);
+    var height = GetAdvancedValue(config, 'height', undefined);
+    if (addToScene !== undefined) {
+        config.add = addToScene;
+    }
+    var gameObject = new FileChooser(this.scene, 0, 0, width, height, config);
+    BuildGameObject(this.scene, gameObject, config);
+    return gameObject;
+};
