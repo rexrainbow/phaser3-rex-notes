7b95a07d6527242e5ee5f66be811117ab9fe3488
Rex <binhua.tein@gmail.com>
2020-05-01 18:58:57 +0800

    Test space parameter of buttons

--- templates/ui/gridbuttons/AddChildMethods.js
+++ templates/ui/gridbuttons/AddChildMethods.js
@@ -5,24 +5,7 @@ const GridSizerAdd = GridSizer.prototype.add;
 
 export default {
     addButton(gameObject, columnIndex, rowIndex) {
-        var padding = {};
-        if (columnIndex === 0) {
-            padding.left = this.buttonSpace.left;
-        } else {
-            padding.left = this.buttonSpace.itemX;
-        }
-        if (columnIndex === (this.columnCount - 1)) {
-            padding.right = this.buttonSpace.right;
-        }
-        if (rowIndex === 0) {
-            padding.top = this.buttonSpace.top;
-        } else {
-            padding.top = this.buttonSpace.itemY;
-        }
-        if (rowIndex === (this.rowCount - 1)) {
-            padding.bottom = this.buttonSpace.bottom;
-        }
-        GridSizerAdd.call(this, gameObject, columnIndex, rowIndex, undefined, padding, this.buttonsExpand);
+        GridSizerAdd.call(this, gameObject, columnIndex, rowIndex, undefined, 0, this.buttonsExpand);
         this.buttons.push(gameObject);
         ButtonSetInteractive.call(this, gameObject, this.clickConfig);
         return this;
