0ad8cc1712a6c853688bff6d21da32572f874b57
thecodejack <aadisrikanth@gmail.com>
2020-05-02 13:16:47 +0530

    remove console logs

--- plugins/utils/object/ShallowCopy.js
+++ plugins/utils/object/ShallowCopy.js
@@ -1,10 +1,7 @@
 export default function shallow(source, destination) {
   Object.keys(source).forEach((key) => {
     if (source.hasOwnProperty(key)) {
-      console.log(key);
       destination[key] = source[key];
-    } else {
-      console.log(key);
     }
   });
 }
