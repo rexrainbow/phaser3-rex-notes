4cf6a34c598a8232026d686c160536fec80f5a3d
Rex <binhua.tein@gmail.com>
2020-06-01 12:20:59 +0800

    Add testing of applying inverse filter to video game object

---
+++ examples/shader-inverse/inverse-video.bat
@@ -0,0 +1,5 @@
+@echo off
+set main=./examples/shader-inverse/inverse-video.js
+cd ..
+cd ..
+npm run watch
---
+++ examples/shader-inverse/inverse-video.js
@@ -0,0 +1,54 @@
+import InversePipelinePlugin from '../../plugins/inversepipeline-plugin.js'
+
+class Demo extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'examples'
+        })
+    }
+
+    preload() {
+        this.load.video('test', './assets/video/test.mp4', 'canplaythrough', false, true);
+    }
+
+    create() {
+        var customPipeline = this.plugins.get('rexInversePipeline').add(this, 'Inverse');
+
+        this.add.video(400, 300, 'test')
+            .setOrigin(0.5)
+            .play()
+            .setPipeline('Inverse')
+
+        this.input
+            .on('pointerdown', function () {
+                customPipeline.intensity = 1;
+            })
+            .on('pointerup', function () {
+                customPipeline.intensity = 0;
+            })
+    }
+
+    update() {
+    }
+}
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    scene: Demo,
+    plugins: {
+        global: [{
+            key: 'rexInversePipeline',
+            plugin: InversePipelinePlugin,
+            start: true
+        }]
+    }
+};
+
+var game = new Phaser.Game(config);
