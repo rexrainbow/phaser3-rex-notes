020666f4ce85452d3e321859548d3b0d5563e05c
Rex <binhua.tein@gmail.com>
2020-06-19 15:53:11 +0800

    Remove outline effect

--- examples/shader-outline/outline.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set main=./examples/shader-outline/outline.js
-cd ..
-cd ..
-npm run watch
--- plugins/outlinepipeline.js
+++
@@ -1,2 +0,0 @@
-import OutlinePipeline from './shaders/outline/OutlinePipeline.js';
-export default OutlinePipeline;
