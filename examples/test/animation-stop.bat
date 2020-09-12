e180a8cc5f78df656de6d9a503877fc32696ec79
Rex <binhua.tein@gmail.com>
2020-09-12 15:47:17 +0800

    Test animation stop

---
+++ examples/test/animation-stop.bat
@@ -0,0 +1,6 @@
+@echo off
+set testmode=2
+set main=./examples/test/animation-stop.js
+cd ..
+cd ..
+npm run watch
--- examples/test/camera-shader-with-image.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set testmode=2
-set main=./examples/test/camera-shader-with-image.js
-cd ..
-cd ..
-npm run watch
