fe1d0232b0d44ad3321e2edb8dd15c88e4d7b822
Rex <binhua.tein@gmail.com>
2020-10-17 16:40:35 +0800

    Rollback

--- plugins/behaviors/flip/Flip.js
+++ plugins/behaviors/flip/Flip.js
@@ -1,5 +1,5 @@
 import TweenBase from '../../utils/tween/TweenBase.js';
-import GetFrameUpdatingCallback from '../../utils/texture/GetFrameUpdatingCallback.js';
+import GetFaceUpdatingCallback from './GetFaceUpdatingCallback.js';
 
 const GetValue = Phaser.Utils.Objects.GetValue;
 const GetAdvancedValue = Phaser.Utils.Objects.GetAdvancedValue;
@@ -84,12 +84,12 @@ class Flip extends TweenBase {
     }
 
     setFrontFace(key, frame) {
-        this.frontFaceCallback = GetFrameUpdatingCallback(key, frame, this.gameObject);
+        this.frontFaceCallback = GetFaceUpdatingCallback(key, frame, this.gameObject);
         return this;
     }
 
     setBackFace(key, frame) {
-        this.backFaceCallback = GetFrameUpdatingCallback(key, frame, this.gameObject);
+        this.backFaceCallback = GetFaceUpdatingCallback(key, frame, this.gameObject);
         return this;
     }
 
