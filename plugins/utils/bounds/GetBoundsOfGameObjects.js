2165844e622ff59c312903d60051db9bc416eb51
Rex <binhua.tein@gmail.com>
2020-10-27 00:49:48 +0800

    Add GetBoundsOfGameObjects method

---
+++ plugins/utils/bounds/GetBoundsOfGameObjects.js
@@ -0,0 +1,34 @@
+const Rectangle = Phaser.Geom.Rectangle;
+const Union = Phaser.Geom.Rectangle.Union;
+
+var GetBoundsOfGameObjects = function (gameObjects, out) {
+    if (out === undefined) {
+        out = new Rectangle();
+    } else if (out === true) {
+        out = globBounds;
+    }
+
+    var gameObject;
+    var firstClone = true;
+    for (var i = 0, cnt = gameObjects.length; i < cnt; i++) {
+        gameObject = gameObjects[i];
+        if (!gameObject.getBounds) {
+            continue;
+        }
+
+        GOBounds = gameObjects[i].getBounds(GOBounds);
+
+        if (firstClone) {
+            out.setTo(GOBounds.x, GOBounds.y, GOBounds.width, GOBounds.height);
+            firstClone = false;
+        } else {
+            Union(GOBounds, out, out);
+        }
+    }
+
+    return out;
+}
+
+var GOBounds, globBounds;
+
+export default GetBoundsOfGameObjects;
