025b4d9594adaa7c65b4140948878208033650b3
Rex <binhua.tein@gmail.com>
2020-10-29 17:23:22 +0800

    Add spin ui testing

---
+++ examples/perspective-render-texture/label.bat
@@ -0,0 +1,5 @@
+set testmode=1
+set main=./examples/perspective-render-texture/label.js
+cd ..
+cd ..
+npm run watch
--- examples/test/rendertexture.bat
+++
@@ -1,5 +0,0 @@
-set testmode=1
-set main=./examples/test/rendertexture.js
-cd ..
-cd ..
-npm run watch
