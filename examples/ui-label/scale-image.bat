5ecfb00f851713faa6ed12bd367c067e4f153515
Rex <binhua.tein@gmail.com>
2021-01-20 10:16:27 +0800

    Add scaled image testing

---
+++ examples/ui-label/scale-image.bat
@@ -0,0 +1,5 @@
+@echo off
+set main=./examples/ui-label/scale-image.js
+cd ..
+cd ..
+npm run watch
---
+++ examples/ui-label/scale-image.js
@@ -0,0 +1,62 @@
+import UIPlugin from '../../templates/ui/ui-plugin.js';
+
+const COLOR_PRIMARY = 0x4e342e;
+const COLOR_LIGHT = 0x7b5e57;
+const COLOR_DARK = 0x260e04;
+
+class Demo extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'examples'
+        })
+
+    }
+
+    preload() {
+        this.load.image('mushroom', 'assets/images/mushroom.png');
+    }
+
+    create() {
+        var label = this.rexUI.add.label({
+            background: this.rexUI.add.roundRectangle(0, 0, 2, 2, 20, COLOR_PRIMARY),
+            text: this.add.text(0, 0, 'Label', {
+                fontSize: '24px'
+            }),
+            icon: this.add.image(0, 0, 'mushroom'),
+            action: this.add.image(0, 0, 'mushroom').setDisplaySize(36, 36),
+            space: {
+                left: 20,
+                right: 20,
+                top: 20,
+                bottom: 20,
+                icon: 10,
+                text: 10,
+            }
+        })
+            .layout()
+            .setPosition(400, 300)
+    }
+
+    update() { }
+}
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    scene: Demo,
+    plugins: {
+        scene: [{
+            key: 'rexUI',
+            plugin: UIPlugin,
+            mapping: 'rexUI'
+        }]
+    }
+};
+
+var game = new Phaser.Game(config);
