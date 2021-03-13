a8d2f3a7bf52e3e4e6d9ac3964ba7b84f6f15844
Rex <binhua.tein@gmail.com>
2021-03-13 10:57:11 +0800

    Update test case

--- examples/formula-parser/method.js
+++ examples/formula-parser/method.js
@@ -1,5 +1,31 @@
 import Parser from '../../plugins/math/formulaparser/FormulaParser.js';
 
+// Solution 1:
+var parser = new Parser();
+parser.randomInt = function (a, b) {
+    return Math.floor(Math.random() * (b - a) + a);
+};
+var f = parser.compile("randomInt(a, b)");
+var context = { a: 10, b: 20 };
+for (var i = 0; i < 5; i++) {
+    console.log(i, f(context));
+}
+
+// Solution 2:
+class MyParser extends Parser {
+    randomInt(a, b) {
+        return Math.floor(Math.random() * (b - a) + a);
+    }
+}
+
+var parser = new MyParser();
+var f = parser.compile("randomInt(a, b)");
+var context = { a: 10, b: 20 };
+for (var i = 0; i < 5; i++) {
+    console.log(i, f(context));
+}
+
+// Solution 3:
 var parser = new Parser();
 var f = parser.compile("randomInt(a, b)");
 var context = {
@@ -13,16 +39,3 @@ for (var i = 0; i < 5; i++) {
     console.log(i, f(context));
 }
 
-// Another solution
-class MyParser extends Parser {
-    randomInt(a, b) {
-        return Math.floor(Math.random() * (b - a) + a);
-    }
-}
-
-var parser = new MyParser();
-var f = parser.compile("randomInt(a, b)");
-var context = { a: 10, b: 20 };
-for (var i = 0; i < 5; i++) {
-    console.log(i, f(context));
-}
