4e9ec856b95a38706e879a4420a770f2bdfe1914
Rex <binhua.tein@gmail.com>
2021-04-08 23:00:29 +0800

    Fix drop down list testing

--- examples/ui-menu/drop-down-list.js
+++ examples/ui-menu/drop-down-list.js
@@ -14,64 +14,80 @@ class Demo extends Phaser.Scene {
     preload() { }
 
     create() {
-        var items = [
-            { label: 'AAA' },
-            { label: 'BBB' },
-            { label: 'CCC' },
-            { label: 'DDD' }
-        ];
+        var options = ['A', 'BB', 'CCC', 'DDD'];
 
-        var scene = this,
-            menu = undefined;
-        this.print = this.add.text(0, 0, '');
-        this.input.on('pointerdown', function (pointer) {
-            if (menu === undefined) {
-                menu = CreateList(scene, pointer.x, pointer.y, items, function (button) {
-                    scene.print.text += 'Click ' + button.text + '\n';
-                });
-            } else if (!menu.isInTouching(pointer)) {
-                menu.collapse();
-                menu = undefined;
-                scene.print.text = '';
-            }
-        }, this);
+        var dropDownList = CreateDropDownList(this, 400, 300, options)
+            .layout()
     }
 
     update() { }
 }
 
-var CreateDropDownList = function (scene, x, y, items) {
+var CreateDropDownList = function (scene, x, y, options) {
+    var maxTextSize = GetMaxTextObjectSize(scene, options);
+
     var label = scene.rexUI.add.label({
         x: x, y: y,
-        background: scene.rexUI.add.roundRectangle(0, 0, 2, 2, 0, COLOR_PRIMARY),
-        // icon:
-        text: scene.add.text(0, 0, item.name, {
-            fontSize: '20px'
-        })
-        // action:
-    })
 
+        background: scene.rexUI.add.roundRectangle(0, 0, 2, 2, 0, COLOR_PRIMARY),
+
+        icon: scene.rexUI.add.roundRectangle(0, 0, 20, 20, 10, COLOR_LIGHT),
+
+        text: CreateTextObject(scene, '')
+            .setFixedSize(maxTextSize.width, maxTextSize.height),
+
+        // action:
+
+        space: {
+            left: 10,
+            right: 10,
+            top: 10,
+            bottom: 10,
+            icon: 10
+        }
+    })
+        .setData('value', '');
+
+    label.data.events.on('changedata-value', function (parent, value, previousValue) {
+        label.text = value;
+    })
+    if (options[0]) {
+        label.setData('value', options[0])
+    }
+
+    var menu;
     scene.rexUI.add.click(label)
         .on('click', function () {
-
+            if (!menu) {
+                var menuX = label.getElement('text').getTopLeft().x,
+                    menuY = label.bottom;
+                menu = CreatePopupList(scene, menuX, menuY, options, function (button) {
+                    label.setData('value', button.text);
+                    menu.collapse();
+                    menu = undefined;
+                });
+            } else {
+                menu.collapse();
+                menu = undefined;
+            }
         })
     return label;
 }
 
-var CreateList = function (scene, x, y, items, onClick) {
-    var list = scene.rexUI.add.menu({
+var CreatePopupList = function (scene, x, y, options, onClick) {
+    var items = options.map(function (option) { return { label: option } });
+    var menu = scene.rexUI.add.menu({
         x: x,
         y: y,
         orientation: 'y',
 
         items: items,
-        createButtonCallback: function (item, i, items) {
+        createButtonCallback: function (item, i, options) {
             return scene.rexUI.add.label({
-                background: scene.rexUI.add.roundRectangle(0, 0, 2, 2, 0, COLOR_PRIMARY),
-                text: scene.add.text(0, 0, item.name, {
-                    fontSize: '20px'
-                }),
-                icon: scene.rexUI.add.roundRectangle(0, 0, 0, 0, 10, COLOR_DARK),
+                background: scene.rexUI.add.roundRectangle(0, 0, 2, 2, 0, COLOR_DARK),
+
+                text: CreateTextObject(scene, item.label),
+
                 space: {
                     left: 10,
                     right: 10,
@@ -114,7 +130,27 @@ var CreateList = function (scene, x, y, items, onClick) {
             console.log('scaledown.complete')
         })
 
-    return list;
+    return menu;
+}
+
+var GetMaxTextObjectSize = function (scene, contentArray) {
+    var textObject = CreateTextObject(scene, '');
+    var width = 0, height = 0;
+    for (var i = 0, cnt = contentArray.length; i < cnt; i++) {
+        textObject.text = contentArray[i];
+        width = Math.max(textObject.width, width);
+        height = Math.max(textObject.height, height);
+    }
+    textObject.destroy();
+
+    return { width: width, height: height };
+}
+
+var CreateTextObject = function (scene, text) {
+    var textObject = scene.add.text(0, 0, text, {
+        fontSize: '20px'
+    })
+    return textObject;
 }
 
 var config = {
