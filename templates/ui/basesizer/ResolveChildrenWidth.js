75fb15dd4e3b29e130e8ae68cdfac065e3b40f19
Rex <binhua.tein@gmail.com>
2021-04-14 13:43:08 +0800

    Reverse

--- templates/ui/basesizer/ResolveChildrenWidth.js
+++ templates/ui/basesizer/ResolveChildrenWidth.js
@@ -1,9 +1,8 @@
 var ResolveChildrenWidth = function (width) {
     // Resolve width of sizer children
-    var children = this.getChildrenSizers(),
-        child, childWidth;
-    for (var i = 0, cnt = children.length; i < cnt; i++) {
-        child = children[i];
+    var child, childWidth;
+    for (var i in this.sizerChildren) {
+        child = this.sizerChildren[i];
         if (child && child.isRexSizer && !child.ignoreLayout) {
             childWidth = this.getExpandedChildWidth(child, width);
             childWidth = child.resolveWidth(childWidth);
