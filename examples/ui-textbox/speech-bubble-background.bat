92514a44817f7c39d6162657bfed81ff3f970d18
Rex <binhua.tein@gmail.com>
2021-04-14 23:12:29 +0800

    Add test case

---
+++ examples/ui-textbox/speech-bubble-background.bat
@@ -0,0 +1,5 @@
+@echo off
+set main=./examples/ui-textbox/speech-bubble-background.js
+cd ..
+cd ..
+npm run watch
---
+++ examples/ui-textbox/textbox.bat
@@ -0,0 +1,5 @@
+@echo off
+set main=./examples/ui-textbox/textbox.js
+cd ..
+cd ..
+npm run watch
