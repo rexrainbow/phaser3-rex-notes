6e769f55e7d59777c9e7fe8ae089b48dc2b89478
Rex <binhua.tein@gmail.com>
2021-04-25 09:26:16 +0800

    Move folder

--- plugins/persistenceeffect.js
+++ plugins/persistenceeffect.js
@@ -1,6 +1,6 @@
-import Factory from './gameobjects/persistenceeffect/Factory.js';
-import Creator from './gameobjects/persistenceeffect/Creator.js';
-import PersistenceEffect from './gameobjects/persistenceeffect/PersistenceEffect.js';
+import Factory from './gameobjects/blitter/persistenceeffect/Factory.js';
+import Creator from './gameobjects/blitter/persistenceeffect/Creator.js';
+import PersistenceEffect from './gameobjects/blitter/persistenceeffect/PersistenceEffect.js';
 
 Phaser.GameObjects.GameObjectFactory.register('rexPersistenceEffect', Factory);
 Phaser.GameObjects.GameObjectCreator.register('rexPersistenceEffect', Creator);
