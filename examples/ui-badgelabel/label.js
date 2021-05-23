1982af02e1fea843c485972c39be9788bc0bc0d5
Rex <binhua.tein@gmail.com>
2021-05-24 00:23:13 +0800

    Update test case

--- examples/ui-badgelabel/label.js
+++ examples/ui-badgelabel/label.js
@@ -19,13 +19,18 @@ class Demo extends Phaser.Scene {
         var item = this.rexUI.add.badgeLabel({
             x: 400, y: 300,
             width: 80, height: 80,
-            space: { left: 5, right: 5, top: 5, bottom: 5 },
+            space: { left: -5, right: -5, top: -5, bottom: -5 },
 
             background: this.rexUI.add.roundRectangle(0, 0, 2, 2, 10, COLOR_PRIMARY),
 
             main: this.add.text(0, 0, 'Item', { fontSize: '20px', color: 'white' }),
 
-            'right-bottom': this.add.text(0, 0, '', { color: 'yellow', align: 'right' }),
+            'right-bottom': this.add.text(0, 0, '', {
+                color: 'yellow',
+                align: 'right',
+                backgroundColor: '#260e04',
+                padding: { left: 3, right: 3, top: 3, bottom: 3 }
+            }),
         })
             .layout()
 
