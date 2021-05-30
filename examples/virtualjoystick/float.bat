f4d43c638a12b6ba0a0dd615b9ffbb8dd0507a34
Rex <binhua.tein@gmail.com>
2021-05-30 14:17:26 +0800

    Add test case

---
+++ examples/virtualjoystick/float.bat
@@ -0,0 +1,5 @@
+@echo off
+set main=./examples/virtualjoystick/float.js
+cd ..
+cd ..
+npm run watch
---
+++ examples/virtualjoystick/float.js
@@ -0,0 +1,67 @@
+import VirtualJoyStickPlugin from '../../plugins/virtualjoystick-plugin.js';
+
+class Demo extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'examples'
+        })
+    }
+
+    preload() { }
+
+    create() {
+        this.joyStick = this.plugins.get('rexVirtualJoyStick').add(this, {
+            x: 400,
+            y: 300,
+            radius: 100,
+            // base: this.add.circle(0, 0, 100, 0x888888),
+            // thumb: this.add.circle(0, 0, 50, 0xcccccc),
+            // dir: '8dir',   // 'up&down'|0|'left&right'|1|'4dir'|2|'8dir'|3
+            // forceMin: 16,
+            // enable: true
+        })
+            .on('update', this.dumpJoyStickState, this);
+
+        this.text = this.add.text(0, 0);
+        this.dumpJoyStickState();
+
+        this.input.on('pointerdown', function (pointer) {
+            this.joyStick.setPosition(pointer.x, pointer.y);
+        }, this)
+    }
+
+    dumpJoyStickState() {
+        var cursorKeys = this.joyStick.createCursorKeys();
+        var s = 'Key down: ';
+        for (var name in cursorKeys) {
+            if (cursorKeys[name].isDown) {
+                s += name + ' ';
+            }
+        }
+        s += '\n';
+        s += ('Force: ' + Math.floor(this.joyStick.force * 100) / 100 + '\n');
+        s += ('Angle: ' + Math.floor(this.joyStick.angle * 100) / 100 + '\n');
+        this.text.setText(s);
+    }
+}
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    scene: Demo,
+    plugins: {
+        global: [{
+            key: 'rexVirtualJoyStick',
+            plugin: VirtualJoyStickPlugin,
+            start: true
+        }]
+    }
+};
+
+var game = new Phaser.Game(config);
