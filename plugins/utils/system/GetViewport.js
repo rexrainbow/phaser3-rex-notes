b8f1a6bba18bbb0b17aff9e7b7fdc8dfcc8d2d8c
Rex <binhua.tein@gmail.com>
2021-06-11 12:57:12 +0800

    Import Constant name

--- plugins/utils/system/GetViewport.js
+++ plugins/utils/system/GetViewport.js
@@ -1,4 +1,5 @@
 const Rectangle = Phaser.Geom.Rectangle;
+const Center = Phaser.Scale.Center;
 
 var GetViewport = function (scene, out) {
     if (out === undefined) {
@@ -8,17 +9,20 @@ var GetViewport = function (scene, out) {
     }
 
     var scaleManager = scene.scale;
+    var baseSize = scaleManager.baseSize;
     var bounds = scaleManager.canvasBounds;
     var scale = scaleManager.displayScale;
     var autoCenter = scaleManager.autoCenter;
+
     out.x = (bounds.x >= 0) ? 0 : -(bounds.x * scale.x);
     out.y = (bounds.y >= 0) ? 0 : -(bounds.y * scale.y);
-    out.width = (bounds.width * scale.x) - out.x;
-    out.height = (bounds.height * scale.y) - out.y;
-    if ((autoCenter === 1) || (autoCenter === 2)) {
+
+    out.width = baseSize.width - out.x;
+    out.height = baseSize.height - out.y;
+    if ((autoCenter === Center.CENTER_BOTH) || (autoCenter === Center.CENTER_HORIZONTALLY)) {
         out.width -= out.x;
     }
-    if ((autoCenter === 1) || (autoCenter === 3)) {
+    if ((autoCenter === Center.CENTER_BOTH) || (autoCenter === Center.CENTER_VERTICALLY)) {
         out.height -= out.y;
     }
     return out;
