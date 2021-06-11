aeff20035891e16c36c86214edee387ed7feb3fd
Rex <binhua.tein@gmail.com>
2021-06-12 01:21:13 +0800

    Twist some logic

--- plugins/utils/system/GetViewport.js
+++ plugins/utils/system/GetViewport.js
@@ -13,24 +13,15 @@ var GetViewport = function (scene, out) {
     var bounds = scaleManager.canvasBounds;
     var scale = scaleManager.displayScale;
     var parentSize = scaleManager.parentSize;
-    var autoCenter = scaleManager.autoCenter;
 
     out.x = (bounds.x >= 0) ? 0 : -(bounds.x * scale.x);
     out.y = (bounds.y >= 0) ? 0 : -(bounds.y * scale.y);
 
-    if ((autoCenter === Center.CENTER_BOTH) || (autoCenter === Center.CENTER_HORIZONTALLY)) {
-        out.width = baseSize.width - (out.x * 2);
-    } else {
-        var width = baseSize.width - (bounds.width - parentSize.width) * scale.x;
-        out.width = Math.min(baseSize.width, width);
-    }
+    var width = baseSize.width - (bounds.width - parentSize.width) * scale.x;
+    out.width = Math.min(baseSize.width, width);
+    var height = baseSize.height - (bounds.height - parentSize.height) * scale.y;
+    out.height = Math.min(baseSize.height, height);
 
-    if ((autoCenter === Center.CENTER_BOTH) || (autoCenter === Center.CENTER_VERTICALLY)) {
-        out.height = baseSize.height - (out.y * 2);
-    } else {
-        var height = baseSize.height - (bounds.height - parentSize.height) * scale.y;
-        out.height = Math.min(baseSize.height, height);
-    }
     return out;
 }
 
