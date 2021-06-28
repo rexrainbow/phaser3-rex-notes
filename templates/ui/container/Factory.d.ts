3b1c8618e29ed92416df6bf313135ebe4ba362aa
Rex <binhua.tein@gmail.com>
2021-06-28 13:46:47 +0800

    Add d.ts of ContainerLit in ui folder

---
+++ templates/ui/container/Factory.d.ts
@@ -0,0 +1,10 @@
+import * as Phaser from 'phaser';
+import Container from "./Container";
+
+declare type ContainerFactory = (
+    x?: number, y?: number,
+    width?: number, height?: number,
+    children?: Phaser.GameObjects.GameObject[]
+) => Container;
+
+export default ContainerFactory;
--- templates/ui/ui-plugin.d.ts
+++ templates/ui/ui-plugin.d.ts
@@ -1,4 +1,6 @@
 import * as Phaser from 'phaser';
+
+import ContainerFactory from './container/Factory';
 import DialogFactory from './dialog/Factory';
 import LabelFactory from './label/Factory';
 import RoundRectangleFactory from './roundrectangle/Factory';
@@ -7,6 +9,7 @@ export default class UIFactories {
     constructor(scene: Phaser.Scene);
 
     add: {
+        container: ContainerFactory,
         dialog: DialogFactory,
         label: LabelFactory,
         roundRectangle: RoundRectangleFactory
