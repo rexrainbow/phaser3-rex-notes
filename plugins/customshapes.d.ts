c43479ead9cae89efcd34393b2d24a2cf08ddd7e
Rex <binhua.tein@gmail.com>
2021-07-06 11:05:24 +0800

    Remove CustomShapesGeoms export

--- plugins/customshapes.d.ts
+++ plugins/customshapes.d.ts
@@ -1,5 +1,2 @@
 import CustomShapes from './gameobjects/shape/customshapes/CustomShapes';
 export default CustomShapes;
-
-import * as GeomTypes from './gameobjects/shape/shapes/geoms/index';
-export { GeomTypes };
