185c3a573567ab6fa08fccad1990f289f183ad0a
Rex <binhua.tein@gmail.com>
2021-07-17 01:56:01 +0800

    Don't add Space Zone into Scene's display list

--- templates/ui/utils/Space.js
+++ templates/ui/utils/Space.js
@@ -1,5 +1,6 @@
+const Zone = Phaser.GameObjects.Zone;
 var Space = function (scene) {
-    var gameObject = scene.add.zone(0, 0, 1, 1);
+    var gameObject = new Zone(scene, 0, 0, 1, 1); // Don't add Zone into scene
     gameObject.isRexSpace = true;
     return gameObject;
 }
