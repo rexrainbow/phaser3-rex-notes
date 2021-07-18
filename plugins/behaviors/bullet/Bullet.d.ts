9cf78a7b1daadecfdb984dc1e5e1864291ef0831
Rex <binhua.tein@gmail.com>
2021-07-18 13:06:31 +0800

    Add d.ts of Bullet

---
+++ plugins/behaviors/bullet/Bullet.d.ts
@@ -0,0 +1,24 @@
+import TickTask from '../../utils/behaviorbase/TickTask';
+
+export default Bullet;
+
+declare namespace Bullet {
+
+    interface IConfig {
+        speed?: number,
+        enable?: boolean
+    }
+}
+
+declare class Bullet extends TickTask {
+    constructor(
+        gameObject: Phaser.GameObjects.GameObject,
+        config?: Bullet.IConfig
+    )
+
+    setEnable(enable?: boolean): this;
+    enable: boolean;
+
+    setSpeed(speed: number): this;
+    speed: number;
+}
