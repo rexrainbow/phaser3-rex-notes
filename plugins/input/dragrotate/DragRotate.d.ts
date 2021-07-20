a044a186b98caded22d085c0b3d9d2606188cc62
Rex <binhua.tein@gmail.com>
2021-07-20 11:38:41 +0800

    Add d.ts of DragRotate

---
+++ plugins/input/dragrotate/DragRotate.d.ts
@@ -0,0 +1,42 @@
+export default DragRotate;
+
+declare namespace DragRotate {
+    interface IConfig {
+        x?: number,
+        y?: number,
+        maxRadius?: number,
+        minRadius?: number,
+        enable?: boolean,
+    }
+}
+
+declare class DragRotate {
+    constructor(
+        scene: Phaser.Scene,
+        config?: DragRotate.IConfig
+    );
+
+    setEnable(enable?: boolean): this;
+    toggleEnable(): this;
+    enable: boolean;
+
+    setOrigin(x: number, y: number): this;
+    setOrigin(pointer: { x: number, y: number }): this;
+    x: number;
+    y: number;
+
+    setRadius(maxRadius: number, minRadius?: number): this;
+    maxRadius: number;
+    minRadius: number;
+
+    readonly deltaRotation: number;
+    readonly deltAangle: number;
+    readonly cw: boolean;
+    readonly ccw: boolean;
+
+    dragCancel(): this;
+
+    readonly pointer: Phaser.Input.Pointer;
+    readonly isDrag: boolean;
+    readonly state: number;
+}
