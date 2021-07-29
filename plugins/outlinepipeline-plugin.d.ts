041af69200d80994ef3c61c306674d3bb874aaa9
Rex <binhua.tein@gmail.com>
2021-07-29 17:52:33 +0800

    Add d.ts of RandomPlace

---
+++ plugins/outlinepipeline-plugin.d.ts
@@ -0,0 +1,32 @@
+// import * as Phaser from 'phaser';
+import OutlinePostFxPipeline from './outlinepipeline';
+
+
+export default OutlinePipelinePlugin;
+
+declare namespace OutlinePipelinePlugin {
+
+    interface IConfig {
+        intensity?: number,
+
+        name?: string,
+    }
+
+}
+
+declare class OutlinePipelinePlugin extends Phaser.Plugins.BasePlugin {
+    add(
+        gameObject: Phaser.GameObjects.GameObject,
+        config?: OutlinePipelinePlugin.IConfig
+    ): OutlinePostFxPipeline;
+
+    remove(
+        gameObject: Phaser.GameObjects.GameObject,
+        name?: string
+    ): this;
+
+    get(
+        gameObject: Phaser.GameObjects.GameObject,
+        name?: string
+    ): OutlinePostFxPipeline | OutlinePostFxPipeline[];
+}
