8e730803d5a65a1be59d8e620521358d497b9bd9
Rex <binhua.tein@gmail.com>
2021-08-08 16:20:42 +0800

    Remove 'sizer.layout' event

--- templates/ui/basesizer/utils/LayoutChild.js
+++ templates/ui/basesizer/utils/LayoutChild.js
@@ -3,10 +3,6 @@ import AlignIn from '../../../../plugins/utils/actions/AlignIn.js';
 var LayoutChild = function (child, x, y, width, height, align) {
     AlignIn(child, x, y, width, height, align);
 
-    if (this.sizerEventsEnable) {
-        child.emit('sizer.layout', child, this);
-    }
-
     this.resetChildPositionState(child);
 
     if (this.sizerEventsEnable) {
