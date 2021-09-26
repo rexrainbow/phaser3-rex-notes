c3fe6d80a7c4a3af1a08095a6c5543224383aff0
Rex <binhua.tein@gmail.com>
2021-09-26 16:32:00 +0800

    Add 'compress', 'decompress' parameters

--- plugins/utils/lokijs/SerializeMethods.js
+++ plugins/utils/lokijs/SerializeMethods.js
@@ -1,13 +1,26 @@
 import LZString from '../lzstring/lz-string.min.js';
 
 // Serialize then compress
-var Serialize = function (db) {
-    return LZString.compress(db.serialize());
+var Serialize = function (db, compress) {
+    if (compress === undefined) {
+        compress = true;
+    }
+    var s = db.serialize();
+    if (compress) {
+        s = LZString.compress(s);
+    }
+    return s;
 }
 
 // Decompress then deserialize, load into db
-var Deserialize = function (db, s) {
-    db.loadJSON(LZString.decompress(s));
+var Deserialize = function (db, s, decompress) {
+    if (decompress === undefined) {
+        decompress = true;
+    }
+    if (decompress) {
+        s = LZString.decompress(s);
+    }
+    db.loadJSON(s);
     return db;
 }
 
