46f12360f07284c47e908e885509e6c885023a10
Rex <binhua.tein@gmail.com>
2021-09-29 15:34:08 +0800

    Can pass compress method name

--- plugins/utils/lokijs/SerializeMethods.js
+++ plugins/utils/lokijs/SerializeMethods.js
@@ -2,23 +2,23 @@ import LZString from '../lzstring/lz-string.min.js';
 
 // Serialize then compress
 var Serialize = function (db, compress) {
-    if (compress === undefined) {
-        compress = true;
+    if ((compress === undefined) || (compress === true)) {
+        compress = 'compress';
     }
     var s = db.serialize();
     if (compress) {
-        s = LZString.compress(s);
+        s = LZString[compress](s);
     }
     return s;
 }
 
 // Decompress then deserialize, load into db
 var Deserialize = function (db, s, decompress) {
-    if (decompress === undefined) {
-        decompress = true;
+    if ((decompress === undefined) || (decompress === true)) {
+        decompress = 'decompress';
     }
     if (decompress) {
-        s = LZString.decompress(s);
+        s = LZString[decompress](s);
     }
     db.loadJSON(s);
     return db;
