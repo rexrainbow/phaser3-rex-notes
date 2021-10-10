124114996de58de2d63eb2bed33a04cf6fde9a7d
Rex <binhua.tein@gmail.com>
2021-10-10 20:47:03 +0800

    Add test case

---
+++ examples/drag/drag-disable.bat
@@ -0,0 +1,5 @@
+@echo off
+set main=./examples/drag/drag-disable.js
+cd ..
+cd ..
+npm run watch
---
+++ examples/drag/drag-disable.js
@@ -0,0 +1,44 @@
+import 'phaser';
+import DragPlugin from '../../plugins/drag-plugin.js';
+
+class Demo extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'examples'
+        })
+    }
+
+    preload() {
+        this.load.image('arrow', 'assets/images/arrow.png');
+    }
+
+    create() {
+        var img = this.add.image(300, 300, 'arrow');
+        img.drag = this.plugins.get('rexDrag').add(img);
+
+        img.on('drag', function (pointer, dragX, dragY) {
+            img.drag.enable = false;
+        });
+    }
+}
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    scene: Demo,
+    plugins: {
+        global: [{
+            key: 'rexDrag',
+            plugin: DragPlugin,
+            start: true
+        }]
+    }
+};
+
+var game = new Phaser.Game(config);
