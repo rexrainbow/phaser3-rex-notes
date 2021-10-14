e4a6b138f6701eba918bd4addff7235d960f5b23
Rex <binhua.tein@gmail.com>
2021-10-14 10:46:45 +0800

    Use disableInteractive method

--- plugins/input/toucheventstop/TouchEventStop.js
+++ plugins/input/toucheventstop/TouchEventStop.js
@@ -19,35 +19,31 @@ class TouchEventStop extends ComponentBase {
     boot() {
         this.parent
             .on('pointerdown', function (pointer, localX, localY, event) {
-                if (this.enable) {
-                    event.stopPropagation();
-                }
-            }, this)
+                event.stopPropagation();
+            })
             .on('pointerup', function (pointer, localX, localY, event) {
-                if (this.enable) {
-                    event.stopPropagation();
-                }
-            }, this)
+                event.stopPropagation();
+            })
             .on('pointermove', function (pointer, localX, localY, event) {
-                if (this.enable) {
-                    event.stopPropagation();
-                }
-            }, this)
+                event.stopPropagation();
+            })
             .on('pointerover', function (pointer, localX, localY, event) {
-                if (this.enable) {
-                    event.stopPropagation();
-                }
-            }, this)
+                event.stopPropagation();
+            })
             .on('pointerout', function (pointer, event) {
-                if (this.enable) {
-                    event.stopPropagation();
-                }
-            }, this)
+                event.stopPropagation();
+            })
     }
 
     setEnable(e) {
         if (e === undefined) {
             e = true;
+        }
+
+        if (e) {
+            this.parent.setInteractive();
+        } else {
+            this.parent.disableInteractive();
         }
 
         this.enable = e;
