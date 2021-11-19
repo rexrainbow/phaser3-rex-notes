163784b8f57fe79cd5ee908c3b8ed866507947b6
Rex <binhua.tein@gmail.com>
2021-11-19 21:57:12 +0800

    Update d.ts

--- plugins/gameobjects/mesh/perspective/sprite/Sprite.d.ts
+++ plugins/gameobjects/mesh/perspective/sprite/Sprite.d.ts
@@ -1,16 +1,16 @@
 // import * as Phaser from 'phaser';
-import RenderTexture from '../rendertexture/RenderTexture';
+import Image from '../image/Image';
 
 export default Sprite;
 
 declare namespace Sprite {
 
-    interface IConfig extends RenderTexture.IConfig {
+    interface IConfig extends Image.IConfig {
     }
 
 }
 
-declare class Sprite extends RenderTexture {
+declare class Sprite extends Image {
     constructor(
         scene: Phaser.Scene,
         x?: number | Sprite.IConfig,
@@ -19,4 +19,44 @@ declare class Sprite extends RenderTexture {
         frame?: string | null,
         config?: Sprite.IConfig
     )
+
+    play(
+        key: string | Phaser.Animations.Animation | Phaser.Types.Animations.PlayAnimationConfig,
+        ignoreIfPlaying?: boolean
+    ): this;
+
+    playReverse(
+        key: string | Phaser.Animations.Animation | Phaser.Types.Animations.PlayAnimationConfig,
+        ignoreIfPlaying?: boolean
+    ): this;
+
+    playAfterDelay(
+        key: string | Phaser.Animations.Animation | Phaser.Types.Animations.PlayAnimationConfig,
+        delay: number
+    ): this;
+
+    playAfterRepeat(
+        key: string | Phaser.Animations.Animation | Phaser.Types.Animations.PlayAnimationConfig,
+        repeatCount?: number
+    ): this;
+
+    chain(
+        key: string | Phaser.Animations.Animation | Phaser.Types.Animations.PlayAnimationConfig | string[] | Phaser.Animations.Animation[] | Phaser.Types.Animations.PlayAnimationConfig[]
+    ): this;
+
+    stop(
+    ): this;
+
+    stopAfterDelay(
+        delay: number
+    ): this;
+
+    stopAfterRepeat(
+        repeatCount?: number
+    ): this;
+
+    stopOnFrame(
+        frame: Phaser.Animations.AnimationFrame
+    ): this;
+
 }
