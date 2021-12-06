2459cced6757bf94d792e0c01f865e7d139a5cae
Rex <binhua.tein@gmail.com>
2021-12-06 22:19:55 +0800

    Fix bug

--- examples/loader/binary-to-image.js
+++ examples/loader/binary-to-image.js
@@ -9,17 +9,19 @@ class Demo extends Phaser.Scene {
 
     preload() {
         this.load.binary('card', 'assets/images/card2.png', Uint8Array);
-        this.load.on('filecomplete-binary-card', function (key, type, data) {
+        this.load.once('filecomplete-binary-card', function () {
             var buffer = new Uint8Array(this.cache.binary.get('card'));
             var blob = new Blob([buffer], { type: "image/png" });
-            var url = window.URL.createObjectURL(blob);            
-            window.open(url, '_blank');
+            var url = window.URL.createObjectURL(blob);
             this.load.image('card', url);
+            this.load.once('filecomplete-image-card', function () {
+                window.URL.revokeObjectURL(url);
+            })
         }, this);
     }
 
     create() {
-        this.add.image('card', 400, 300);
+        this.add.image(400, 300, 'card');
     }
 
     update() {
