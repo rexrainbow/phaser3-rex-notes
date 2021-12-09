4e4400dfd05f2f33111a0510c3654b2e6d100a8b
Rex <binhua.tein@gmail.com>
2021-12-09 12:22:38 +0800

    Add test of js-yaml package

---
+++ examples/yaml/test.bat
@@ -0,0 +1,4 @@
+set main=./examples/yaml/test.js
+cd ..
+cd ..
+npm run watch
---
+++ examples/yaml/test.js
@@ -0,0 +1,56 @@
+import yaml from 'js-yaml';
+
+var print = function (s) {
+    try {
+        const doc = yaml.load(s);
+        console.log(doc);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+var s;
+
+s = `
+a : 10
+b : 20
+`
+print(s)
+
+s = `
+- 10
+- 20
+`
+print(s)
+
+s = `
+- a: 10
+- b: 20
+`
+print(s)
+
+s = `
+content: |
+    line 1 end
+    line 2 ... end
+    line 3 ... ... end
+`
+print(s)
+
+s = `
+a:
+    ? key0
+    ? key1
+    ? key2
+`
+print(s)
+
+s = `
+obj0: &obj
+    a: 10
+    b: 20
+obj1:
+    <<: *obj
+    c: 30    
+`
+print(s)
