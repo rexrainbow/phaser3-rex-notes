02ee826f6831dd076648627aaaf6358745db9c48
Rex <binhua.tein@gmail.com>
2022-01-02 18:07:45 +0800

    Add test code

---
+++ examples/expression-parser/scene-data.bat
@@ -0,0 +1,4 @@
+set main=./examples/expression-parser/scene-data.js
+cd ..
+cd ..
+npm run watch
---
+++ examples/expression-parser/scene-data.js
@@ -0,0 +1,50 @@
+import 'phaser';
+import ExpressionParserPlugin from '../../plugins/expressionparser-plugin';
+
+class Demo extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'examples'
+        });
+    }
+
+    preload() { }
+
+    create() {
+        this.data.set('coin', 3);
+        var parser = this.plugins.get('rexExpressionParser').add();
+
+        var expression = "scene.data.get('coin') > 0";
+        var f = parser.compile(expression);
+        
+        var context = {
+            scene: this
+        }        
+        console.log(f(context));
+    }
+
+    update() {
+
+    }
+}
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    scene: Demo,
+    plugins: {
+        global: [{
+            key: 'rexExpressionParser',
+            plugin: ExpressionParserPlugin,
+            start: true
+        }]
+    }
+};
+
+var game = new Phaser.Game(config);
