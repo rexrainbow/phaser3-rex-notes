33c19827a12f75dd86c8d783ea51cc0e434f5a55
Rex <binhua.tein@gmail.com>
2022-01-11 14:01:00 +0800

    Remove unused code

--- templates/ui/utils/GetScrollMode.js
+++ templates/ui/utils/GetScrollMode.js
@@ -3,9 +3,6 @@ import SCROLLMODE from './ScrollModeConst.js';
 const GetValue = Phaser.Utils.Objects.GetValue;
 
 var GetScrollMode = function (config, key) {
-    if (key === undefined) {
-        key = 'scrollMode';
-    }
     var scrollMode = GetValue(config, 'scrollMode', 0); // Vertical
     if (typeof (scrollMode) === 'string') {
         scrollMode = SCROLLMODE[scrollMode];
