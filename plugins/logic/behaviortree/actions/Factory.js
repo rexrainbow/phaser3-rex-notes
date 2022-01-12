2aef4c39f33c4a50c1f8ce9fcfc1612bf1be3275
Rex <binhua.tein@gmail.com>
2022-01-12 11:12:58 +0800

    Remove Error,Failer,Runner,Succeeder Actions

--- plugins/logic/behaviortree/actions/Factory.js
+++ plugins/logic/behaviortree/actions/Factory.js
@@ -1,38 +1,14 @@
-import Error from './Error.js';
-import Failer from './Failer.js';
-import Runner from './Runner.js';
-import Succeeder from './Succeeder.js';
 import Wait from './Wait.js';
 import ObjectFactory from '../ObjectFactory.js';
 import SetValue from '../../../utils/object/SetValue.js';
 
 
-ObjectFactory.register('error', function (config) {
-    return new Error(config);
-});
-ObjectFactory.register('failer', function (config) {
-    return new Failer(config);
-});
-ObjectFactory.register('runner', function (config) {
-    return new Runner(config);
-});
-ObjectFactory.register('succeeder', function (config) {
-    return new Succeeder(config);
-});
 ObjectFactory.register('wait', function (config) {
     return new Wait(config);
 });
 
-SetValue(window, 'RexPlugins.BehaviorTree.Errror', Error);
-SetValue(window, 'RexPlugins.BehaviorTree.Failer', Failer);
-SetValue(window, 'RexPlugins.BehaviorTree.Failer', Runner);
-SetValue(window, 'RexPlugins.BehaviorTree.Failer', Succeeder);
 SetValue(window, 'RexPlugins.BehaviorTree.Wait', Wait);
 
 export {
-    Error,
-    Failer,
-    Runner,
-    Succeeder,
     Wait
 };
--- plugins/logic/behaviortree/actions/index.js
+++ plugins/logic/behaviortree/actions/index.js
@@ -1,13 +1,5 @@
-import Error from './Error.js';
-import Failer from './Failer.js';
-import Runner from './Runner.js';
-import Succeeder from './Succeeder.js';
 import Wait from './Wait.js';
 
 export {
-    Error,
-    Failer,
-    Runner,
-    Succeeder,
     Wait
 }
