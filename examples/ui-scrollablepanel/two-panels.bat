1d7c26c5341eab253bdbaba16141ccf85332d10e
Rex <binhua.tein@gmail.com>
2022-02-02 22:41:39 +0800

    Add two panels test

---
+++ examples/ui-scrollablepanel/two-panels.bat
@@ -0,0 +1,5 @@
+@echo off
+set main=./examples/ui-scrollablepanel/two-panels.js
+cd ..
+cd ..
+npm run watch
---
+++ examples/ui-scrollablepanel/two-panels.js
@@ -0,0 +1,233 @@
+import 'phaser';
+import UIPlugin from '../../templates/ui/ui-plugin.js';
+
+const COLOR_PRIMARY = 0x4e342e;
+const COLOR_LIGHT = 0x7b5e57;
+const COLOR_DARK = 0x260e04;
+
+class Demo extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'examples'
+        })
+    }
+
+    preload() { }
+
+    create() {
+        var data = {
+            name: 'Rex',
+            skills: [
+                { name: 'A' },
+                { name: 'B' },
+                { name: 'C' },
+                { name: 'D' },
+                { name: 'E' },
+            ],
+            items: [
+                { name: 'A' },
+                { name: 'B' },
+                { name: 'C' },
+                { name: 'D' },
+                { name: 'E' },
+                { name: 'F' },
+                { name: 'G' },
+                { name: 'H' },
+                { name: 'I' },
+                { name: 'J' },
+                { name: 'K' },
+                { name: 'L' },
+                { name: 'M' },
+            ],
+
+        };
+
+        var scrollablePanel0 = createScrollablePanel(this, data)
+            .setPosition(400, 150)
+            .layout()
+
+        var scrollablePanel1 = createScrollablePanel(this, data)
+            .setPosition(400, 450)
+            .layout()
+
+    }
+
+    update() { }
+}
+
+var createScrollablePanel = function (scene, data) {
+    var scrollablePanel = scene.rexUI.add.scrollablePanel({
+        width: 400,
+        // height: 220,
+
+        scrollMode: 1,
+
+        background: scene.rexUI.add.roundRectangle(0, 0, 2, 2, 10, COLOR_PRIMARY),
+
+        panel: {
+            child: createPanel(scene, data),
+
+            mask: {
+                padding: 1,
+                // layer: this.add.layer()
+            },
+        },
+
+        slider: {
+            track: scene.rexUI.add.roundRectangle(0, 0, 20, 10, 10, COLOR_DARK),
+            thumb: scene.rexUI.add.roundRectangle(0, 0, 0, 0, 13, COLOR_LIGHT),
+        },
+
+        mouseWheelScroller: {
+            focus: true,
+            speed: 0.1
+        },
+
+        space: {
+            left: 10,
+            right: 10,
+            top: 10,
+            bottom: 10,
+
+            panel: 10,
+        }
+    })
+
+    return scrollablePanel;
+}
+
+var createPanel = function (scene, data) {
+    var sizer = scene.rexUI.add.sizer({
+        orientation: 'x',
+        space: { item: 10 }
+    })
+        .add(
+            createHeader(scene, data), // child
+            { expand: true }
+        )
+        .add(
+            createTable(scene, data, 'skills', 1), // child
+            { expand: true }
+        )
+        .add(
+            createTable(scene, data, 'items', 2), // child
+            { expand: true }
+        )
+    return sizer;
+}
+
+var createHeader = function (scene, data) {
+    var title = scene.rexUI.add.label({
+        orientation: 'x',
+        text: scene.add.text(0, 0, 'Character'),
+    });
+    var header = scene.rexUI.add.label({
+        orientation: 'y',
+        icon: scene.rexUI.add.roundRectangle(0, 0, 100, 100, 5, COLOR_LIGHT),
+        text: scene.add.text(0, 0, data.name),
+
+        space: { icon: 10 }
+    });
+
+    return scene.rexUI.add.sizer({
+        orientation: 'y',
+        space: { left: 5, right: 5, top: 5, bottom: 5, item: 10 }
+    })
+        .addBackground(
+            scene.rexUI.add.roundRectangle(0, 0, 0, 0, 0, undefined).setStrokeStyle(2, COLOR_LIGHT, 1)
+        )
+        .add(
+            title, // child
+            { expand: true, align: 'left' }
+        )
+        .add(header, // child
+            { proportion: 1, expand: true }
+        );
+};
+
+var createTable = function (scene, data, key, rows) {
+    var capKey = key.charAt(0).toUpperCase() + key.slice(1);
+    var title = scene.rexUI.add.label({
+        orientation: 'x',
+        text: scene.add.text(0, 0, capKey),
+    });
+
+    var items = data[key];
+    var columns = Math.ceil(items.length / rows);
+    var table = scene.rexUI.add.gridSizer({
+        column: columns,
+        row: rows,
+
+        rowProportions: 1,
+        space: { column: 10, row: 10 },
+        name: key  // Search this name to get table back
+    });
+
+    var item;
+    var iconSize = (rows === 1) ? 80 : 40;
+    for (var i = 0, cnt = items.length; i < cnt; i++) {
+        item = items[i];
+        table.add(
+            createIcon(scene, item, iconSize, iconSize),
+            undefined,
+            true,
+            'top',
+            0,
+            true
+        );
+    }
+
+    return scene.rexUI.add.sizer({
+        orientation: 'y',
+        space: { left: 10, right: 10, top: 10, bottom: 10, item: 10 }
+    })
+        .addBackground(
+            scene.rexUI.add.roundRectangle(0, 0, 0, 0, 0, undefined).setStrokeStyle(2, COLOR_LIGHT, 1)
+        )
+        .add(
+            title, // child
+            0, // proportion
+            'left', // align
+            0, // paddingConfig
+            true // expand
+        )
+        .add(table, // child
+            1, // proportion
+            'center', // align
+            0, // paddingConfig
+            true // expand
+        );
+}
+
+var createIcon = function (scene, item, iconWidth, iconHeight) {
+    var label = scene.rexUI.add.label({
+        orientation: 'y',
+        icon: scene.rexUI.add.roundRectangle(0, 0, iconWidth, iconHeight, 5, COLOR_LIGHT),
+        text: scene.add.text(0, 0, item.name),
+
+        space: { icon: 3 }
+    });
+
+    return label;
+};
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    scene: Demo,
+    plugins: {
+        scene: [{
+            key: 'rexUI',
+            plugin: UIPlugin,
+            mapping: 'rexUI'
+        }]
+    }
+};
+
+var game = new Phaser.Game(config);
