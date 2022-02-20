75a91e0653f2bb68085b217bda07c94d42e38205
Rex <binhua.tein@gmail.com>
2022-02-20 22:22:46 +0800

    Fix bug

--- templates/ui/fixwidthsizer/GetNearestChildIndex.js
+++ templates/ui/fixwidthsizer/GetNearestChildIndex.js
@@ -4,27 +4,35 @@ var GetNearestChildIndex = function (x, y) {
     var children = this.sizerChildren;
     var nearestIndex = -1,
         minDistance = Infinity;
-    var child, distance;
     for (var i = 0, cnt = children.length; i < cnt; i++) {
-        child = children[i];
+        var child = children[i];
         // position is not at this line
         if (Math.abs(child.centerY - y) > (child.height / 2)) {
             continue;
         }
 
-        distance = DistanceBetween(child.left, child.centerY, x, y);
+        var distance = DistanceBetween(child.left, child.centerY, x, y);
         if (minDistance > distance) {
             minDistance = distance;
             nearestIndex = i;
         }
 
+        // Is last child
+        if (i === cnt - 1) {
+            var distance = DistanceBetween(child.right, child.centerY, x, y);
+            if (minDistance > distance) {
+                minDistance = distance;
+                nearestIndex = i + 1;
+            }
+        }
+
+        // nextChild is at next line
         var nextChild = children[i + 1];
         if (!nextChild || (nextChild.y === child.y)) {
             continue;
         }
 
-        // nextChild is at next line
-        distance = DistanceBetween(child.right, child.centerY, x, y);
+        var distance = DistanceBetween(child.right, child.centerY, x, y);
         if (minDistance > distance) {
             minDistance = distance;
             nearestIndex = i + 1;
