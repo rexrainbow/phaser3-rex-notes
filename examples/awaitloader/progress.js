2d7dcb77348e52b9a4ac116513526d287b046ea7
Rex <binhua.tein@gmail.com>
2022-03-04 20:57:05 +0800

    Use variable directly

--- examples/awaitloader/progress.js
+++ examples/awaitloader/progress.js
@@ -12,19 +12,20 @@ class Demo extends Phaser.Scene {
     preload() {
         this.print = this.add.text(0, 0, 'Preload\n');
 
+        var scene = this;
         this.load.rexAwait(function (successCallback, failureCallback) {
-            this.load.on('progress', function (progress) {
-                var loader = this.load;
+            scene.load.on('progress', function (progress) {
+                var loader = scene.load;
                 var total = loader.totalToLoad - 1;
                 var remainder = loader.list.size + loader.inflight.size - 1;
                 var myProgress = 1 - (remainder / total);
-                this.print.text += `${Math.floor(progress * 100)}% vs ${Math.floor(myProgress * 100)}%\n`;
+                scene.print.text += `${Math.floor(progress * 100)}% vs ${Math.floor(myProgress * 100)}%\n`;
                 if (myProgress === 1) {
-                    this.print.text += 'Complete\n';
+                    scene.print.text += 'Complete\n';
                     successCallback();
                 }
-            }, this);
-        }, this);
+            });
+        });
 
         this.load.image('classroom', 'assets/images/backgrounds/classroom.png');
         this.load.image('road', 'assets/images/backgrounds/road.png');
