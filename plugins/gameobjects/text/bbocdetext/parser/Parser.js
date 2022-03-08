66698eb38e6024f2a8392ea02cb5133a3000aa0a
Rex <binhua.tein@gmail.com>
2022-03-08 15:25:37 +0800

    Fix bug

--- plugins/gameobjects/text/bbocdetext/parser/Parser.js
+++ plugins/gameobjects/text/bbocdetext/parser/Parser.js
@@ -1,9 +1,14 @@
 import SplitText from './SplitText.js';
 import TagTextToProp from './TagTextToProp.js';
 import PropToContextStyle from './PropToContextStyle.js'
-import PropToTagText from './PropToContextStyle.js';
+import PropToTagText from './PropToTagText.js';
+
+const GetValue = Phaser.Utils.Objects.GetValue;
 
 class Parser {
+    constructor(config) {
+        this.escapeEnable = GetValue(config, 'escape', false);
+    }
 
     getStrokeThinkness(defaultStyle, prop) {
         var strokeThickness;
