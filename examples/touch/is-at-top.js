c8a357423b13abd4b485b05dd51f935b88b69b01
Rex <binhua.tein@gmail.com>
2022-03-14 01:06:56 +0800

    Add test

--- examples/touch/is-at-top.js
+++ examples/touch/is-at-top.js
@@ -1,5 +1,5 @@
 import phaser from 'phaser/src/phaser.js';
-import ButtonPlugin from '../../plugins/button-plugin.js';
+import IsAtTop from '../../plugins/utils/input/IsAtTop.js';
 
 class Demo extends Phaser.Scene {
     constructor() {
@@ -12,20 +12,21 @@ class Demo extends Phaser.Scene {
 
     create() {
         this.print = this.add.text(0, 0, '');
+        this.input.topOnly = false;
 
-        createBtn(this, {
+        CreateBtn(this, {
             x: 400,
             y: 300,
             color: 0x00cccc,
             name: 'btn0'
         });
-        createBtn(this, {
+        CreateBtn(this, {
             x: 390,
             y: 360,
             color: 0xcc00cc,
             name: 'btn1'
         });
-        createBtn(this, {
+        CreateBtn(this, {
             x: 480,
             y: 280,
             color: 0xcccc00,
@@ -35,7 +36,7 @@ class Demo extends Phaser.Scene {
 }
 
 const GetValue = Phaser.Utils.Objects.GetValue;
-var createBtn = function (scene, config) {
+var CreateBtn = function (scene, config) {
     var x = GetValue(config, 'x', 0);
     var y = GetValue(config, 'y', 0);
     var color = GetValue(config, 'color', 0xffffff);
@@ -47,12 +48,16 @@ var createBtn = function (scene, config) {
         fontSize: '20pt'
     })
         .setOrigin(0.5, 0.5)
-    btn.button = scene.plugins.get('rexButton').add(btn, {
-        // clickInterval: 1000  // ms
-    });
-    btn.button.on('click', function (button, gameObject) {
-        scene.print.text += `click ${gameObject.name}\n`;
-    });
+
+    btn
+        .setInteractive()
+        .on('pointerdown', function () {            
+            if (IsAtTop(scene, 'btn')) {
+                scene.print.text += `click ${btn.name}\n`;
+            }
+        })
+
+
     return btn;
 }
 
@@ -66,13 +71,6 @@ var config = {
         autoCenter: Phaser.Scale.CENTER_BOTH,
     },
     scene: Demo,
-    plugins: {
-        global: [{
-            key: 'rexButton',
-            plugin: ButtonPlugin,
-            start: true
-        }]
-    }
 };
 
 var game = new Phaser.Game(config);
