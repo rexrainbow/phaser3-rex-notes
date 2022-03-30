f3ce8863c28d97bba4b175d1b43915e3e4b29fa0
Rex <binhua.tein@gmail.com>
2022-03-30 10:20:28 +0800

    Draw images for repeating mode

--- plugins/gameobjects/blitter/ninepatch/texture/DrawTileSprite.js
+++ plugins/gameobjects/blitter/ninepatch/texture/DrawTileSprite.js
@@ -1,9 +1,22 @@
 var DrawTileSprite = function (key, frame, x, y, width, height) {
-    this.addImage({
-        frame: frame,
-        x: x, y: y,
-        width: width, height: height
-    })
+    var frameObj = this.texture.get(frame);
+    var frameWidth = frameObj.width,
+        frameHeight = frameObj.height;
+    var colCount = Math.floor(width / frameWidth),
+        rowCount = Math.floor(height / frameHeight);
+    // Align images at center
+    x += (width - (colCount * frameWidth)) / 2;
+    y += (height - (rowCount * frameHeight)) / 2;
+    for (var colIndex = 0; colIndex < colCount; colIndex++) {
+        for (var rowIndex = 0; rowIndex < rowCount; rowIndex++) {
+            this.addImage({
+                frame: frame,
+                x: x + (colIndex * frameWidth),
+                y: y + (rowIndex * frameHeight),
+            })
+        }
+    }
+
 }
 
 export default DrawTileSprite;
