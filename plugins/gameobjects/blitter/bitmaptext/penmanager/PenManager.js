85d83204264a959ad316fab6899dc5e51b2cee63
Rex <binhua.tein@gmail.com>
2022-04-11 17:22:19 +0800

    Split methods

--- plugins/gameobjects/blitter/bitmaptext/penmanager/PenManager.js
+++ plugins/gameobjects/blitter/bitmaptext/penmanager/PenManager.js
@@ -1,8 +1,5 @@
 import Methods from './methods/Methods.js';
 import Pool from '../../../../pool.js';
-import CharPen from './pen/CharPen.js';
-
-const RemoveItem = Phaser.Utils.Array.Remove;
 
 var PensPool = new Pool(); // default pens pool
 var LinesPool = new Pool(); // default lines pool
@@ -21,65 +18,6 @@ class PenManager {
     destroy() {
         this.clear();
     }
-
-    free(pen) {
-        if (!this.PensPool) {
-            return this;
-        }
-        this.PensPool.push(pen);
-        pen.onFree();
-
-        for (var i = 0, cnt = this.lines.length; i < cnt; i++) {
-            RemoveItem(this.lines[i], pen);
-        }
-        return this;
-    }
-
-    freeMultiple(pens) {
-        if (!this.PensPool) {
-            return this;
-        }
-
-        for (var i = 0, cnt = pens.length; i < cnt; i++) {
-            this.free(pens[i]);
-        }
-
-        return this;
-    }
-
-    clear() {
-        // 1. Remove/recycle all children of blitter
-        this.parent.removeChildren();
-        // 2. Free all pens. 
-        for (var i = 0, cnt = this.pens.length; i < cnt; i++) {
-            this.pens[i].onFree();
-        }
-        // 3. Free all lines
-        for (var i = 0, len = this.lines.length; i < len; i++) {
-            this.lines[i].length = 0;
-        }
-
-        // 4. 
-        this.PensPool.pushMultiple(this.pens);
-        this.LinesPool.pushMultiple(this.lines);
-        this.maxLinesWidth = undefined;
-    }
-
-    remove(pen) {
-        this.free(pen);
-
-    }
-
-    addTextPen(text) {
-        for (var i = 0, cnt = text.length; i < cnt; i++) {
-            var pen = new CharPen(this.parent)
-                .setChar(text.charAt(i))
-            this.pens.push(pen);
-        }
-
-        return this;
-    }
-
 }
 
 Object.assign(
--- plugins/gameobjects/blitter/bitmaptext/penmanager/methods/Methods.js
+++ plugins/gameobjects/blitter/bitmaptext/penmanager/methods/Methods.js
@@ -1,7 +1,12 @@
 import AddImageBob from './AddImageBob.js';
+import RemovePenMethods from './RemovePenMethods.js';
+import AddTextPens from './AddTextPens.js';
 
 var Methods = {
-    addImageBob: AddImageBob
+    addImageBob: AddImageBob,
+    ...RemovePenMethods,
+
+    addTextPens: AddTextPens,
 }
 
 export default Methods;
