cd44a720c5e692a3f319ddeb78205a0a98bc8c77
Rex <binhua.tein@gmail.com>
2022-04-12 23:15:34 +0800

    Remove AlignConst.js

--- plugins/gameobjects/dynamictext/dynamictext/methods/wrap/runverticalwrap/AlignLines.js
+++ plugins/gameobjects/dynamictext/dynamictext/methods/wrap/runverticalwrap/AlignLines.js
@@ -1,59 +1,59 @@
-import { HAlign, VAlign } from '../AlignConst.js';
-
 var AlignLines = function (result, width, height) {
     var hAlign = result.hAlign,
         vAlign = result.vAlign;
 
-    if (typeof (hAlign) === 'string') {
-        hAlign = HAlign[hAlign];
-        result.hAlign = hAlign;
-    }
-    if (typeof (vAlign) === 'string') {
-        vAlign = VAlign[vAlign];
-        result.vAlign = vAlign;
-    }
+    var xOffset, yOffset;
 
     var rtl = result.rtl;
     var lines = result.lines,
         lineWidth = result.lineWidth,
         linesWidth = result.linesWidth;
-    var xOffset;
     switch (hAlign) {
-        case 0:  // left
-            xOffset = 0;
-            break;
         case 1:  // center
+        case 'center':
             xOffset = (width - linesWidth) / 2
             break;
+
         case 2:  // right
+        case 'right':
             xOffset = width - linesWidth;
+            break;
+
+        default:  // left
+            xOffset = 0;
             break;
     }
     if (rtl) {
         xOffset += lineWidth;
     }
+
     for (var li = 0, lcnt = lines.length; li < lcnt; li++) {
         var line = lines[(rtl) ? (lcnt - li - 1) : li];
         var children = line.children;
         var lineHeight = line.height;
 
-        var yOffset;
         switch (vAlign) {
-            case 0: // top
-                yOffset = 0;
-                break;
             case 1: // center
+            case 'center':
                 yOffset = (height - lineHeight) / 2;
                 break;
+
             case 2: // bottom
+            case 'bottom':
                 yOffset = height - lineHeight;
+                break;
+
+            default: // top
+                yOffset = 0;
                 break;
         }
 
-        for (var ci = 0, ccnt = children.length; ci < ccnt; ci++) {
-            var child = children[ci];
-            child.x += xOffset;
-            child.y += yOffset;
+        if ((xOffset !== 0) || (yOffset !== 0)) {
+            for (var ci = 0, ccnt = children.length; ci < ccnt; ci++) {
+                var child = children[ci];
+                child.x += xOffset;
+                child.y += yOffset;
+            }
         }
 
         xOffset += lineWidth;
--- plugins/gameobjects/dynamictext/dynamictext/methods/wrap/runwordwrap/AlignLines.js
+++ plugins/gameobjects/dynamictext/dynamictext/methods/wrap/runwordwrap/AlignLines.js
@@ -1,60 +1,58 @@
-import { HAlign, VAlign } from '../AlignConst.js';
-
 var AlignLines = function (result, width, height) {
     var hAlign = result.hAlign,
         vAlign = result.vAlign;
 
-    if (typeof (hAlign) === 'string') {
-        hAlign = HAlign[hAlign];
-        result.hAlign = hAlign;
+    var xOffset, yOffset;
+
+    var linesHeight = result.linesHeight;
+    switch (vAlign) {
+        case 1: // center
+        case 'center':
+            yOffset = (height - linesHeight) / 2;
+            break;
+
+        case 2: // bottom
+        case 'bottom':
+            yOffset = height - linesHeight;
+            break;
+
+        default:
+            yOffset = 0;
+            break;
     }
-    if (typeof (vAlign) === 'string') {
-        vAlign = VAlign[vAlign];
-        result.vAlign = vAlign;
-    }
 
-    if (hAlign !== 0) {  // left align does not have offset
-        var lines = result.lines;
-        for (var li = 0, lcnt = lines.length; li < lcnt; li++) {
-            var line = lines[li];
-            var lineWidth = line.width,
-                children = line.children;
+    var lines = result.lines;
+    for (var li = 0, lcnt = lines.length; li < lcnt; li++) {
+        var line = lines[li];
+        var lineWidth = line.width,
+            children = line.children;
 
-            var xOffset;
-            switch (hAlign) {
-                case 1:  // center
-                    xOffset = (width - lineWidth) / 2
-                    break;
-                case 2:  // right
-                    xOffset = width - lineWidth;
-                    break;
-            }
+        switch (hAlign) {
+            case 1:  // center
+            case 'center':
+                xOffset = (width - lineWidth) / 2
+                break;
 
+            case 2:  // right
+            case 'right':
+                xOffset = width - lineWidth;
+                break;
+
+            default:
+                xOffset = 0;
+                break;
+        }
+
+        if ((xOffset !== 0) || (yOffset !== 0)) {
             for (var ci = 0, ccnt = children.length; ci < ccnt; ci++) {
                 var child = children[ci];
                 child.x += xOffset;
+                child.y += yOffset;
             }
         }
+
     }
 
-    if (vAlign !== 0) { // top align does not have offset
-        var linesHeight = result.linesHeight;
-        var yOffset;
-        switch (vAlign) {
-            case 1: // center
-                yOffset = (height - linesHeight) / 2;
-                break;
-            case 2: // bottom
-                yOffset = height - linesHeight;
-                break;
-        }
-
-        var children = result.children;
-        for (var ci = 0, ccnt = children.length; ci < ccnt; ci++) {
-            var child = children[ci];
-            child.y += yOffset;
-        }
-    }
 }
 
 export default AlignLines;
