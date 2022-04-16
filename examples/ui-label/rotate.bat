a68c2774e796798d95ca04e8fad120e4ea3447f9
Rex <binhua.tein@gmail.com>
2022-04-16 18:39:50 +0800

    Add test code

---
+++ examples/ui-label/rotate.bat
@@ -0,0 +1,5 @@
+@echo off
+set main=./examples/ui-label/rotate.js
+cd ..
+cd ..
+npm run watch
---
+++ examples/ui-label/rotate.js
@@ -0,0 +1,67 @@
+import phaser from 'phaser/src/phaser.js';
+import UIPlugin from '../../templates/ui/ui-plugin.js';
+
+const COLOR_PRIMARY = 0x4e342e;
+const COLOR_LIGHT = 0x7b5e57;
+const COLOR_DARK = 0x260e04;
+
+class Demo extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'examples'
+        })
+
+    }
+
+    preload() { }
+
+    create() {
+        CreateLabel(this, 'button0')
+            .setPosition(200, 300)
+            .layout()
+
+        CreateLabel(this, 'button1')
+            .setPosition(500, 300)
+            .layout()
+            .setAngle(-90) // setAngle after layout()
+    }
+
+    update() { }
+}
+
+var CreateLabel = function (scene, text) {
+    return scene.rexUI.add.label({
+        background: scene.rexUI.add.roundRectangle(0, 0, 2, 2, 20, COLOR_PRIMARY),
+        text: scene.add.text(0, 0, text),
+        icon: scene.rexUI.add.roundRectangle(0, 0, 0, 0, 10, COLOR_LIGHT),
+        align: 'center',
+        space: {
+            left: 20,
+            right: 20,
+            top: 20,
+            bottom: 20,
+            icon: 10
+        }
+    });
+}
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    scene: Demo,
+    plugins: {
+        scene: [{
+            key: 'rexUI',
+            plugin: UIPlugin,
+            mapping: 'rexUI'
+        }]
+    }
+};
+
+var game = new Phaser.Game(config);
