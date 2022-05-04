41bc76c902c0c0fdc6658aeeb9b7ad73e3a81f83
Rex <binhua.tein@gmail.com>
2022-05-05 00:47:30 +0800

    Include transform testing

--- examples/live2d/hit-test.js
+++ examples/live2d/hit-test.js
@@ -1,5 +1,6 @@
 import phaser from 'phaser/src/phaser.js';
 import Live2dPlugin from '../../plugins/live2d-plugin.js';
+import Dat from '../../plugins/utils/dat.gui/dat.gui.min.js';
 
 class Demo extends Phaser.Scene {
     constructor() {
@@ -23,6 +24,7 @@ class Demo extends Phaser.Scene {
             autoPlayIdleMotion: 'TapBody'
         })
             .setScale(0.5)
+            .setOrigin(0.5, 0.5)
             .setInteractive()
             .on('pointerdown-Head', function () {
                 print.text += 'Hit Head\n'
@@ -33,7 +35,7 @@ class Demo extends Phaser.Scene {
 
 
         // Interactive with touch pointer
-        var printDragXY = this.add.text(0, 1080 / 2, '', { fontSize: 24 });
+        var printDragXY = this.add.text(0, (1080 / 2) + 200, '', { fontSize: 24 });
         this.input.on('pointermove', function (pointer) {
             var x = pointer.worldX, y = pointer.worldY;
             character.lookAt(x, y);
@@ -45,9 +47,29 @@ class Demo extends Phaser.Scene {
         })
         character.lookForward();
 
+
+        this.character = character;
+        this.debuggerGraphics = this.add.graphics()
+
+        var gui = new Dat.GUI();
+        gui.add(this.character, 'x', 0, 1920);
+        gui.add(this.character, 'y', 0, 1080);
+        gui.add(this.character, 'scale', 0.1, 1);
+        gui.add(this.character, 'angle', -180, 180);
     }
 
     update() {
+        this.debuggerGraphics
+            .clear()
+            // .lineStyle(2, 0xffff00)
+            // .strokeRectShape(this.character.getBounds())
+            .lineStyle(2, 0xff0000)
+            .lineBetween(
+                this.character.x,
+                this.character.y,
+                this.character.x + 100 * Math.cos(this.character.rotation),
+                this.character.y + 100 * Math.sin(this.character.rotation),
+            )
     }
 }
 
