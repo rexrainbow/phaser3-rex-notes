92681a763fbe787944ae965efa5b57797123feda
Rex <binhua.tein@gmail.com>
2022-05-09 17:23:52 +0800

    Always check hitarea

--- plugins/gameobjects/live2d/gameobject/methods/interactive/HitAreaCallback.js
+++ plugins/gameobjects/live2d/gameobject/methods/interactive/HitAreaCallback.js
@@ -4,23 +4,13 @@ var HitAreaCallback = function (shape, localX, localY, gameObject) {
         return false;
     }
 
-    var hitTestResult = model._hitTestResult;
-
-    if ((localX < 0) || (localX > model._modelWidth) ||
-        (localY < 0) || (localY > model._modelHeight)) {
-        // Set all hit test result to false
-        for (var name in hitTestResult) {
-            hitTestResult[name] = false;
-        }
-        return false;
-    }
-
     var matrixXY = model.localXYToModelMatrixXY(localX, localY, true);
     var x = matrixXY.x
     var y = matrixXY.y;
     var modelSetting = model._modelSetting;
     var count = modelSetting.getHitAreasCount();
     var anyHit = false;
+    var hitTestResult = model._hitTestResult;
     for (var i = 0; i < count; i++) {
         var hitAreaName = modelSetting.getHitAreaName(i);
         var isHit = model.hitTest(hitAreaName, x, y);
