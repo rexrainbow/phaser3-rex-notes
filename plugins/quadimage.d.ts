9025f30fac7fa3c4bb6a746b893b367f90255b73
Rex <binhua.tein@gmail.com>
2022-06-05 15:28:32 +0800

    Export class

--- plugins/quadimage.d.ts
+++ plugins/quadimage.d.ts
@@ -1,7 +1,9 @@
 import QuadImage from './gameobjects/mesh/quad/image/Image';
 import QuadRenderTexture from './gameobjects/mesh/quad/rendertexture/RenderTexture';
+import SkewImage from './gameobjects/mesh/quad/skewimage/SkewImage';
 
 export {
     QuadImage,
     QuadRenderTexture,
+    SkewImage,
 }
--- plugins/quadimage.js
+++ plugins/quadimage.js
@@ -1,7 +1,9 @@
 import QuadImage from './gameobjects/mesh/quad/image/Image.js';
 import QuadRenderTexture from './gameobjects/mesh/quad/rendertexture/RenderTexture.js';
+import SkewImage from './gameobjects/mesh/quad/skewimage/SkewImage.js';
 
 export {
     QuadImage,
-    QuadRenderTexture
+    QuadRenderTexture,
+    SkewImage
 }
