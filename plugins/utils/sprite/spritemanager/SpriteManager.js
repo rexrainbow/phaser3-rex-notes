87b815f5ab5b1ea529b02e822e1b827571bcea0c
Rex <binhua.tein@gmail.com>
2022-06-20 12:03:37 +0800

    Split methods

--- plugins/utils/sprite/spritemanager/SpriteManager.js
+++ plugins/utils/sprite/spritemanager/SpriteManager.js
@@ -1,11 +1,8 @@
 import EventEmitterMethods from '../../eventemitter/EventEmitterMethods.js';
-import SpriteData from './SpriteData.js';
-import AddTintRGBProperties from '../../../behaviors/tintrgb/AddTintRGBProperties.js';
 import IsEmpty from '../../../utils/object/IsEmpty.js';
 import Methods from './methods/Methods.js';
 
 const GetValue = Phaser.Utils.Objects.GetValue;
-const RemoveItem = Phaser.Utils.Array.Remove;
 
 class SpriteManager {
     constructor(scene, config) {
@@ -68,49 +65,10 @@ class SpriteManager {
         return this;
     }
 
-    has(name) {
-        return this.sprites.hasOwnProperty(name);
-    }
-
-    get(name) {
-        return this.sprites[name];
-    }
-
     get isEmpty() {
         return IsEmpty(this.sprites) && (this.removedSprites.length === 0);
     }
 
-    add(name, textureKey, frameName) {
-        this.remove(name);
-
-        var sprite;
-        if (arguments.length === 3) {
-            sprite = this.createCallback(this.scene, textureKey, frameName);
-        } else {
-            var args = Array.prototype.slice.call(arguments, 1);
-            sprite = this.createCallback(this.scene, ...args);
-        }
-
-        if (this.fadeTime > 0) {
-            AddTintRGBProperties(sprite);
-        }
-        sprite.once('destroy', function () {
-            RemoveItem(this.removedSprites, sprite);
-            if (this.isEmpty) {
-                this.emit('empty');
-            }
-        }, this);
-
-        var spriteData = new SpriteData(this, sprite, name);
-        this.sprites[name] = spriteData;
-
-        if (this.fadeTime > 0) {
-            spriteData
-                .setProperty('tintGray', 0)
-                .easeProperty('tintGray', 255, this.fadeTime)
-        }
-        return this;
-    }
 }
 
 Object.assign(
--- plugins/utils/sprite/spritemanager/methods/Methods.js
+++ plugins/utils/sprite/spritemanager/methods/Methods.js
@@ -1,3 +1,4 @@
+import AddMethods from './AddMethods.js';
 import RemoveMethods from './RemoveMethods.js';
 import PropertyMethods from './PropertyMethods.js';
 import AnimationMethods from './AnimationMethods.js';
@@ -5,6 +6,7 @@ import AnimationMethods from './AnimationMethods.js';
 var Methods = {}
 Object.assign(
     Methods,
+    AddMethods,
     RemoveMethods,
     PropertyMethods,
     AnimationMethods
