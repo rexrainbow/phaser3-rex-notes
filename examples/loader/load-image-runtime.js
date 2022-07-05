582b500e0b7717f25672c5d8042b466aca1f62fb
Rex <binhua.tein@gmail.com>
2022-07-05 18:14:33 +0800

    Update test code

--- examples/loader/load-image-runtime.js
+++ examples/loader/load-image-runtime.js
@@ -8,26 +8,18 @@ class Demo extends Phaser.Scene {
     }
 
     preload() {
-        // load image in preload stage
-        this.load.image('arrow', 'assets/images/arrow.png');
     }
 
     create() {
-        this.add.image(700, 500, 'arrow');
-
-        // load image in update loop
-        this.input.on('pointerdown', function () {
-            this.load
-                .image('map', 'https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap')
-                .once('complete', function () {
-                    this.add.image(0, 0, 'map').setOrigin(0);
-                }, this)
-                .start();
-        }, this);
-
+        this.load
+            .image('classroom', 'assets/images/backgrounds/classroom.png')
+            .once('complete', function () {
+                this.add.image(0, 0, 'classroom').setOrigin(0);
+            }, this)
+            .start();
     }
 
-    update() {}
+    update() { }
 }
 
 var config = {
