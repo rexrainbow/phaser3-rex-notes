1d3b27fd80c0b400a09b6719c62af2c33887cb8c
Rex <binhua.tein@gmail.com>
2022-07-14 01:15:58 +0800

    Rename

--- examples/gridcutimage/cut-camera.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set main=./examples/gridcutimage/cut-camera.js
-cd ..
-cd ..
-npm run watch
---
+++ examples/gridcutimage/cut-rendertexture.bat
@@ -0,0 +1,5 @@
+@echo off
+set main=./examples/gridcutimage/cut-rendertexture.js
+cd ..
+cd ..
+npm run watch
