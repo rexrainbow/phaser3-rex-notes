aedcac456c31dc5dd5608ae2c80d762062417ef5
Rex <binhua.tein@gmail.com>
2022-07-17 23:09:08 +0800

    Split PropertyMap out

--- plugins/utils/text/textbase/textstyle/TextStyle.js
+++ plugins/utils/text/textbase/textstyle/TextStyle.js
@@ -1,3 +1,4 @@
+import DefaultPropertyMap from './PropertyMap.js';
 import MeasureText from './MeasureText.js';
 import CONST from '../const.js';
 import GetStyle from '../../../canvas/GetStyle.js';
@@ -7,64 +8,16 @@ const GetValue = Phaser.Utils.Objects.GetValue;
 
 //  Key: [ Object Key, Default Value, postCallback ]
 
-var propertyMap = {
-    // background
-    backgroundColor: ['backgroundColor', null, GetStyle],
-    backgroundColor2: ['backgroundColor2', null, GetStyle],
-    backgroundHorizontalGradient: ['backgroundHorizontalGradient', true, null],
-    backgroundStrokeColor: ['backgroundStrokeColor', null, GetStyle],
-    backgroundStrokeLineWidth: ['backgroundStrokeLineWidth', 2, null],
-    backgroundCornerRadius: ['backgroundCornerRadius', 0, null],
-    backgroundCornerIteration: ['backgroundCornerIteration', null, null],
-
-    // font
-    fontFamily: ['fontFamily', 'Courier', null],
-    fontSize: ['fontSize', '16px', null],
-    fontStyle: ['fontStyle', '', null],
-    color: ['color', '#fff', GetStyle],
-    stroke: ['stroke', '#fff', GetStyle],
-    strokeThickness: ['strokeThickness', 0, null],
-    shadowOffsetX: ['shadow.offsetX', 0, null],
-    shadowOffsetY: ['shadow.offsetY', 0, null],
-    shadowColor: ['shadow.color', '#000', GetStyle],
-    shadowBlur: ['shadow.blur', 0, null],
-    shadowStroke: ['shadow.stroke', false, null],
-    shadowFill: ['shadow.fill', false, null],
-
-    // underline
-    underlineColor: ['underline.color', '#000', GetStyle],
-    underlineThickness: ['underline.thickness', 0, null],
-    underlineOffset: ['underline.offset', 0, null],
-
-    // align
-    halign: ['halign', 'left', null],
-    valign: ['valign', 'top', null],
-
-    // size
-    maxLines: ['maxLines', 0, null],
-    fixedWidth: ['fixedWidth', 0, null],
-    fixedHeight: ['fixedHeight', 0, null],
-    resolution: ['resolution', 0, null],
-    lineSpacing: ['lineSpacing', 0, null],
-    xOffset: ['xOffset', 0, null],
-
-    rtl: ['rtl', false, null],
-    testString: ['testString', '|MÃ‰qgy', null],
-    baselineX: ['baselineX', 1.2, null],
-    baselineY: ['baselineY', 1.4, null],
-
-    // wrap
-    wrapMode: ['wrap.mode', 0, null],
-    wrapWidth: ['wrap.width', 0, null],
-    wrapCallback: ['wrap.callback', null],
-    wrapCallbackScope: ['wrap.callbackScope', null],
-};
-
 class TextStyle {
-    constructor(text, style) {
+    constructor(text, style, propertyMap) {
         this.parent = text;
         // parent.updateText()
         // parent.width, parent.height
+
+        if (propertyMap === undefined) {
+            propertyMap = DefaultPropertyMap;
+        }
+        this.propertyMap = propertyMap;
 
         this.backgroundColor;
         this.backgroundColor2;
@@ -119,11 +72,11 @@ class TextStyle {
     }
 
     // Override
-    get canvas() {      
+    get canvas() {
     }
 
     // Override
-    get context() {        
+    get context() {
     }
 
     get isWrapFitMode() {
@@ -162,6 +115,7 @@ class TextStyle {
             style.fontSize = style.fontSize.toString() + 'px';
         }
 
+        var propertyMap = this.propertyMap;
         for (var key in propertyMap) {
             var prop = propertyMap[key];  // [ Object Key, Default Value, preCallback ]
             var objKey = prop[0];
@@ -608,6 +562,7 @@ class TextStyle {
     toJSON() {
         var output = {};
 
+        var propertyMap = this.propertyMap;
         for (var key in propertyMap) {
             output[key] = this[key];
         }
