6f655f88f34bec9446e7269ef5b547b3720f4c71
Rex <binhua.tein@gmail.com>
2022-07-19 21:42:55 +0800

    Move files

---
+++ examples/ui-label/vertical-label-text.bat
@@ -0,0 +1,5 @@
+@echo off
+set main=./examples/ui-label/vertical-label-text.js
+cd ..
+cd ..
+npm run watch
--- examples/ui-wrap-expand-text/vertical-label-text.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set main=./examples/ui-wrap-expand-text/vertical-label-text.js
-cd ..
-cd ..
-npm run watch
