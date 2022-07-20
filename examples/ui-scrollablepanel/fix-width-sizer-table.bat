753622f0d1e75466eba13b8bc030430cb2cfc463
Rex <binhua.tein@gmail.com>
2022-07-20 23:23:20 +0800

    Rename files

---
+++ examples/ui-scrollablepanel/fix-width-sizer-table.bat
@@ -0,0 +1,5 @@
+@echo off
+set main=./examples/ui-scrollablepanel/fix-width-sizer-table.js
+cd ..
+cd ..
+npm run watch
---
+++ examples/ui-scrollablepanel/grid-sizer-table.bat
@@ -0,0 +1,5 @@
+@echo off
+set main=./examples/ui-scrollablepanel/grid-sizer-table.js
+cd ..
+cd ..
+npm run watch
