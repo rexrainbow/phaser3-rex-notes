55a0332c042babf2a495c14b9a8a5c57ac423411
Rex <binhua.tein@gmail.com>
2022-08-05 12:32:44 +0800

    Add test code

---
+++ examples/shader-warp/camera-postfx-behavior.bat
@@ -0,0 +1,5 @@
+@echo off
+set main=./examples/shader-warp/camera-postfx-behavior.js
+cd ..
+cd ..
+npm run watch
---
+++ examples/shader-warp/camera-postfx-behavior.js
@@ -0,0 +1,46 @@
+import phaser from 'phaser/src/phaser.js';
+import WarpPipelineBehavior from '../../plugins/warppipelinebehavior.js'
+import Dat from '../../plugins/utils/dat.gui/dat.gui.min.js';
+
+class Demo extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'examples'
+        })
+    }
+
+    preload() {
+        this.load.image('classroom', 'assets/images/backgrounds/classroom.png');
+    }
+
+    create() {
+        var gameObject = this.add.image(400, 300, 'classroom')//.setScale(0.75);
+        var behavior = new WarpPipelineBehavior(this.cameras.main);
+        var postFxPipeline = behavior.getPipeline();
+
+        var gui = new Dat.GUI();
+        gui.add(postFxPipeline, 'frequencyX', 0, 100);
+        gui.add(postFxPipeline, 'frequencyY', 0, 100);
+        gui.add(postFxPipeline, 'amplitudeX', 0, 100);
+        gui.add(postFxPipeline, 'amplitudeY', 0, 100);
+        gui.add(postFxPipeline, 'progressX', 0, 1);
+        gui.add(postFxPipeline, 'progressY', 0, 1);
+    }
+
+    update() {
+    }
+}
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    scene: Demo,
+};
+
+var game = new Phaser.Game(config);
