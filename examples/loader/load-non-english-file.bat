3fc8b496551b2c94b9913c2139913e772477fdaa
Rex <binhua.tein@gmail.com>
2022-08-12 10:50:13 +0800

    Add test

---
+++ examples/loader/load-non-english-file.bat
@@ -0,0 +1,5 @@
+@echo off
+set main=./examples/loader/load-non-english-file.js
+cd ..
+cd ..
+npm run watch
---
+++ examples/loader/load-non-english-file.js
@@ -0,0 +1,33 @@
+import phaser from 'phaser/src/phaser.js';
+
+class Demo extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'examples'
+        })
+    }
+
+    preload() {
+        this.load.image('箭頭', 'assets/images/箭頭.png');
+    }
+
+    create() {
+        this.add.image(400, 300, '箭頭');
+    }
+
+    update() { }
+}
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    scene: Demo
+};
+
+var game = new Phaser.Game(config);
