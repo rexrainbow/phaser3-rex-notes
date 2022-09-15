63ee945c10c63f3da7ed5d3fe56c8684d2f61084
Rex <binhua.tein@gmail.com>
2022-09-15 19:50:23 +0800

    Twist logic

--- templates/spinner/box/Box.js
+++ templates/spinner/box/Box.js
@@ -1,8 +1,6 @@
 import Base from '../base/Base.js';
 import { Lines } from '../utils/Geoms.js';
 
-const Linear = Phaser.Math.Linear;
-
 class Box extends Base {
     constructor(scene, config) {
         super(scene, config);
@@ -21,24 +19,23 @@ class Box extends Base {
 
         var halfWidth = radius * 0.7;
         var left = centerX - halfWidth,
-            right = centerX + halfWidth,
             top = centerY - halfWidth,
-            bottom = centerY + halfWidth;
+            width = halfWidth * 2;
 
         this.getShape('border')
             .lineStyle(2, this.color, 1)
-            .startAt(left, top).lineTo(right, top)
-            .lineTo(right, bottom).lineTo(left, bottom)
-            .lineTo(left, top).close();
+            .startAt(left, top).lineTo(width, 0, true)
+            .lineTo(0, width, true).lineTo(-width, 0, true)
+            .lineTo(0, -width, true).close();
 
         if (this.value < 0.5) {
             var t = (0.5 - this.value) * 2;
-            var fillBottom = top + t * halfWidth * 2;
+            var height = width * t;
             this.getShape('fill')
                 .fillStyle(this.color, 1)
-                .startAt(left, top).lineTo(right, top)
-                .lineTo(right, fillBottom).lineTo(left, fillBottom)
-                .lineTo(left, top).close();
+                .startAt(left, top).lineTo(width, 0, true)
+                .lineTo(0, height, true).lineTo(-width, 0, true)
+                .lineTo(0, -height, true).close();
 
         } else { // Rotate
             var t = (this.value - 0.5) * 2;
