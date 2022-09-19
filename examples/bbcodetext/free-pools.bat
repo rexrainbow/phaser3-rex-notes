41cb82b3dae2b23382eb56052741b2ff33d5c0a5
Rex <binhua.tein@gmail.com>
2022-09-19 10:06:19 +0800

    Add test code

---
+++ examples/bbcodetext/free-pools.bat
@@ -0,0 +1,5 @@
+@echo off
+set main=./examples/bbcodetext/free-pools.js
+cd ..
+cd ..
+npm run watch
---
+++ examples/bbcodetext/free-pools.js
@@ -0,0 +1,50 @@
+import phaser from 'phaser/src/phaser.js';
+import BBCodeTextPlugin from '../../plugins/bbcodetext-plugin.js';
+
+class Demo extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'examples'
+        })
+    }
+
+    preload() {
+
+    }
+
+    create() {
+        for (var i = 0; i < 1; i++) {
+            this.add.rexBBCodeText(400, 300, 'ABC')
+            .once('destroy', function(){
+                console.log('destroy text')
+            })
+        }
+
+        this.input.once('pointerdown', function () {
+            this.game.destroy();
+        }, this)
+    }
+
+    update() { }
+}
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    scene: Demo,
+    plugins: {
+        global: [{
+            key: 'BBCodeTextPlugin',
+            plugin: BBCodeTextPlugin,
+            start: true
+        }]
+    }
+};
+
+var game = new Phaser.Game(config);
