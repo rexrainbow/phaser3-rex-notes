edcfec70480b3b7b3d6af9dd6189d827aa7fa29e
Rex <binhua.tein@gmail.com>
2022-10-07 18:04:16 +0800

    Expose VPXYToXY method

--- plugins/viewportcoordinate-plugin.d.ts
+++ plugins/viewportcoordinate-plugin.d.ts
@@ -1,5 +1,7 @@
 import AddViewportCoordinateProperties from './viewportcoordinate';
+import { VPXYToXY } from './viewportcoordinate';
 
 export default class PolarCoordinatePlugin extends Phaser.Plugins.BasePlugin {
     add: typeof AddViewportCoordinateProperties;
+    vpxyToxy: typeof VPXYToXY;
 }
