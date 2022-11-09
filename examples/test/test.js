9133b78731e5e875f26b11c834066ea18395e969
Rex <binhua.tein@gmail.com>
2022-11-09 13:56:55 +0800

    Clear test code

--- examples/test/test.js
+++ examples/test/test.js
@@ -1,9 +1,4 @@
 import phaser from 'phaser/src/phaser.js';
-import ScrollerPlugin from '../../plugins/scroller-plugin.js';
-
-const linesCount = 300;
-
-const pad = Phaser.Utils.String.Pad;
 
 class Demo extends Phaser.Scene {
     constructor() {
@@ -13,38 +8,9 @@ class Demo extends Phaser.Scene {
     }
 
     preload() {
-        this.load.bitmapFont('gothic', 'assets/fonts/gothic.png', 'assets/fonts/gothic.xml');
     }
 
     create() {
-        var s = '';
-        for (var i = 0; i < linesCount; i++) {
-            s += pad(i.toString(), 4, '0', 1);
-            if (i < (linesCount - 1)) {
-                s += '\n';
-            }
-        }
-
-        var txt = this.add.bitmapText(0, 0, 'gothic', s).setOrigin(0, 0);
-
-        var bg = this.add.rectangle(0, 0, 800, txt.height, 0xff0000).setOrigin(0, 0).setAlpha(0.2);
-
-        var bottomBound = txt.height;
-        var topBound = 0;
-        var cam = this.cameras.main;
-
-        this.plugins.get('rexScroller').add(bg, {
-            bounds: [
-                bottomBound,
-                topBound
-            ],
-            dragReverse: true,
-            value: 0,
-            valuechangeCallback: function (value) {                
-                cam.scrollY = value;
-                console.log(value)
-            }
-        });
     }
 
     update() { }
@@ -59,15 +25,7 @@ var config = {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
     },
-    scene: Demo,
-    backgroundColor: 0x333333,
-    plugins: {
-        global: [{
-            key: 'rexScroller',
-            plugin: ScrollerPlugin,
-            start: true
-        }]
-    }
+    scene: Demo
 };
 
 var game = new Phaser.Game(config);
