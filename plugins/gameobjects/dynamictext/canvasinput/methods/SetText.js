16743a3253280f4c88b8c56ffc811ec889428049
Rex <binhua.tein@gmail.com>
2022-11-12 23:45:31 +0800

    Remove all children when set text to empty string

--- plugins/gameobjects/dynamictext/canvasinput/methods/SetText.js
+++ plugins/gameobjects/dynamictext/canvasinput/methods/SetText.js
@@ -8,18 +8,24 @@ var SetText = function (textObject, newText) {
 
     // textObject.setText(newText);
 
-    var results = diffChars(text, newText);
-    var charIndex = 0;
-    for (var i = 0, cnt = results.length; i < cnt; i++) {
-        var result = results[i];
-        if (result.removed) {
-            // Remove character at charIndex
-            textObject.removeText(charIndex, result.count);
-        } else if (result.added) {
-            textObject.insertText(charIndex, result.value);
-            charIndex += result.count;
-        } else {
-            charIndex += result.count;
+    if (newText === '') {
+        textObject.popChild(textObject.lastInsertCursor);
+        textObject.removeChildren();
+        textObject.addChild(textObject.lastInsertCursor, 0);
+    } else {
+        var results = diffChars(text, newText);
+        var charIndex = 0;
+        for (var i = 0, cnt = results.length; i < cnt; i++) {
+            var result = results[i];
+            if (result.removed) {
+                // Remove character at charIndex
+                textObject.removeText(charIndex, result.count);
+            } else if (result.added) {
+                textObject.insertText(charIndex, result.value);
+                charIndex += result.count;
+            } else {
+                charIndex += result.count;
+            }
         }
     }
 
