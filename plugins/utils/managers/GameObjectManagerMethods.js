eb5e97974c856587ef0af17417ab317e765b7018
Rex <binhua.tein@gmail.com>
2023-01-11 09:00:28 +0800

    Remove file

--- plugins/utils/managers/GameObjectManagerMethods.js
+++ plugins/utils/managers/GameObjectManagerMethods.js
@@ -1,7 +1,22 @@
-import AddGameObjectManager from './AddGameObjectManager.js';
+import GameObjectManagerBase from '../gameobject/gomanager/GOManager.js';
 
 export default {
-    addGameObjectManager: AddGameObjectManager,
+    addGameObjectManager(config, GameObjectManagerClass) {
+        if (config === undefined) {
+            config = {};
+        }
+        if (GameObjectManagerClass === undefined) {
+            GameObjectManagerClass = GameObjectManagerBase;
+        }
+
+        if (!config.createGameObjectScope) {
+            config.createGameObjectScope = this;
+        }
+        var gameobjectManager = new GameObjectManagerClass(this.managersScene, config);
+        this.gameObjectManagers[config.name] = gameobjectManager;
+
+        return this;
+    },
 
     getGameObjectManager(name) {
         return this.gameObjectManagers[name];
