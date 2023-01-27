52e3aeb7563b8a21c72c1258801b773deeded8b2
Rex <binhua.tein@gmail.com>
2023-01-27 10:26:34 +0800

    Duplicate logic

--- templates/ui/tweaker/gameobjects/utils/CreateSlider.js
+++ templates/ui/tweaker/gameobjects/utils/CreateSlider.js
@@ -1,8 +1,6 @@
 import Slider from '../../../slider/Slider.js';
-import BuildSliderConfig from './BuildSliderConfig.js';
 
 var CreateSlider = function (scene, config) {
-    config = BuildSliderConfig(scene, config);
     var gameObject = new Slider(scene, config);
     scene.add.existing(gameObject);
 
