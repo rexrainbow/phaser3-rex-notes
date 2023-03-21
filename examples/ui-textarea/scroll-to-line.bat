7a581db3cbf925ff4107cd0a09ddc35d95c3a143
Rex <binhua.tein@gmail.com>
2023-03-21 10:45:38 +0800

    Rename

---
+++ examples/ui-textarea/scroll-to-line.bat
@@ -0,0 +1,5 @@
+@echo off
+set main=./examples/ui-textarea/scroll-to-line.js
+cd ..
+cd ..
+npm run watch
--- examples/ui-textarea/scrolldown-line.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set main=./examples/ui-textarea/scrolldown-line.js
-cd ..
-cd ..
-npm run watch
