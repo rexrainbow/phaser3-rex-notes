e906094517e832e30aaf1e7cc165c49de05d6a52
Rex <binhua.tein@gmail.com>
2023-04-23 00:01:54 +0800

    Remove unused code

--- dist/rexparticlesalongboundsplugin.js
+++ dist/rexparticlesalongboundsplugin.js
@@ -268,27 +268,10 @@
   };
   var globPoint;
 
-  /*
-  var ParticlesAlongBounds = function (gameObject, config, particles) {
-      if (config === undefined) {
-          config = {};
-      }
-
-      // Create particles
-      if ((particles === undefined) || (!particles.scene)) {
-          particles = CreateParticles(gameObject);
-      }
-      particles.setTexture(config.textureKey);
-
-      // Create emitter
-      config.gameObject = gameObject;
-      var emitter = CreateEmitter(particles, config);
-
-      return particles;
-  }
-  */
-
   var ParticlesAlongBounds = function ParticlesAlongBounds(gameObject, config) {
+    if (config === undefined) {
+      config = {};
+    }
     var emitterConfig = CreateEmitterConfig(gameObject, config);
     var particles = gameObject.scene.add.particles(0, 0, config.textureKey, emitterConfig);
     SyncToGameObject(particles, gameObject, config);
--- plugins/behaviors/particlesalongbounds/ParticlesAlongBounds.js
+++ plugins/behaviors/particlesalongbounds/ParticlesAlongBounds.js
@@ -1,27 +1,11 @@
 import CreateEmitterConfig from './methods/CreateEmitterConfig.js';
 import SyncToGameObject from './methods/SyncToGameObject.js';
 
-/*
-var ParticlesAlongBounds = function (gameObject, config, particles) {
+var ParticlesAlongBounds = function (gameObject, config) {
     if (config === undefined) {
         config = {};
     }
 
-    // Create particles
-    if ((particles === undefined) || (!particles.scene)) {
-        particles = CreateParticles(gameObject);
-    }
-    particles.setTexture(config.textureKey);
-
-    // Create emitter
-    config.gameObject = gameObject;
-    var emitter = CreateEmitter(particles, config);
-
-    return particles;
-}
-*/
-
-var ParticlesAlongBounds = function (gameObject, config) {
     var emitterConfig = CreateEmitterConfig(gameObject, config);
     var particles = gameObject.scene.add.particles(0, 0, config.textureKey, emitterConfig);
     SyncToGameObject(particles, gameObject, config);
