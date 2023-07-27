06b07e9910c23165addfc3851c9ae972870411c3
Rex <binhua.tein@gmail.com>
2023-07-27 20:29:03 +0800

    Only check phaser3 version one time

--- plugins/utils/system/CheckP3Version.js
+++ plugins/utils/system/CheckP3Version.js
@@ -1,13 +1,21 @@
 const MinVersion = 60;
 
+var HasTested = false;
+
 var CheckP3Version = function (minVersion) {
+    if (HasTested) {
+        return;
+    }
+
     if (minVersion === undefined) {
         minVersion = MinVersion;
     }
     var currentVersion = parseInt(Phaser.VERSION.match(/\.(\d+)\./)[1]);
     if (currentVersion < minVersion) {
-        console.error(`Minimum supported version : ${minVersion}, current phaser3 version : ${currentVersion}`)
+        console.error(`Minimum supported version : 3.${minVersion}`)
     }
+
+    HasTested = true;
 }
 
 export default CheckP3Version;
