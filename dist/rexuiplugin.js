cda0651ffb86289e0d85068a7220c1c0e02cc1fa
Rex <binhua.tein@gmail.com>
2023-07-28 23:29:07 +0800

    Export plugins

--- dist/rexuiplugin.js
+++ dist/rexuiplugin.js
@@ -54241,24 +54241,25 @@
     if (!this.hasChild(child)) {
       return this;
     }
+    var scrollableBlock = this.childrenMap.child;
     var dy;
     if (this.scrollMode === 0) {
       switch (align) {
         case AlignConst.top:
         case 'top':
-          dy = this.top - child.getTopLeft().y;
+          dy = scrollableBlock.top - child.getTopLeft().y;
           break;
         case AlignConst.bottom:
         case 'bottom':
-          dy = this.bottom - child.getBottomLeft().y;
+          dy = scrollableBlock.bottom - child.getBottomLeft().y;
           break;
         case AlignConst.center:
         case 'center':
-          dy = this.centerY - child.getCenter().y;
+          dy = scrollableBlock.centerY - child.getCenter().y;
           break;
         default:
-          var dTop = this.top - child.getTopLeft().y;
-          var dBottom = this.bottom - child.getBottomLeft().y;
+          var dTop = scrollableBlock.top - child.getTopLeft().y;
+          var dBottom = scrollableBlock.bottom - child.getBottomLeft().y;
           if (dTop <= 0 && dBottom >= 0) {
             dy = 0;
           } else {
@@ -54270,19 +54271,19 @@
       switch (align) {
         case AlignConst.left:
         case 'left':
-          dy = this.left - child.getTopLeft().x;
+          dy = scrollableBlock.left - child.getTopLeft().x;
           break;
         case AlignConst.right:
         case 'right':
-          dy = this.right - child.getTopRight().x;
+          dy = scrollableBlock.right - child.getTopRight().x;
           break;
         case AlignConst.center:
         case 'center':
-          dy = this.centerX - child.getCenter().x;
+          dy = scrollableBlock.centerX - child.getCenter().x;
           break;
         default:
-          var dLeft = this.left - child.getTopLeft().x;
-          var dRight = this.right - child.getTopRight().x;
+          var dLeft = scrollableBlock.left - child.getTopLeft().x;
+          var dRight = scrollableBlock.right - child.getTopRight().x;
           if (dLeft <= 0 && dRight >= 0) {
             dy = 0;
           } else {
