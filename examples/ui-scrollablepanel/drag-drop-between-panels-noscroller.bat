ec4936aa02b631e80feaf5697d33114eee2608de
Rex <binhua.tein@gmail.com>
2023-08-24 22:20:52 +0800

    Rename

---
+++ examples/ui-scrollablepanel/drag-drop-between-panels-noscroller.bat
@@ -0,0 +1,5 @@
+@echo off
+set main=./examples/ui-scrollablepanel/drag-drop-between-panels-noscroller.js
+cd ..
+cd ..
+npm run watch
--- examples/ui-scrollablepanel/drag-drop-between-panels-v2.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set main=./examples/ui-scrollablepanel/drag-drop-between-panels-v2.js
-cd ..
-cd ..
-npm run watch
