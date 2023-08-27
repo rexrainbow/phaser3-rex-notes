f5748226e3ee10232b3637237d6c05749928e78e
Rex <binhua.tein@gmail.com>
2023-08-27 15:41:13 +0800

    Merge method back

--- plugins/gameobjects/dynamictext/dynamictext/bob/renderbase/Contains.js
+++ plugins/gameobjects/dynamictext/dynamictext/bob/renderbase/Contains.js
@@ -1,5 +1,4 @@
 import CanvasPositionToBobPosition from '../../methods/utils/transform/CanvasPositionToBobPosition.js';
-import GetBobBounds from './GetBobBounds.js';
 
 var Contains = function (canvasX, canvasY) {
     if ((this.width === 0) || (this.height === 0)) {
@@ -9,5 +8,24 @@ var Contains = function (canvasX, canvasY) {
     var bobPosition = CanvasPositionToBobPosition(canvasX, canvasY, this, true);
     return GetBobBounds(this).contains(bobPosition.x, bobPosition.y);
 }
+
+const Rectangle = Phaser.Geom.Rectangle;
+
+var GetBobBounds = function (bob, out) {
+    if (out === undefined) {
+        if (globBounds === undefined) {
+            globBounds = new Rectangle();
+        }
+        out = globBounds;
+    }
+
+    var x = bob.drawTLX,
+        y = bob.drawTLY;
+    out.setTo(x, y, (bob.drawTRX - x), (bob.drawBLY - y));
+
+    return out;
+}
+
+var globBounds;
 
 export default Contains;
