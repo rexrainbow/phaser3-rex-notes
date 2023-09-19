189467d1f1b4129899573f06077945b0673ce18d
Rex <binhua.tein@gmail.com>
2023-09-19 20:26:25 +0800

    Add test code

---
+++ examples/ui-textarea/scale.bat
@@ -0,0 +1,5 @@
+@echo off
+set main=./examples/ui-textarea/scale.js
+cd ..
+cd ..
+npm run watch
---
+++ examples/ui-textarea/scale.js
@@ -0,0 +1,97 @@
+import phaser from 'phaser/src/phaser.js';
+import UIPlugin from '../../templates/ui/ui-plugin.js';
+
+const COLOR_PRIMARY = 0x4e342e;
+const COLOR_LIGHT = 0x7b5e57;
+const COLOR_DARK = 0x260e04;
+
+class Demo extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'examples'
+        })
+    }
+
+    preload() { }
+
+    create() {
+        var textArea = this.rexUI.add.textArea({
+            x: 400,
+            y: 300,
+            width: 220,
+            height: 400,
+
+            background: this.rexUI.add.roundRectangle(0, 0, 2, 2, 0, COLOR_PRIMARY),
+
+            // text: this.add.text(),
+            text: this.rexUI.add.BBCodeText(),
+            // textMask: false,
+
+            slider: {
+                track: this.rexUI.add.roundRectangle(0, 0, 20, 10, 10, COLOR_DARK),
+                thumb: this.rexUI.add.roundRectangle(0, 0, 0, 0, 13, COLOR_LIGHT),
+            },
+
+            mouseWheelScroller: {
+                focus: false,
+                speed: 0.1
+            },
+
+            header: this.rexUI.add.label({
+                height: 30,
+
+                orientation: 0,
+                background: this.rexUI.add.roundRectangle(0, 0, 20, 20, 0, COLOR_DARK),
+                text: this.add.text(0, 0, 'Header'),
+            }),
+
+            footer: this.rexUI.add.label({
+                height: 30,
+
+                orientation: 0,
+                background: this.rexUI.add.roundRectangle(0, 0, 20, 20, 0, COLOR_DARK),
+                text: this.add.text(0, 0, 'Footer'),
+            }),
+
+            content: CreateContent(100),
+        })
+            .layout()
+
+        this.add.text(0, 580, 'scale')
+            .setInteractive()
+            .once('pointerdown', function () {
+                textArea.setScale(0.7)
+            })
+    }
+
+    update() { }
+}
+
+var CreateContent = function (linesCount) {
+    var numbers = [];
+    for (var i = 0; i < linesCount; i++) {
+        numbers.push('[color=' + ((i % 2) ? 'green' : 'yellow') + ']' + i.toString() + '[/color]');
+    }
+    return numbers.join('\n');
+}
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    scene: Demo,
+    plugins: {
+        scene: [{
+            key: 'rexUI',
+            plugin: UIPlugin,
+            mapping: 'rexUI'
+        }]
+    }
+};
+
+var game = new Phaser.Game(config);
