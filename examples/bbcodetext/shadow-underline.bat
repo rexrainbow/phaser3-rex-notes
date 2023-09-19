658831b448f34d6aae7bbbf0d866af0802ae6b77
Rex <binhua.tein@gmail.com>
2023-09-19 09:48:17 +0800

    Rename

---
+++ examples/bbcodetext/shadow-underline.bat
@@ -0,0 +1,5 @@
+@echo off
+set main=./examples/bbcodetext/shadow-underline.js
+cd ..
+cd ..
+npm run watch
--- examples/bbcodetext/stroke-underline.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set main=./examples/bbcodetext/stroke-underline.js
-cd ..
-cd ..
-npm run watch
