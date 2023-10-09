120e21fb2076b8fcb753bd6865a437f0b8d8b0e1
Rex <binhua.tein@gmail.com>
2023-10-09 20:48:26 +0800

    Align top-left to (0,0) by default

--- templates/gameobjectshell/shell/methods/PropertiesPanelMethods.js
+++ templates/gameobjectshell/shell/methods/PropertiesPanelMethods.js
@@ -4,15 +4,21 @@ const GetValue = Phaser.Utils.Objects.GetValue;
 
 export default {
     addPropertiesPanel(config) {
-        var panelConfig = GetValue(config, 'panel');
-        var extraProperties = GetValue(config, 'extraProperties');
+        var panelConfig = GetValue(config, 'panel', {});
+        var unknowPositon = (panelConfig.x === undefined) && (panelConfig.y === undefined);
+
+        var extraProperties = GetValue(config, 'extraProperties', {});
+
         var panel = new PropertiesPanel(this.scene, panelConfig, extraProperties);
-        if ((panel.x === 0) && (panel.y === 0)) {
-            panel.setOrigin(0)
-        }
+
         this.scene.add.existing(panel);
         panel.layout();
         this.addToUILayer(panel);
+
+        if (unknowPositon) {
+            panel.left = 0;
+            panel.top = 0;
+        }
 
         this.panel = panel;
 
