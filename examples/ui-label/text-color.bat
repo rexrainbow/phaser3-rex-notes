b3462180268cf06d25bb2fdb8a72a4dd4c3ecb18
Rex <binhua.tein@gmail.com>
2023-12-06 09:28:10 +0800

    Add test case

---
+++ examples/ui-label/text-color.bat
@@ -0,0 +1,5 @@
+@echo off
+set main=./examples/ui-label/text-color.js
+cd ..
+cd ..
+npm run watch
---
+++ examples/ui-label/text-color.js
@@ -0,0 +1,64 @@
+import phaser from 'phaser/src/phaser.js';
+import UIPlugin from '../../templates/ui/ui-plugin.js';
+
+const COLOR_MAIN = 0x4e342e;
+const COLOR_LIGHT = 0x7b5e57;
+const COLOR_DARK = 0x260e04;
+
+class Demo extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'examples'
+        })
+
+    }
+
+    preload() { }
+
+    create() {
+        var label = CreateLabel(this, 'button0')
+            .setPosition(400, 300)
+            .layout()
+
+        label.getElement('text').setColor('red')
+    }
+
+    update() { }
+}
+
+var CreateLabel = function (scene, text) {
+    return scene.rexUI.add.label({
+        background: scene.rexUI.add.roundRectangle(0, 0, 2, 2, 20, COLOR_MAIN),
+        text: scene.add.text(0, 0, text),
+        icon: scene.rexUI.add.roundRectangle(0, 0, 0, 0, 10, COLOR_LIGHT),
+        align: 'center',
+        space: {
+            left: 20,
+            right: 20,
+            top: 20,
+            bottom: 20,
+            icon: 10
+        }
+    });
+}
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    scene: Demo,
+    plugins: {
+        scene: [{
+            key: 'rexUI',
+            plugin: UIPlugin,
+            mapping: 'rexUI'
+        }]
+    }
+};
+
+var game = new Phaser.Game(config);
