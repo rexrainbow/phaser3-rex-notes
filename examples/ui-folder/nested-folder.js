4789eb7046f97af4d6a39fdb94adbcba232cd845
Rex <binhua.tein@gmail.com>
2024-02-18 18:02:20 +0800

    Update test code

--- examples/ui-folder/nested-folder.js
+++ examples/ui-folder/nested-folder.js
@@ -19,6 +19,11 @@ class Demo extends Phaser.Scene {
     }
 
     create() {
+        var print = this.add.text(0, 0, '');
+        var clickCallback = function (item) {
+            print.text += `Click ${item.text}\n`
+        }
+
         var folderA = CreateFolder(this, {
             title: 'FolderA',
             children: [
@@ -35,7 +40,7 @@ class Demo extends Phaser.Scene {
                 'BB',
                 'CC'
             ],
-        })
+        }, clickCallback)
             .setMinWidth(300)
             .setPosition(200, 100)
             .layout();
@@ -44,12 +49,23 @@ class Demo extends Phaser.Scene {
     update() { }
 }
 
-var CreateFolder = function (scene, { title, children }) {
+var CreateFolder = function (scene, { title, children }, clickCallback) {
+    var child = CreateFolderChild(scene, children, clickCallback);
+
+    var title = CreateTitle(scene, title, COLOR_MAIN, clickCallback)
+        .on('folder.expand', function (folder) {
+            title.getElement('toggleTarget').setDirection('down')
+        })
+        .on('folder.collapse', function (folder) {
+            title.getElement('toggleTarget').setDirection('right')
+        })
+
     return scene.rexUI.add.folder({
         orientation: 'y',
 
-        child: CreateFolderChild(scene, children),
-        title: CreateLabel(scene, title, COLOR_MAIN),
+        child: child,
+        title: title,
+        toggleByTarget: title.getElement('toggleTarget'),
         toggleClickConfig: {
             threshold: 10,
         },
@@ -72,7 +88,7 @@ var CreateFolder = function (scene, { title, children }) {
 
 }
 
-var CreateFolderChild = function (scene, children) {
+var CreateFolderChild = function (scene, children, clickCallback) {
     var panel = scene.rexUI.add.sizer({
         orientation: 'y',
     })
@@ -80,9 +96,9 @@ var CreateFolderChild = function (scene, children) {
     for (var i = 0, cnt = children.length; i < cnt; i++) {
         var child = children[i];
         if (typeof (child) === 'string') {
-            child = CreateLabel(scene, child, COLOR_DARK);
+            child = CreateItem(scene, child, COLOR_DARK, clickCallback);
         } else {
-            child = CreateFolder(scene, child);
+            child = CreateFolder(scene, child, clickCallback);
         }
 
         panel.add(child, { expand: true });
@@ -91,16 +107,60 @@ var CreateFolderChild = function (scene, children) {
     return panel;
 }
 
-var CreateLabel = function (scene, text, backgroundColor) {
-    return scene.rexUI.add.label({
-        background: scene.rexUI.add.roundRectangle(0, 0, 0, 0, 0, backgroundColor).setStrokeStyle(2, COLOR_LIGHT),
+var CreateTitle = function (scene, text, backgroundColor, clickCallback) {
+    var title = scene.rexUI.add.sizer({
+        orientation: 'x',
+        space: {
+            left: 10, right: 10, top: 10, bottom: 10, item: 5
+        }
+    })
+        .addBackground(scene.rexUI.add.roundRectangle({
+            color: backgroundColor,
+            strokeColor: COLOR_LIGHT,
+            strokeWidth: 2
+        }))
+        .add(scene.rexUI.add.triangle({
+            color: COLOR_LIGHT,
+            padding: 2
+        }), { fitRatio: 1, key: 'toggleTarget' })
+        .add(
+            scene.rexUI.add.label({
+                text: scene.add.text(0, 0, text, {
+                    fontSize: 18
+                }),
+            }),
+            { proportion: 1, key: 'item' }
+        );
+
+    var item = title.getElement('item');
+    item.onClick(function () {
+        clickCallback(item)
+    });
+
+    return title;
+}
+
+var CreateItem = function (scene, text, backgroundColor, clickCallback) {
+    var item = scene.rexUI.add.label({
+        background: scene.rexUI.add.roundRectangle({
+            color: backgroundColor,
+            strokeColor: COLOR_LIGHT,
+            strokeWidth: 2
+        }),
+
         text: scene.add.text(0, 0, text, {
             fontSize: 18
         }),
+
         space: {
             left: 10, right: 10, top: 10, bottom: 10
         }
-    });
+    })
+        .onClick(function () {
+            clickCallback(item)
+        });
+
+    return item;
 }
 
 var config = {
