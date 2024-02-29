02d3d5940289b05e8ec2227fc8ddc3a428db767c
Rex <binhua.tein@gmail.com>
2024-02-29 11:33:32 +0800

    Add test code

---
+++ examples/scale-manager/expand.js
@@ -0,0 +1,33 @@
+import phaser from 'phaser/src/phaser.js';
+
+class Demo extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'examples'
+        })
+    }
+
+    preload() {
+        this.load.image('classroom', 'assets/images/backgrounds/classroom.png');
+    }
+
+    create() {
+        this.add.image(0, 0, 'classroom').setOrigin(0);
+    }
+
+    update() { }
+}
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scale: {
+        mode: Phaser.Scale.EXPAND,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    scene: Demo
+};
+
+var game = new Phaser.Game(config);
