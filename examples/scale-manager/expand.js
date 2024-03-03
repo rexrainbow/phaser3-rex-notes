bc8f997a4ddfd8e84b45b8413eb9ab9f5847131c
Rex <binhua.tein@gmail.com>
2024-03-04 01:29:32 +0800

    GetViewport method failed

--- examples/scale-manager/expand.js
+++ examples/scale-manager/expand.js
@@ -13,6 +13,16 @@ class Demo extends Phaser.Scene {
 
     create() {
         this.add.image(0, 0, 'classroom').setOrigin(0);
+
+        var rect = this.add.rectangle(0, 0, 0, 0).setOrigin(0).setStrokeStyle(5, 0xff0000);
+
+        this.scale.on('resize', function (gameSize, baseSize, displaySize, previousWidth, previousHeight) {
+            var viewport = this.scale.getViewPort();
+            rect
+                .setPosition(viewport.x, viewport.y)
+                .setSize(viewport.width, viewport.height)
+
+        }, this);
     }
 
     update() { }
