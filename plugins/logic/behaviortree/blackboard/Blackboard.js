acd1fce0662feac31acf4c53eecba3878edda87a
Rex <binhua.tein@gmail.com>
2024-03-05 12:50:54 +0800

    Twist logic

--- plugins/logic/behaviortree/blackboard/Blackboard.js
+++ plugins/logic/behaviortree/blackboard/Blackboard.js
@@ -2,12 +2,21 @@ import Base from './Base.js';
 import { TREE_STATE, CURRENT_TIME } from '../constants.js';
 
 class Blackboard extends Base {
+    getTreeData(treeID, key) {
+        return this.get(key, treeID);
+    }
+
+    setTreeData(treeID, key, data) {
+        this.set(key, data, treeID);
+        return this;
+    }
+
     getTreeState(treeID) {
-        return this.get(TREE_STATE, treeID);
+        return this.getTreeData(treeID, TREE_STATE);
     }
 
     setTreeState(treeID, state) {
-        this.set(TREE_STATE, state, treeID);
+        this.setTreeData(treeID, TREE_STATE, state);
         return this;
     }
 
