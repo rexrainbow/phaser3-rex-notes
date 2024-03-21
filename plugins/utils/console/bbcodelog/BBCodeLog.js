a26bcea764a7418d555a0bd6d1cf0ac6a733d0bc
Rex <binhua.tein@gmail.com>
2024-03-21 13:46:57 +0800

    Twist logic

--- plugins/utils/console/bbcodelog/BBCodeLog.js
+++ plugins/utils/console/bbcodelog/BBCodeLog.js
@@ -16,20 +16,11 @@ class BBCodeLog {
         var inputs = [];
         var modifiers = [];
         this.parser.parse(s).forEach(function (item) {
-            var value = item.value;
-            if ((typeof (value) === 'object') ||
-                (typeof (value) === 'function')) {
-                inputs.push('%o');
-                modifiers.push(value);
-
-            } else {  // string
-                if (item.css) {
-                    inputs.push(`%c${value}`);
-                    modifiers.push(item.css);
-                } else { // No css
-                    inputs.push(value);
-                }
-
+            if (item.css) {
+                inputs.push(`%c${item.value}`);
+                modifiers.push(item.css);
+            } else { // No css
+                inputs.push(item.value);
             }
         })
 
