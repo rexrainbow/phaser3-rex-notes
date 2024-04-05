87e92ded6bffe4267f84875f3295e252f4652f42
Rex <binhua.tein@gmail.com>
2024-04-06 01:56:43 +0800

    Change depth of child in container

--- plugins/utils/gameobject/displaylist/DisplayListMethods.js
+++ plugins/utils/gameobject/displaylist/DisplayListMethods.js
@@ -1,49 +1,47 @@
+const ArrayUtils = Phaser.Utils.Array;
+
 export default {
     bringMeToTop() {
-        var displayList = this.displayList;
-        if (!displayList) {
+        var list = (this.parentContainer) ? gameObject.parentContainer : this.displayList.list;
+        if (!list) {
             return this;
         }
 
-        displayList.bringToTop(this);
+        ArrayUtils.BringToTop(list, this);
+
         return this;
     },
 
     sendMeToBack() {
-        var displayList = this.displayList;
-        if (!displayList) {
+        var list = (this.parentContainer) ? gameObject.parentContainer : this.displayList.list;
+        if (!list) {
             return this;
         }
 
-        displayList.sendToBack(this);
+        ArrayUtils.SendToBack(list, this);
+
         return this;
     },
 
     moveMyDepthBelow(gameObject) {
-        var displayList = this.displayList;
-        if (!displayList) {
-            return this;
-        }
-        if (gameObject.displayList !== displayList) {
-            // Do nothing if not at the same display list
+        var list = (this.parentContainer) ? gameObject.parentContainer : this.displayList.list;
+        if (!list) {
             return this;
         }
 
-        displayList.moveBelow(gameObject, this);
+        ArrayUtils.MoveBelow(list, gameObject, this);
+
         return this;
     },
 
     moveMyDepthAbove(gameObject) {
-        var displayList = this.displayList;
-        if (!displayList) {
-            return this;
-        }
-        if (gameObject.displayList !== displayList) {
-            // Do nothing if not at the same display list
+        var list = (this.parentContainer) ? gameObject.parentContainer : this.displayList.list;
+        if (!list) {
             return this;
         }
 
-        displayList.moveAbove(gameObject, this);
+        ArrayUtils.MoveAbove(list, gameObject, this);
+
         return this;
     },
 }
