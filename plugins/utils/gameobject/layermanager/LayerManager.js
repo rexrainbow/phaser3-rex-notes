47a7c596baf1db416c3d34e33243cb52062c665c
Rex <binhua.tein@gmail.com>
2024-04-22 21:37:23 +0800

    Split methods

--- plugins/utils/gameobject/layermanager/LayerManager.js
+++ plugins/utils/gameobject/layermanager/LayerManager.js
@@ -1,5 +1,5 @@
 import GOManager from '../gomanager/GOManager.js';
-import SortGameObjectsByDepth from '../../system/SortGameObjectsByDepth.js';
+import LayerMethods from './LayerMethods.js';
 
 const GetValue = Phaser.Utils.Objects.GetValue;
 
@@ -69,83 +69,6 @@ class LayerManager extends GOManager {
         return this;
     }
 
-    // New methods
-    getLayer(name) {
-        return this.getGO(name);
-    }
-
-    getLayers(out) {
-        if (out === undefined) {
-            out = [];
-        }
-        this.forEachGO(function (gameObject) {
-            out.push(gameObject);
-        })
-        SortGameObjectsByDepth(out, false);
-        return out;
-    }
-
-    addToLayer(name, gameObject) {
-        var layer = this.getGO(name);
-        if (!layer) {
-            console.warn(`[LayerManager] Can't get layer "${name}"`);
-            return;
-        }
-
-        if (gameObject.isRexContainerLite) {
-            gameObject.addToLayer(layer);
-        } else {
-            layer.add(gameObject);
-        }
-
-        return this;
-    }
-
-    removeFromLayer(name, gameObject, addToScene) {
-        var layer = this.getGO(name);
-        if (!layer) {
-            console.warn(`[LayerManager] Can't get layer "${name}"`);
-            return;
-        }
-
-        if (addToScene === undefined) {
-            addToScene = true;
-        }
-
-        if (gameObject.isRexContainerLite) {
-            gameObject.removeFromLayer(layer, addToScene);
-        } else {
-            layer.remove(gameObject);
-            if (addToScene) {
-                gameObject.addToDisplayList();
-            }
-        }
-
-        return this;
-    }
-
-    clearLayer(name, destroyChildren) {
-        if (destroyChildren === undefined) {
-            destroyChildren = true;
-        }
-
-        var layer = this.getGO(name);
-        if (!layer) {
-            console.warn(`Can't get layer "${name}"`);
-            return;
-        }
-
-        if (destroyChildren) {
-            var children = layer.getAll();
-            for (var i = 0, cnt = children.length; i < cnt; i++) {
-                children.destroy();
-            }
-        } else {
-            layer.removeAll();
-        }
-
-        return this;
-    }
 }
 
 var CreateLayer = function (scene, depth) {
@@ -155,6 +78,11 @@ var CreateLayer = function (scene, depth) {
     }
     return layer;
 }
+
+Object.assign(
+    LayerManager.prototype,
+    LayerMethods
+)
 
 
 export default LayerManager;
---
+++ plugins/utils/gameobject/layermanager/LayerMethods.js
@@ -0,0 +1,80 @@
+import SortGameObjectsByDepth from '../../system/SortGameObjectsByDepth.js';
+
+export default {
+    getLayer(name) {
+        return this.getGO(name);
+    },
+
+    getLayers(out) {
+        if (out === undefined) {
+            out = [];
+        }
+        this.forEachGO(function (gameObject) {
+            out.push(gameObject);
+        })
+        SortGameObjectsByDepth(out, false);
+        return out;
+    },
+
+    addToLayer(name, gameObject) {
+        var layer = this.getGO(name);
+        if (!layer) {
+            console.warn(`[LayerManager] Can't get layer "${name}"`);
+            return;
+        }
+
+        if (gameObject.isRexContainerLite) {
+            gameObject.addToLayer(layer);
+        } else {
+            layer.add(gameObject);
+        }
+
+        return this;
+    },
+
+    removeFromLayer(name, gameObject, addToScene) {
+        var layer = this.getGO(name);
+        if (!layer) {
+            console.warn(`[LayerManager] Can't get layer "${name}"`);
+            return;
+        }
+
+        if (addToScene === undefined) {
+            addToScene = true;
+        }
+
+        if (gameObject.isRexContainerLite) {
+            gameObject.removeFromLayer(layer, addToScene);
+        } else {
+            layer.remove(gameObject);
+            if (addToScene) {
+                gameObject.addToDisplayList();
+            }
+        }
+
+        return this;
+    },
+
+    clearLayer(name, destroyChildren) {
+        if (destroyChildren === undefined) {
+            destroyChildren = true;
+        }
+
+        var layer = this.getGO(name);
+        if (!layer) {
+            console.warn(`Can't get layer "${name}"`);
+            return;
+        }
+
+        if (destroyChildren) {
+            var children = layer.getAll();
+            for (var i = 0, cnt = children.length; i < cnt; i++) {
+                children.destroy();
+            }
+        } else {
+            layer.removeAll();
+        }
+
+        return this;
+    },
+}
