7ef71b6a28cc84450b5ffa10558371ac9d55820e
Rex <binhua.tein@gmail.com>
2024-04-23 11:55:39 +0800

    Update comments

--- templates/markdownvisualnovel/defaultvariables/DefaultVariables.js
+++ templates/markdownvisualnovel/defaultvariables/DefaultVariables.js
@@ -1,5 +1,5 @@
 export default {
-    $typingSpeed: 100,             // TEXTBOX
-    $transitionDuration: 200,      // SPRITE, BG
-    $tintOthers: 0x333333,         // SPRITE
+    $typingSpeed: 100,             // TEXTBOX.typing
+    $transitionDuration: 200,      // SPRITE.cross, BG.cross
+    $tintOthers: 0x333333,         // SPRITE.focus
 }
