a7f99007e195eeba9f7a06fd42ebf9b3638fcab7
Rex <binhua.tein@gmail.com>
2024-04-29 18:12:06 +0800

    Add parameters

--- templates/markdownvisualnovel/expressions/RegisterRandomExpression.js
+++ templates/markdownvisualnovel/expressions/RegisterRandomExpression.js
@@ -1,6 +1,13 @@
 var RegisterRandomExpression = function (eventSheetManager) {
-    eventSheetManager.addExpression('random', function () {
-        return Math.random();
+    eventSheetManager.addExpression('random', function (a, b) {
+        if ((a === undefined) && (b === undefined)) {
+            a = 0;
+            b = 1;
+        } else if (b === undefined) {
+            b = a;
+            a = 0;
+        }
+        return a + Math.random() * (b - a);
     })
 }
 
