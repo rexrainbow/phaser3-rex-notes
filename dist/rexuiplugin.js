5565ccd873abfb2f9490ea568dd893f8bf4e777e
Rex <binhua.tein@gmail.com>
2024-05-12 15:32:27 +0800

    Export plugins

--- dist/rexuiplugin.js
+++ dist/rexuiplugin.js
@@ -61568,27 +61568,28 @@
         case 0:
           // bottom-to-top
           gameObject.setOrigin(0.5, 1);
-          gameObject.popUp(duration, 'y');
+          ScaleMethods.popUp.call(gameObject, duration, 'y');
           break;
         case 1:
           // top-to-bottom
           gameObject.setOrigin(0.5, 0);
-          gameObject.popUp(duration, 'y');
+          ScaleMethods.popUp.call(gameObject, duration, 'y');
           break;
         case 2:
           // right-to-left
           gameObject.setOrigin(1, 0.5);
-          gameObject.popUp(duration, 'x');
+          ScaleMethods.popUp.call(gameObject, duration, 'x');
           break;
         case 3:
           // left-to-right
           gameObject.setOrigin(0, 0.5);
-          gameObject.popUp(duration, 'x');
+          ScaleMethods.popUp.call(gameObject, duration, 'x');
           break;
       }
+      FadeMethods.fadeIn.call(gameObject, duration);
     },
     transitOut: function transitOut(gameObject, duration, parent) {
-      gameObject.fadeOut(duration);
+      FadeMethods.fadeOut.call(gameObject, duration);
     }
   };
 
