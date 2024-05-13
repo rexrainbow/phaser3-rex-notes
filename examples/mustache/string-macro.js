9f42934eef8fef0e9f68672b6d40feb4ad772efe
Rex <binhua.tein@gmail.com>
2024-05-13 23:15:08 +0800

    Update test code

--- examples/mustache/string-macro.js
+++ examples/mustache/string-macro.js
@@ -3,14 +3,26 @@ import mustache from 'mustache';
 var mainTemplate = `\
 <<prelude>>
 Hello, I am {{name}}
-<< postlude >>
+<<{postlude}>>
 `;
 
 var postTemplate = mustache.render(mainTemplate, {
     prelude: '(A fixed string)',
-    postlude: function(){
-        return '(A string from function)'
-    },
+    postlude: `
+TITLE
+  id=Title
+  vpx=-0.5
+  vpy=0
+  alpha=0
+  alignLeft=true
+  text0=1949年，2月25日，台北市大稻埕
+  
+Title.to
+  vpx=0
+  alpha=1
+  ease=Cubic
+  duration=2000
+`,
 }, {}, ['<<', '>>'])
 
 var result = mustache.render(postTemplate, { name: 'rex' })
