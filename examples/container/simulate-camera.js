07ec5757045f96641a8d07977da0e2b70b38ceeb
Rex <binhua.tein@gmail.com>
2024-05-16 10:54:58 +0800

    Wrap nested container into a class

--- examples/container/simulate-camera.js
+++ examples/container/simulate-camera.js
@@ -1,6 +1,77 @@
 import phaser from 'phaser/src/phaser.js';
 import Dat from '../../plugins/utils/dat.gui/dat.gui.min.js';
 
+const ContainerClass = Phaser.GameObjects.Container;
+const ContainerAdd = ContainerClass.prototype.add;
+class CameraTransformNode extends ContainerClass {
+    constructor(scene) {
+        super(scene);
+        this.type = 'rexCameraTransformNode';
+
+        this.scrollNode = new ContainerClass(scene);
+        ContainerAdd.call(this, this.scrollNode);
+
+        this.boot();
+    }
+
+    boot() {
+        this.scene.scale.on('resize', this.pinToCenter, this);
+        this.pinToCenter();
+    }
+
+    destroy(fromScene) {
+        //  This Game Object has already been destroyed
+        if (!this.scene || this.ignoreDestroy) {
+            return;
+        }
+        this.scene.scale.off('resize', this.pinToCenter, this);
+        super.destroy(fromScene);
+    }
+
+    pinToCenter() {
+        var prevX = this.x;
+        var prevY = this.y;
+
+        // var viewport = this.scene.scale.getViewPort();
+        var viewport = this.scene.cameras.main;
+        this.setPosition(viewport.centerX, viewport.centerY);
+
+        this.scrollNode.x -= this.x - prevX;
+        this.scrollNode.y -= this.y - prevY;
+
+        return this;
+    }
+
+    add(gameObject) {
+        this.scrollNode.add(gameObject);
+        return this;
+    }
+
+    set zoom(value) {
+        this.scale = value;
+    }
+
+    get zoom() {
+        return this.scale;
+    }
+
+    set scrollX(value) {
+        this.scrollNode.x = - this.x - value;
+    }
+
+    get scrollX() {
+        return - this.scrollNode.x - this.x;
+    }
+
+    set scrollY(value) {
+        this.scrollNode.y = - this.y - value;
+    }
+
+    get scrollY() {
+        return - this.scrollNode.y - this.y;
+    }
+}
+
 class Demo extends Phaser.Scene {
     constructor() {
         super({
@@ -14,61 +85,17 @@ class Demo extends Phaser.Scene {
     }
 
     create() {
-        var zoomRotationContainer = this.add.container();
-        var scrollXYContainer = this.add.container();
-        zoomRotationContainer.add(scrollXYContainer);
-
-        // Always put zoomRotationContainer at center of viewport
-        var self = this;
-        var CenterOn = function () {
-            var originX = scrollXYContainer.x + zoomRotationContainer.x;
-            var originY = scrollXYContainer.y + zoomRotationContainer.y;
-
-            var viewport = self.scale.getViewPort();
-            zoomRotationContainer.setPosition(viewport.centerX, viewport.centerY);
-
-            scrollXYContainer.tlx = - zoomRotationContainer.x;
-            scrollXYContainer.tly = - zoomRotationContainer.y;
-            scrollXYContainer.setPosition(originX - zoomRotationContainer.x, originY - zoomRotationContainer.y)
-        }
-        this.scale.on('resize', CenterOn);
-        CenterOn();
-
-        Object.defineProperty(zoomRotationContainer, 'zoom', {
-            set(value) {
-                this.scale = value;
-            },
-            get() {
-                return this.scale
-            }
-        });
-
-        Object.defineProperty(scrollXYContainer, 'scrollX', {
-            set(value) {
-                this.x = this.tlx + value;
-            },
-            get() {
-                return this.x - this.tlx;
-            }
-        });
-
-        Object.defineProperty(scrollXYContainer, 'scrollY', {
-            set(value) {
-                this.y = this.tly + value;
-            },
-            get() {
-                return this.y - this.tly;
-            }
-        });
+        var cameraNdoe = new CameraTransformNode(this);
+        this.add.existing(cameraNdoe);
 
         var gui = new Dat.GUI();
-        gui.add(zoomRotationContainer, 'zoom', 0.1, 3);
-        gui.add(scrollXYContainer, 'scrollX', -200, 200);
-        gui.add(scrollXYContainer, 'scrollY', -200, 200);
-        gui.add(zoomRotationContainer, 'rotation', -1.57, 1.57);
+        gui.add(cameraNdoe, 'zoom', 0.1, 3);
+        gui.add(cameraNdoe, 'scrollX', -200, 200);
+        gui.add(cameraNdoe, 'scrollY', -200, 200);
+        gui.add(cameraNdoe, 'rotation', -1.57, 1.57);
 
         var gameObject = this.add.image(400, 300, 'classroom');
-        scrollXYContainer.add(gameObject);
+        cameraNdoe.add(gameObject);
     }
 
     update() { }
