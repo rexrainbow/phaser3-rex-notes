97addd193542f7d5cdce50eff387dd1b8e601521
Rex <binhua.tein@gmail.com>
2024-05-20 23:15:07 +0800

    Move file

--- examples/ui-gridbuttons/trace-pointerover.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set main=./examples/ui-gridbuttons/trace-pointerover.js
-cd ..
-cd ..
-npm run watch
---
+++ examples/ui-gridsizer/trace-pointerover.bat
@@ -0,0 +1,5 @@
+@echo off
+set main=./examples/ui-gridsizer/trace-pointerover.js
+cd ..
+cd ..
+npm run watch
