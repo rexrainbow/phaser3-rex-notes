2ccd3116f768aac6f60bd16527f1ae67af4bda56
Rex <binhua.tein@gmail.com>
2024-06-26 00:33:45 +0800

    Add get camera method

--- templates/gameobjectshell/shell/methods/Methods.js
+++ templates/gameobjectshell/shell/methods/Methods.js
@@ -1,5 +1,6 @@
 import LayerManagerMethods from './LayerManagerMethods.js';
 import BindingTargetMethods from './BindingTargetMethods.js';
+import CameraMethods from './CameraMethods.js';
 
 var Methods = {
 
@@ -9,6 +10,7 @@ Object.assign(
     Methods,
     LayerManagerMethods,
     BindingTargetMethods,
+    CameraMethods,
 )
 
 export default Methods;
