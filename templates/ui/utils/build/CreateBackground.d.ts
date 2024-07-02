e68439de3c6e12fbe69dca3c08a784e76517b3a4
Rex <binhua.tein@gmail.com>
2024-07-02 10:11:46 +0800

    Update d.ts

--- templates/ui/utils/build/CreateBackground.d.ts
+++ templates/ui/utils/build/CreateBackground.d.ts
@@ -1,14 +1,36 @@
 import StatesRoundRectangle from '../../statesroundrectangle/StatesRoundRectangle';
+import StatesNineSlice from '../../statesnineslice/StatesNineSlice';
+import StatesImage from '../../statesimage/StatesImage';
+import StatesNinePatch from '../../statesninepatch/StatesNinePatch';
 
 export default CreateBackground;
 
 declare namespace CreateBackground {
-    interface IConfig extends StatesRoundRectangle.IConfig {
+    interface IRoundRectangle extends StatesRoundRectangle.IConfig {
+        alpha?: number,
+        origin?: number, originX?: number, originY?: number,
+    }
+
+    interface INineSlice extends StatesNineSlice.IConfig {
+        alpha?: number,
+        tint?: number,
+        origin?: number, originX?: number, originY?: number,
+    }
+
+    interface IImage extends StatesImage.IConfig {
         alpha?: number,
         tint?: number,
         flipX?: boolean, flipY?: boolean,
         origin?: number, originX?: number, originY?: number,
     }
+
+    interface INinePatch extends StatesNinePatch.IConfig {
+        alpha?: number,
+        tint?: number,
+        origin?: number, originX?: number, originY?: number,
+    }
+
+    type IConfig = IRoundRectangle | INineSlice | IImage | INinePatch;
 }
 
 declare function CreateBackground(
--- templates/ui/utils/build/CreateImage.d.ts
+++ templates/ui/utils/build/CreateImage.d.ts
@@ -1,14 +1,37 @@
 import StatesImage from '../../statesimage/StatesImage';
+import StatesNineSlice from '../../statesnineslice/StatesNineSlice';
+import StatesNinePatch from '../../statesninepatch/StatesNinePatch';
+import StatesRoundRectangle from '../../statesroundrectangle/StatesRoundRectangle';
 
 export default CreateImage;
 
 declare namespace CreateImage {
-    interface IConfig extends StatesImage.IConfig {
+
+    interface IImage extends StatesImage.IConfig {
         alpha?: number,
         tint?: number,
         flipX?: boolean, flipY?: boolean,
         origin?: number, originX?: number, originY?: number,
     }
+
+    interface INineSlice extends StatesNineSlice.IConfig {
+        alpha?: number,
+        tint?: number,
+        origin?: number, originX?: number, originY?: number,
+    }
+
+    interface IRoundRectangle extends StatesRoundRectangle.IConfig {
+        alpha?: number,
+        origin?: number, originX?: number, originY?: number,
+    }
+
+    interface INinePatch extends StatesNinePatch.IConfig {
+        alpha?: number,
+        tint?: number,
+        origin?: number, originX?: number, originY?: number,
+    }
+
+    type IConfig = IImage | INineSlice | IRoundRectangle | INinePatch;
 }
 
 declare function CreateImage(
