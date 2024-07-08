432cbdeec6d8e837675e4250f18c0ea7770e8311
Rex <binhua.tein@gmail.com>
2024-07-08 11:22:33 +0800

    Remove unused parameter 'wrapCallback'

--- plugins/gameobjects/textbase/textstyle/TextStyle.js
+++ plugins/gameobjects/textbase/textstyle/TextStyle.js
@@ -64,8 +64,6 @@ class TextStyle {
 
         this.wrapMode;
         this.wrapWidth;
-        this.wrapCallback;
-        this.wrapCallbackScope;
 
         this._font;
 
@@ -127,17 +125,12 @@ class TextStyle {
             var defaultValue = (setDefaults) ? prop[1] : this[key];
             var postCallback = prop[2];
 
-
-            if (key === 'wrapCallback' || key === 'wrapCallbackScope') {
-                // Callback & scope should be set without processing the values
-                this[key] = GetValue(style, objKey, defaultValue);
-            } else {
-                var value = GetAdvancedValue(style, objKey, defaultValue);
-                if (postCallback) {
-                    value = postCallback(value);
-                }
-                this[key] = value;
+            var value = GetAdvancedValue(style, objKey, defaultValue);
+            if (postCallback) {
+                value = postCallback(value);
             }
+            this[key] = value;
+
 
         }
 
