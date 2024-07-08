e579cc995497cc0ba5c1cc87270f8b1437cdb5a1
Rex <binhua.tein@gmail.com>
2024-07-08 08:04:05 +0800

    Update test case

--- examples/cutjigsawimage/test.js
+++ examples/cutjigsawimage/test.js
@@ -10,16 +10,19 @@ class Demo extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('source', 'assets/images/jigsaw/square.png');
+        this.load.image('source', 'assets/images/jigsaw/hot-wheels.jpg');
     }
 
     create() {
-        var pieces = CreatePieces(this, 'source', 500, 350);
+        var centerX = this.cameras.main.width / 2;
+        var centerY = this.cameras.main.height / 2;
+        var pieces = CreatePieces(this, 'source', centerX, centerY);
 
         for (var i = 0, cnt = pieces.length; i < cnt; i++) {
             let piece = pieces[i];
+            piece.setAlpha(0.75)
             piece.preFX.setPadding(2);
-            piece.preFX.addGlow(0xff0000, 2, 0);
+            piece.preFX.addGlow(0x000000, 1, 0);
 
             piece
                 .setInteractive({
@@ -39,30 +42,14 @@ class Demo extends Phaser.Scene {
 
 
 var CreatePieces = function (scene, key, x, y) {
-    if (x === undefined) {
-        x = 0;
-    }
-    if (y === undefined) {
-        y = 0;
-    }
+    if (x === undefined) { x = 0; }
+    if (y === undefined) { y = 0; }
 
     var sourceImage = scene.add.image(x, y, key);
 
-    //I added this to the example, but it wont work anyway
-    let screenWidth = scene.sys.game.canvas.width;
-    let screenHeight = scene.sys.game.canvas.height;
-    let scaleX = (screenWidth - 200) / sourceImage.width;
-    let scaleY = (screenHeight - 300) / sourceImage.height;
-    if (scaleX < scaleY) {
-        sourceImage.setScale(scaleX, scaleX);
-    }
-    else {
-        sourceImage.setScale(scaleY, scaleY);
-    }
-
     var pieces = scene.plugins.get('rexCutJigsawImage').gridCut(sourceImage, {
         piecesKey: 'pieces',
-        columns: 6, rows: 5,
+        columns: 5, rows: 7,
         edgeWidth: 15, edgeHeight: 15,
         useDynamicTexture: false
     });
@@ -75,8 +62,8 @@ var CreatePieces = function (scene, key, x, y) {
 var config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
-    width: 1024,
-    height: 768,
+    width: 720,
+    height: 1280,
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
