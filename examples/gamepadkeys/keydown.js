24611200bf3d70e1167ec23ae260ff7cc85e8004
Rex <binhua.tein@gmail.com>
2024-07-12 17:54:25 +0800

    Add test case

---
+++ examples/gamepadkeys/keydown.js
@@ -0,0 +1,48 @@
+import phaser from 'phaser/src/phaser.js';
+import GamepadKeys from '../../plugins/input/gamepadkeys/GamepadKeys.js';
+
+
+class Demo extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'examples'
+        })
+    }
+
+    preload() {
+    }
+
+    create() {
+        this.print = this.add.text(0, 0, '')
+
+        this.gamepadKeys = new GamepadKeys(this);
+    }
+
+    update() {
+        var keys = this.gamepadKeys.keys;
+        var s = 'Key down: ';
+        for (var name in keys) {
+            if (keys[name].isDown) {
+                s += `${name} `;
+            }
+        }
+
+        this.print.text = s;
+    }
+}
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scale: {
+        mode: Phaser.Scale.RESIZE,
+    },
+    input: {
+        gamepad: true
+    },
+    scene: Demo,
+};
+
+var game = new Phaser.Game(config);
--- plugins/input/gamepadkeys/GamepadKeys.js
+++ plugins/input/gamepadkeys/GamepadKeys.js
@@ -1,56 +1,143 @@
 import CursorKeys from '../../utils/input/CursorKeys.js';
 
-const KeyNames = ['a', 'b', 'x', 'y', 'l1', 'l2', 'r1', 'r2'];
+const GetValue = Phaser.Utils.Objects.GetValue;
+const KeyNames = ['A', 'Y', 'X', 'B', 'L1', 'L2', 'R1', 'R2'];
 
 class GamepadKeys extends CursorKeys {
     constructor(scene) {
         super(scene);
         // this.scene = scene;
 
+        if (!this.gamepadManager) {
+            console.warn(`Gamepad feature is not activated`);
+        }
+
         this.addKeys(KeyNames);
         this.pad = null;
+        this.autoCapture = GetValue(config, 'autoCapture', true);
+
+        this.boot();
+    }
+
+    boot() {
+        if (this.gamepadManager) {
+            this.gamepadManager.on('down', this.onUpdateKeysState, this);
+            this.gamepadManager.on('disconnected', this.onDisconnect, this);
+
+            if (this.autoCapture) {
+                this.captureGamepad();
+            }
+        }
     }
 
     shutdown(fromScene) {
+        if (this.gamepadManager) {
+            this.gamepadManager.off('down', this.onUpdateKeysState, this);
+            this.gamepadManager.off('disconnected', this.onDisconnect, this);
+        }
+
         super.shutdown(fromScene);
+    }
+
+    onUpdateKeysState(gamepad) {
+        if (!this.isMyPad(gamepad)) {
+            return;
+        }
+
+        for (var keyName in this.keys) {
+            this.setKeyState(keyName, gamepad[keyName]);
+        }
+    }
+
+    onDisconnect(gamepad, event) {
+        if (!this.isMyPad(gamepad)) {
+            return;
+        }
+
+        this.setGamepad(null);
+
+        if (this.autoCapture) {
+            this.captureGamepad();
+        }
     }
 
     setGamepad(pad) {
         this.pad = pad;
+
+        if (pad) {
+            this.onUpdateKeysState(pad);
+        } else {
+            this.clearAllKeysState();
+        }
+
         return this;
     }
 
-    get aKeyDown() {
-        return this.keys.a.isDown;
+    isMyPad(pad) {
+        return (!!this.pad) && (this.pad === pad);
     }
 
-    get bKeyDown() {
-        return this.keys.b.isDown;
+    captureGamepad(callback) {
+        if (this.gamepadManager) {
+            this.gamepadManager.once('down', function (gamepad, button, value) {
+                this.setGamepad(gamepad);
+                if (callback) {
+                    callback(gamepad);
+                }
+            }, this)
+
+        } else {
+            if (callback) {
+                callback();
+            }
+
+        }
+        return this;
     }
 
-    get xKeyDown() {
-        return this.keys.x.isDown;
+    captureGamepadPromise() {
+        var self = this;
+        return new Promise(function (resolve, reject) {
+            self.captureGamepad(resolve);
+        });
     }
 
-    get yKeyDown() {
-        return this.keys.y.isDown;
+    get gamepadManager() {
+        this.scene.input.gamepad;
     }
 
-    get l1KeyDown() {
-        return this.keys.l1.isDown;
+    get AKeyDown() {
+        return this.keys.A.isDown;
     }
 
-    get l2KeyDown() {
-        return this.keys.l2.isDown;
+    get YKeyDown() {
+        return this.keys.Y.isDown;
     }
 
-    get r1KeyDown() {
-        return this.keys.r1.isDown;
+    get XKeyDown() {
+        return this.keys.X.isDown;
     }
 
-    get r2KeyDown() {
-        return this.keys.r2.isDown;
+    get BKeyDown() {
+        return this.keys.B.isDown;
     }
+
+    get L1KeyDown() {
+        return this.keys.L1.isDown;
+    }
+
+    get L2KeyDown() {
+        return this.keys.L2.isDown;
+    }
+
+    get R1KeyDown() {
+        return this.keys.R1.isDown;
+    }
+
+    get R2KeyDown() {
+        return this.keys.R2.isDown;
+    }
+
 }
 
 export default GamepadKeys;
