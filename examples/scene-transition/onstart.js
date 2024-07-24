08bfce2a494de48788e81c06bc404281813952c7
Rex <binhua.tein@gmail.com>
2024-07-24 22:12:10 +0800

    Cross fade camera

--- examples/scene-transition/onstart.js
+++ examples/scene-transition/onstart.js
@@ -1,4 +1,4 @@
-import phaser from 'phaser/src/phaser';
+import phaser from '../../../phaser/src/phaser.js';
 
 class SceneA extends Phaser.Scene {
     constructor() {
@@ -15,28 +15,43 @@ class SceneA extends Phaser.Scene {
         console.log('Create SceneA')
         this.add.image(400, 300, 'classroom');
 
+        var mode = 'tint';
         this.input.on('pointerdown', function () {
             this.scene.transition({
                 target: 'SceneB',
-                duration: 6000,
+                duration: 1000,
                 moveBelow: true,
 
                 onStart(fromScene, toScene, duration) {
-                    fromScene.tweens.add({
-                        targets: fromScene.cameras.main,
-                        alpha: { start: 1, to: 0 },
-                        delay: 0,
-                        duration: duration * 0.6,
-                        repeat: 0,
-                    });
+                    if (mode === 'alpha') {
+                        fromScene.tweens.add({
+                            targets: fromScene.cameras.main,
+                            alpha: { start: 1, to: 0 },
+                            delay: 0,
+                            duration: duration * 0.6,
+                            repeat: 0,
+                        });
 
-                    toScene.tweens.add({
-                        targets: toScene.cameras.main,
-                        alpha: { start: 0, to: 1 },
-                        delay: duration * 0.4,
-                        duration: duration * 0.6,
-                        repeat: 0,
-                    });
+                        toScene.tweens.add({
+                            targets: toScene.cameras.main,
+                            alpha: { start: 0, to: 1 },
+                            delay: duration * 0.4,
+                            duration: duration * 0.6,
+                            repeat: 0,
+                        });
+                    } else {
+                        // Tint fade out : 0 ~ 0.5
+                        fromScene.cameras.main.fadeOut(duration * 0.5);
+
+                        // Delay : 0 ~ 0.5
+                        // Tint fade in : 0.5 ~ 1
+                        toScene.time.delayedCall(duration * 0.5, function () {
+                            toScene.cameras.main.fadeIn(duration * 0.5);
+                        })
+                    }
+
+
+
                 }
             });
         }, this)
