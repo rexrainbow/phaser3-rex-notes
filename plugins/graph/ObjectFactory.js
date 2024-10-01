9cef26b3c922ee1b54343d4457a54c98d02d68ac
Rex <binhua.tein@gmail.com>
2024-10-01 09:34:43 +0800

    Fix bug

--- plugins/graph/ObjectFactory.js
+++ plugins/graph/ObjectFactory.js
@@ -1,6 +1,12 @@
 class ObjectFactory {
     constructor(scene) {
         this.scene = scene;
+
+        scene.sys.events.once('destroy', this.destroy, this);
+    }
+
+    destroy() {
+        this.scene = null;
     }
 
     static register(type, callback) {
