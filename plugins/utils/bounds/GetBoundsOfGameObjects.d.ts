d52c2fbb8e223f8804f9f50888a24f9cb57700e7
Rex <binhua.tein@gmail.com>
2024-10-01 12:44:47 +0800

    Add type def

---
+++ plugins/utils/bounds/GetBoundsOfGameObjects.d.ts
@@ -0,0 +1,6 @@
+export default GetBoundsOfGameObjects;
+
+declare function GetBoundsOfGameObjects(
+    gameObjects: Phaser.GameObjects.GameObject[],
+    out: Phaser.Geom.Rectangle
+): Phaser.Geom.Rectangle;
