ab432f7c43570752a0f2cc69955d9e336da1bf2b
Rex <binhua.tein@gmail.com>
2024-10-01 09:35:51 +0800

    Remove unused code

--- templates/ui/ObjectFactory.js
+++ templates/ui/ObjectFactory.js
@@ -1,16 +1,12 @@
 class ObjectFactory {
     constructor(scene) {
         this.scene = scene;
-        this.displayList = scene.sys.displayList;
-        this.updateList = scene.sys.updateList;
 
         scene.events.once('destroy', this.destroy, this);
     }
 
     destroy() {
         this.scene = null;
-        this.displayList = null;
-        this.updateList = null;
     }
 
     static register(type, callback) {
