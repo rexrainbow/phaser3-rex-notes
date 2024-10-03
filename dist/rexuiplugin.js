3a05ac5d49b416e58490679b023a59102e85efd4
Rex <binhua.tein@gmail.com>
2024-10-03 20:50:19 +0800

    fix bug

--- dist/rexuiplugin.js
+++ dist/rexuiplugin.js
@@ -30898,7 +30898,7 @@
         Methods$a,
     );
 
-    ObjectFactory.register('lineProgress', function (x, y, width, height, radiusConfig, barColor, value, config) {
+    ObjectFactory.register('roundRectanleProgress', function (x, y, width, height, radiusConfig, barColor, value, config) {
         var gameObject = new RoundRectangleProgress(this.scene, x, y, width, height, radiusConfig, barColor, value, config);
         this.scene.add.existing(gameObject);
         return gameObject;
--- templates/ui/roundrectangleprogress/Factory.js
+++ templates/ui/roundrectangleprogress/Factory.js
@@ -2,7 +2,7 @@ import RoundRectangleProgress from './RoundRectangleProgress.js';
 import ObjectFactory from '../ObjectFactory.js';
 import SetValue from '../../../plugins/utils/object/SetValue.js';
 
-ObjectFactory.register('lineProgress', function (x, y, width, height, radiusConfig, barColor, value, config) {
+ObjectFactory.register('roundRectanleProgress', function (x, y, width, height, radiusConfig, barColor, value, config) {
     var gameObject = new RoundRectangleProgress(this.scene, x, y, width, height, radiusConfig, barColor, value, config);
     this.scene.add.existing(gameObject);
     return gameObject;
