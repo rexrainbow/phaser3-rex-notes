b0a81f9542ea50475ab58e38fd2b751875c628a7
Rex <binhua.tein@gmail.com>
2025-01-20 10:55:27 +0800

    Consider Layer game object
    Layer game object is not extended from GameObject class

--- plugins/utils/system/IsGameObject.js
+++ plugins/utils/system/IsGameObject.js
@@ -1,5 +1,8 @@
 const GameObjectClass = Phaser.GameObjects.GameObject;
+const LayerClass = Phaser.GameObjects.Layer;
+
 var IsGameObject = function (object) {
-    return (object instanceof GameObjectClass);
+    return (object instanceof GameObjectClass) || (object instanceof LayerClass);
 }
+
 export default IsGameObject;
