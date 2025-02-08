a41e1b014d2633d393a5060e6505f03795564ffa
Rex <binhua.tein@gmail.com>
2025-02-09 00:10:01 +0800

    Abandon glow effect

--- plugins/glowfilter2pipeline.d.ts
+++
@@ -1,2 +0,0 @@
-import GlowFilterPostFxPipeline from './shaders/glowfilter2/GlowFilterPostFxPipeline';
-export default GlowFilterPostFxPipeline;
--- plugins/glowfilterpipeline.d.ts
+++
@@ -1,2 +0,0 @@
-import GlowFilterPostFxPipeline from './shaders/glowfilter/GlowFilterPostFxPipeline';
-export default GlowFilterPostFxPipeline;
