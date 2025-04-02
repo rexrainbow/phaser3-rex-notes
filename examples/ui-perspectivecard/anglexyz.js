563620aa4bea97621e20ce5cc7776f5c4b4c9b80
Rex <binhua.tein@gmail.com>
2025-04-02 11:59:34 +0800

    Update test code

--- examples/ui-perspectivecard/anglexyz.js
+++ examples/ui-perspectivecard/anglexyz.js
@@ -1,4 +1,4 @@
-import phaser from 'phaser/src/phaser.js';
+import phaser from '../../../phaser/src/phaser.js';
 import UIPlugin from '../../templates/ui/ui-plugin.js';
 import Dat from '../../plugins/utils/dat.gui/dat.gui.min.js';
 
