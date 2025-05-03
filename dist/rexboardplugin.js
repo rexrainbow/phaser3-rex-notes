a639d81ed34ce849306791ce7fad1f4df7b93c52
Rex <binhua.tein@gmail.com>
2025-05-04 01:32:39 +0800

    Export plugins

--- dist/rexboardplugin.js
+++ dist/rexboardplugin.js
@@ -5098,13 +5098,15 @@
                 yMax = -Infinity;
 
             var UIDToXYZ = this.UIDToXYZ,
-                x;
+                xyz, x, y;
             for (var uid in UIDToXYZ) {
-                x = UIDToXYZ[uid].x;
+                xyz = UIDToXYZ[uid];
+                x = xyz.x;
+                y = xyz.y;
                 if (xMin > x) { xMin = x; }
                 if (xMax < x) { xMax = x; }
-                if (yMin > x) { yMin = x; }
-                if (yMax < x) { yMax = x; }
+                if (yMin > y) { yMin = y; }
+                if (yMax < y) { yMax = y; }
             }
 
             return {
