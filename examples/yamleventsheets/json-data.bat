e755d584b49e7af928d3c5b8a8b424582412d750
Rex <binhua.tein@gmail.com>
2025-08-05 21:35:45 +0800

    Add test case

---
+++ examples/yamleventsheets/json-data.bat
@@ -0,0 +1,5 @@
+@echo off
+set main=./examples/yamleventsheets/json-data.js
+cd ..
+cd ..
+npm run dev
---
+++ examples/yamleventsheets/json-data.js
@@ -0,0 +1,28 @@
+import YAMLEventSheets from '../../plugins/logic/eventsheets/yamleventsheets/YAMLEventSheets.js';
+import content from 'raw-loader!/assets/yamleventsheets/json-data/json-data.yml';
+
+class CommandExecutor {
+    print({ text = '' } = {}, eventSheetManager, eventSheet) {
+        console.log(text);
+    }
+
+    setData(config, eventSheetManager, eventSheet) {
+        for (var name in config) {
+            eventSheetManager.setData(name, config[name]);
+        }
+    }
+}
+var commandExecutor = new CommandExecutor();
+
+var eventSheetManager = new YAMLEventSheets({
+    commandExecutor: commandExecutor
+});
+eventSheetManager.addEventSheet(content);
+
+eventSheetManager.setData('CharA', { hp: 100, mp: 100 })
+console.log('JS: ', eventSheetManager.getData('CharA'))
+console.log('JS: ', eventSheetManager.getData('CharA.hp'))
+
+eventSheetManager.startGroup()
+
+console.log('JS: ', eventSheetManager.memory)
