82c8ff90b9166c8962b5a60434481da1083a1cca
Rex <binhua.tein@gmail.com>
2025-08-07 17:05:42 +0800

    Add test case

---
+++ projects/yamlscenario/main.bat
@@ -0,0 +1,5 @@
+@echo off
+set main=./projects/yamlscenario/main.js
+cd ..
+cd ..
+npm run dev
---
+++ projects/yamlscenario/main.js
@@ -0,0 +1,92 @@
+import phaser from '../../../phaser/src/phaser.js';
+import YAMLScenario from '../../templates/scenario/YAMLScenario.js';
+import TextBoxStyle from './styles/TextBoxStyle.js';
+import ChoiceStyle from './styles/ChoiceStyle.js';
+import TitleStyle from './styles/TitleStyle.js';
+import NameInputStyle from './styles/NameInputStyle.js';
+
+class Demo extends Phaser.Scene {
+
+    constructor() {
+        super({
+            key: 'examples'
+        })
+    }
+
+    preload() {
+        this.load.text('eventSheet0', 'assets/yamleventsheets/scenario/command-executor.yml');
+
+        this.load.image('classroom', 'assets/images/backgrounds/classroom.png');
+        this.load.image('road', 'assets/images/backgrounds/road.png');
+
+        this.load.atlas('characters', 'assets/images/characters/characters.png', 'assets/images/characters/characters.json');
+        this.load.atlas('portraits', 'assets/images/characters/portraits.png', 'assets/images/characters/portraits.json');
+
+        this.load.audio('theme0', [
+            'assets/audio/oedipus_wizball_highscore.ogg',
+            'assets/audio/oedipus_wizball_highscore.mp3'
+        ]);
+        this.load.audio('theme1', [
+            'assets/audio/jungle.ogg',
+            'assets/audio/jungle.mp3'
+        ]);
+        this.load.audio('explosion', [
+            'assets/audio/soundeffect/explosion.mp3'
+        ]);
+    }
+
+    create() {
+        var print = this.add.text(0, 1050, '', { fontSize: 20, backgroundColor: 'grey' }).setDepth(100);
+        print.text = 'Any click to start';
+
+        var rootLayer = this.add.layer().setName('root');
+        var viewport = this.scale.getViewPort();
+
+        var eventSheetManager = new YAMLScenario(this, {
+            styles: {
+                TEXTBOX: TextBoxStyle,
+                CHOICE: ChoiceStyle,
+                TITLE: TitleStyle,
+                NAMEINPUT: NameInputStyle,
+            },
+            rootLayer,
+            multipleCamerasEnable: true,
+            viewport
+        })
+            .addEventSheet(this.cache.text.get('eventSheet0'))
+
+        eventSheetManager
+            .on('pause.input', function () {
+                print.text = 'Wait any click to continue';
+            })
+            .on('resume.input', function () {
+                print.text = '';
+            })
+            .on('complete', function () {
+                print.text = 'Complete';
+                console.log(eventSheetManager.memory)
+            })
+
+        this.input.once('pointerdown', function () {
+            print.text = '';
+            eventSheetManager.start('Story');
+        })
+
+    }
+
+    update() { }
+}
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 1920,
+    height: 1080,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    scene: Demo
+};
+
+var game = new Phaser.Game(config);
