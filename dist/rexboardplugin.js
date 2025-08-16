4b075fc3836f246645352858282738fed5464480
Rex <binhua.tein@gmail.com>
2025-08-17 00:21:06 +0800

    Export plugins

--- dist/rexboardplugin.js
+++ dist/rexboardplugin.js
@@ -16034,33 +16034,42 @@
             cellHeight: tilemap.tileHeight,
         };
 
+        var gridOffsetX, gridOffsetY;
         switch (tilemap.orientation) {
             case 0:    // ORTHOGONAL
                 grid.gridType = 'quadGrid';
                 grid.type = 'orthogonal';
+                gridOffsetX = 0;
+                gridOffsetY = 0;
                 break;
 
             case 1:    // ISOMETRIC
                 grid.gridType = 'quadGrid';
                 grid.type = 'isometric';
+                gridOffsetX = tilemap.tileWidth * 0.5;
+                gridOffsetY = tilemap.tileHeight * 1.5;
                 break;
 
             case 3:    // HEXAGONAL
                 grid.gridType = 'hexagonGrid';
                 grid.staggeraxis = 'y';
                 grid.staggerindex = 'odd';
+                gridOffsetX = 0;
+                gridOffsetY = 0;
                 break;
 
             default:   // ORTHOGONAL
                 grid.gridType = 'quadGrid';
                 grid.type = 'orthogonal';
+                gridOffsetX = 0;
+                gridOffsetY = 0;
                 break;
         }
 
         var layer = tilemap.layers[0];
         if (layer) {
-            grid.x = layer.x;
-            grid.y = layer.y;
+            grid.x = layer.x + gridOffsetX;
+            grid.y = layer.y + gridOffsetY;
         }
 
         return grid;
