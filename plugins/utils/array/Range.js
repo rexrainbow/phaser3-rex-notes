4b2b90e774c7117f117668f0d0466c698106f325
Rex <binhua.tein@gmail.com>
2025-08-27 21:35:33 +0800

    Add Range utility function for array generation
    Introduces a Range function that generates an array of numbers from start to end with a specified step. This utility supports both ascending and descending ranges and is exported as the default module export.

---
+++ plugins/utils/array/Range.js
@@ -0,0 +1,18 @@
+var Range = function (start, end, step = 1) {
+    if (step === undefined) {
+        step = (end >= start) ? 1 : -1;
+    }
+    var arr = [];
+    if (step > 0) {
+        for (var i = start; i < end; i += step) {
+            arr.push(i);
+        }
+    } else if (step < 0) {
+        for (var i = start; i > end; i += step) {
+            arr.push(i);
+        }
+    }
+    return arr;
+}
+
+export default Range;
