0e88a82db31dd2468ffa0e38e7daf86e203908e0
Rex <binhua.tein@gmail.com>
2025-10-02 23:48:33 +0800

    Refactor line drawing methods to use absolute coordinates

--- plugins/gameobjects/shape/line/methods/DrawQuadraticBezierCurve.js
+++ plugins/gameobjects/shape/line/methods/DrawQuadraticBezierCurve.js
@@ -1,21 +1,15 @@
 var DrawQuadraticBezierCurve = function (line) {
     var points = this.points;
-
-    var startPoint = points[0];
-    var startX = startPoint.x;
-    var startY = startPoint.y;
-
     var controlPoint = points[1];
-    var cx = controlPoint.x - startX;
-    var cy = controlPoint.y - startY;
-
     var endPoint = points[2];
-    var endX = endPoint.x - startX;
-    var endY = endPoint.y - startY;
 
     line
         .startAt(0, 0)
-        .quadraticBezierTo(cx, cy, endX, endY)
+        .quadraticBezierTo(
+            controlPoint.x,
+            controlPoint.y,
+            endPoint.x, endPoint.y
+        )
         .end();
 
 }
--- plugins/gameobjects/shape/line/methods/DrawStraightLine.js
+++ plugins/gameobjects/shape/line/methods/DrawStraightLine.js
@@ -1,18 +1,11 @@
 var DrawStraightLine = function (line) {
     var points = this.points;
-    var startPoint = points[0];
-    var startX = startPoint.x;
-    var startY = startPoint.y;
-
-
     var pointsCount = points.length;
     var endPoint = points[pointsCount - 1];
-    var endX = endPoint.x - startX;
-    var endY = endPoint.y - startY;
 
     line
         .startAt(0, 0)
-        .lineTo(endX, endY)
+        .lineTo(endPoint.x, endPoint.y)
         .end();
 
 }
