a13a1fe482068bd9fd8d50f24f77105d5d0c1d84
Rex <binhua.tein@gmail.com>
2025-10-03 10:38:00 +0800

    Refactor Layout.js to destructure layoutConfig
    Destructures layoutConfig for improved readability and updates event emission to include graphBounds and offset in 'layout.postlayout'. This change streamlines function calls and event handling within the layout process.

--- plugins/graph/layout/utils/Layout.js
+++ plugins/graph/layout/utils/Layout.js
@@ -1,23 +1,30 @@
 var Layout = async function (layoutConfig, graph, userConfig) {
+    var {
+        buildGraphData,
+        isAsyncRunLayout, runLayout,
+        placeGameObjects,
+    } = layoutConfig;
+
     if (userConfig === undefined) {
         userConfig = {};
     }
 
     graph.emit('layout.start', graph);
 
-    var graphData = layoutConfig.buildGraphData(graph, userConfig);
+    var graphData = buildGraphData(graph, userConfig);
 
     graph.emit('layout.prelayout', graph);
 
-    if (layoutConfig.isAsyncRunLayout) {
-        await layoutConfig.runLayout(graphData, userConfig);
+    if (isAsyncRunLayout) {
+        await runLayout(graphData, userConfig);
     } else {
-        layoutConfig.runLayout(graphData, userConfig);
+        runLayout(graphData, userConfig);
     }
 
-    graph.emit('layout.postlayout', graph);
+    var offset = { x: 0, y: 0 };
+    graph.emit('layout.postlayout', graph, graphBounds, offset);
 
-    layoutConfig.placeGameObjects(graph, graphData, userConfig);
+    placeGameObjects(graph, graphData, userConfig);
 
     graph.emit('layout.complete', graph);
 }
