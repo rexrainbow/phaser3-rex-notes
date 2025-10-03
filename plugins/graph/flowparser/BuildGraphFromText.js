6e27460af98b8d8afa96cde08ecd6c6682bf1a18
Rex <binhua.tein@gmail.com>
2025-10-03 08:50:07 +0800

    Refactor graph building logic into separate module
    Moved the logic for building a graph from text into a new BuildGraphFromText.js module. Updated BuildFromTextMethods.js to use this new module, improving code organization and reusability.

---
+++ plugins/graph/flowparser/BuildGraphFromText.js
@@ -0,0 +1,57 @@
+import { Parser } from './parser.js';
+
+var BuildGraphFromText = function (config) {
+    var {
+        graph,
+        onCreateNodeGameObject, onCreateEdgeGameObject,
+        layer,
+        context
+    } = config;
+
+    var { nodes, edges } = new Parser().parse(context);
+
+    var scene = graph.scene;
+
+    var nodeGameObject;
+    for (var i = 0, cnt = nodes.length; i < cnt; i++) {
+        var nodeData = nodes[i];
+        var id = nodeData.id;
+        var parameters = nodeData.parameters;
+
+        if (onCreateNodeGameObject) {
+            nodeGameObject = onCreateNodeGameObject(scene, id, parameters);
+        } else {
+            nodeGameObject = { width: 0, height: 0 };
+        }
+
+        graph.addNode(nodeGameObject, parameters, id);
+
+        if (layer) {
+            layer.add(nodeGameObject);
+        }
+    }
+
+    var edgeGameObject;
+    for (var i = 0, cnt = edges.length; i < cnt; i++) {
+        var edgeData = edges[i];
+        var id = edgeData.id;
+        var parameters = edgeData.parameters;
+        var sourceId = edgeData.sourceId;
+        var targetId = edgeData.targetId;
+
+        if (onCreateEdgeGameObject) {
+            edgeGameObject = onCreateEdgeGameObject(scene, id, parameters);
+        } else {
+            edgeGameObject = {};
+        }
+
+        graph.addEdge(edgeGameObject, sourceId, targetId, undefined, parameters, id);
+
+        if (layer) {
+            layer.add(edgeGameObject);
+        }
+    }
+
+}
+
+export default BuildGraphFromText;
--- plugins/graph/graph/methods/BuildFromTextMethods.js
+++ plugins/graph/graph/methods/BuildFromTextMethods.js
@@ -1,4 +1,4 @@
-import { Parser } from '../../flowparser/parser.js';
+import BuildGraphFromText from '../../flowparser/BuildGraphFromText.js';
 
 export default {
     setOnCreateNodeGameObjectCallback(callback, scope) {
@@ -19,42 +19,13 @@ export default {
     buildFromText(context) {
         this.clear();
 
-        var { nodes, edges } = new Parser().parse(context);
-
-        var scene = this.scene;
-        var onCreateNodeGameObject = this.onCreateNodeGameObject;
-        var onCreateEdgeGameObject = this.onCreateEdgeGameObject;
-        var layer = this.gameObjectLayer;
-
-        var nodeGameObject;
-        for (var i = 0, cnt = nodes.length; i < cnt; i++) {
-            var nodeData = nodes[i];
-            var id = nodeData.id;
-            var parameters = nodeData.parameters;
-            nodeGameObject = onCreateNodeGameObject(scene, id, parameters);
-
-            this.addNode(nodeGameObject, parameters, id);
-
-            if (layer) {
-                layer.add(nodeGameObject);
-            }
-        }
-
-        var edgeGameObject;
-        for (var i = 0, cnt = edges.length; i < cnt; i++) {
-            var edgeData = edges[i];
-            var id = edgeData.id;
-            var parameters = edgeData.parameters;
-            var sourceId = edgeData.sourceId;
-            var targetId = edgeData.targetId;
-            edgeGameObject = onCreateEdgeGameObject(scene, id, parameters);
-
-            this.addEdge(edgeGameObject, sourceId, targetId, undefined, parameters, id);
-
-            if (layer) {
-                layer.add(edgeGameObject);
-            }
-        }
+        BuildGraphFromText({
+            graph: this,
+            onCreateNodeGameObject: this.onCreateNodeGameObject,
+            onCreateEdgeGameObject: this.onCreateEdgeGameObject,
+            layer: this.gameObjectLayer,
+            context: context,
+        })
 
         return this;
     }
