044da09c4c696400f28d1b25f70442126d00828c
Rex <binhua.tein@gmail.com>
2025-10-07 09:31:35 +0800

    Add line object to scene on creation
    Updated the line factory to automatically add the created Line object to the scene using scene.add.existing. This ensures that new lines are properly managed and rendered by the scene.

--- plugins/graph/line/Factory.js
+++ plugins/graph/line/Factory.js
@@ -3,7 +3,9 @@ import ObjectFactory from '../ObjectFactory.js';
 import SetValue from '../../utils/object/SetValue.js';
 
 ObjectFactory.register('line', function (points, lineWidth, color, alpha, lineType) {
-    return new Line(this.scene, points, lineWidth, color, alpha, lineType);
+    var gameObject = new Line(this.scene, points, lineWidth, color, alpha, lineType);
+    this.scene.add.existing(gameObject);
+    return gameObject;
 });
 
 SetValue(window, 'RexPlugins.Graph.Line', Line);
