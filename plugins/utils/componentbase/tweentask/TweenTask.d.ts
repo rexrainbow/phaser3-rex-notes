46e9445281252008bf53407f50b18250d70f2fe9
Rex <binhua.tein@gmail.com>
2026-01-28 15:04:20 +0800

    Add comments

--- plugins/utils/componentbase/tweentask/TweenTask.d.ts
+++ plugins/utils/componentbase/tweentask/TweenTask.d.ts
@@ -2,13 +2,55 @@ import ComponentBase from '../ComponentBase';
 
 export default TweenTask;
 
+/**
+ * Component that controls a Phaser tween lifecycle.
+ */
 declare class TweenTask extends ComponentBase {
-    start(config: Object): this;
+    /**
+     * Start the tween task with a tween configuration.
+     *
+     * @param config - Tween builder configuration.
+     * @returns This TweenTask instance.
+     */
+    start(
+        config: Phaser.Types.Tweens.TweenBuilderConfig | object
+    ): this;
+    /**
+     * Stop the tween task.
+     *
+     * @returns This TweenTask instance.
+     */
     stop(): this;
-    restart(config: Object): this;
+    /**
+     * Restart the tween task with a tween configuration.
+     *
+     * @param config - Tween builder configuration.
+     * @returns This TweenTask instance.
+     */
+    restart(
+        config: Phaser.Types.Tweens.TweenBuilderConfig | object
+    ): this;
 
+    /**
+     * Pause the tween task.
+     *
+     * @returns This TweenTask instance.
+     */
     pause(): this;
+    /**
+     * Resume the tween task.
+     *
+     * @returns This TweenTask instance.
+     */
     resume(): this;
+    /**
+     * Complete the tween task immediately.
+     *
+     * @returns This TweenTask instance.
+     */
     complete(): this;
+    /**
+     * Whether the tween task is currently running.
+     */
     readonly isRunning: boolean;
 }
