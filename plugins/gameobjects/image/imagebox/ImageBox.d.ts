e31169d4e02f2baf2bbc4e248832c511676e46f0
Rex <binhua.tein@gmail.com>
2026-02-02 10:11:51 +0800

    Add comments

--- plugins/gameobjects/image/imagebox/ImageBox.d.ts
+++ plugins/gameobjects/image/imagebox/ImageBox.d.ts
@@ -4,35 +4,98 @@ export default ImageBox;
 
 declare namespace ImageBox {
 
+    /**
+     * Configuration for creating an image box.
+     */
     interface IConfig {
-        x?: number, y?: number,
-        key?: string, frame?: string,
+        /**
+         * World x position.
+         */
+        x?: number,
+        /**
+         * World y position.
+         */
+        y?: number,
+        /**
+         * Texture key.
+         */
+        key?: string,
+        /**
+         * Texture frame key.
+         */
+        frame?: string,
 
+        /**
+         * Allow scaling image above its original size.
+         */
         scaleUp?: boolean,
-        width?: number, height?: number,
+        /**
+         * Display width.
+         */
+        width?: number,
+        /**
+         * Display height.
+         */
+        height?: number,
 
+        /**
+         * Background definition or background game object.
+         */
         background?: ImageBoxBase.IRectangle | Phaser.GameObjects.GameObject,
+        /**
+         * Image game object to use instead of auto-created one.
+         */
         image?: Phaser.GameObjects.GameObject,
 
     }
 }
 
+/**
+ * Image container with optional background and fit-to-size behavior.
+ */
 declare class ImageBox extends ImageBoxBase {
+    /**
+     * Create an image box from configuration.
+     *
+     * @param scene - Scene that owns this game object.
+     * @param config - Optional image box configuration.
+     */
     constructor(
         scene: Phaser.Scene,
         config?: ImageBox.IConfig
     );
 
+    /**
+     * Create an image box.
+     *
+     * @param scene - Scene that owns this game object.
+     * @param x - World x position.
+     * @param y - World y position.
+     * @param texture - Texture key.
+     * @param frame - Texture frame key.
+     * @param config - Optional image box configuration.
+     */
     constructor(
         scene: Phaser.Scene,
-        x?: number, y?: number,
-        texture?: string, frame?: string,
+        x?: number,
+        y?: number,
+        texture?: string,
+        frame?: string,
         config?: ImageBox.IConfig
     );
 
+    /**
+     * Create an image box using position and configuration.
+     *
+     * @param scene - Scene that owns this game object.
+     * @param x - World x position.
+     * @param y - World y position.
+     * @param config - Optional image box configuration.
+     */
     constructor(
         scene: Phaser.Scene,
-        x?: number, y?: number,
+        x?: number,
+        y?: number,
         config?: ImageBox.IConfig
     );
 }
--- plugins/gameobjects/image/imagebox/ImageBoxBase.d.ts
+++ plugins/gameobjects/image/imagebox/ImageBoxBase.d.ts
@@ -3,28 +3,126 @@ import ContainerLite from '../../container/containerlite/ContainerLite';
 export default ImageBoxBase;
 
 declare namespace ImageBoxBase {
+    /**
+     * Rectangle style configuration used for auto-created backgrounds.
+     */
     interface IRectangle {
-        color?: number, alpha?: number,
-        strokeWidth?: number, strokeColor?: number, strokeAlpha?: number,
+        /**
+         * Fill color as a hex number.
+         */
+        color?: number,
+        /**
+         * Fill alpha.
+         */
+        alpha?: number,
+        /**
+         * Stroke width.
+         */
+        strokeWidth?: number,
+        /**
+         * Stroke color as a hex number.
+         */
+        strokeColor?: number,
+        /**
+         * Stroke alpha.
+         */
+        strokeAlpha?: number,
     }
 }
 
+/**
+ * Base container for image box game objects.
+ */
 declare class ImageBoxBase extends ContainerLite {
 
+    /**
+     * Optional background game object.
+     */
     background: Phaser.GameObjects.GameObject;
+    /**
+     * Main image game object.
+     */
     image: Phaser.GameObjects.GameObject;
 
-    setTexture(key?: string, frame?: string): this;
+    /**
+     * Set image texture and frame.
+     *
+     * @param key - Texture key.
+     * @param frame - Texture frame key.
+     * @returns This image box instance.
+     */
+    setTexture(
+        key?: string,
+        frame?: string
+    ): this;
+    /**
+     * Current texture of the internal image.
+     */
     readonly texture: Phaser.Textures.Texture | Phaser.Textures.CanvasTexture;
+    /**
+     * Current texture frame of the internal image.
+     */
     readonly frame: Phaser.Textures.Frame;
 
+    /**
+     * Set horizontal flip state.
+     *
+     * @param value - True to flip horizontally.
+     * @returns This image box instance.
+     */
     setFlipX(value: boolean): this;
+    /**
+     * Set vertical flip state.
+     *
+     * @param value - True to flip vertically.
+     * @returns This image box instance.
+     */
     setFlipY(value: boolean): this;
+    /**
+     * Toggle horizontal flip state.
+     *
+     * @returns This image box instance.
+     */
     toggleFlipX(): this;
+    /**
+     * Toggle vertical flip state.
+     *
+     * @returns This image box instance.
+     */
     toggleFlipY(): this;
-    setFlip(x: boolean, y: boolean): this;
+    /**
+     * Set horizontal and vertical flip states.
+     *
+     * @param x - Horizontal flip state.
+     * @param y - Vertical flip state.
+     * @returns This image box instance.
+     */
+    setFlip(
+        x: boolean,
+        y: boolean
+    ): this;
+    /**
+     * Whether horizontal flip is enabled.
+     */
     flipX: boolean;
+    /**
+     * Whether vertical flip is enabled.
+     */
     flipY: boolean;
 
-    setTint(topLeft?: number, topRight?: number, bottomLeft?: number, bottomRight?: number): this;
+    /**
+     * Set tint colors of the internal image corners.
+     *
+     * @param topLeft - Top-left tint color.
+     * @param topRight - Top-right tint color.
+     * @param bottomLeft - Bottom-left tint color.
+     * @param bottomRight - Bottom-right tint color.
+     * @returns This image box instance.
+     */
+    setTint(
+        topLeft?: number,
+        topRight?: number,
+        bottomLeft?: number,
+        bottomRight?: number
+    ): this;
 }
