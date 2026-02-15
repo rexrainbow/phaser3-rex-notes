dc0780602537f9d6b63b703f6739d7eb675cf274
Rex <binhua.tein@gmail.com>
2026-02-15 23:50:10 +0800

    Add addHeader, addFooter methods

--- templates/ui/utils/scrollable/methods/Methods.js
+++ templates/ui/utils/scrollable/methods/Methods.js
@@ -1,8 +1,12 @@
+import AddHeader from './AddHeader.js';
+import AddFooter from './AddFooter.js';
 import ResizeController from './ResizeController.js';
 import UpdateController from './UpdateController.js';
 import ChildPositionMethods from './ChildPositionMethods.js';
 
 var Methods = {
+    addHeader: AddHeader,
+    addFooter: AddFooter,
     resizeController: ResizeController,
     updateController: UpdateController
 }
