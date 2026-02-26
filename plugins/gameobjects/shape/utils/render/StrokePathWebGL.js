2525b8b9c791d3d8d53197c4b99a5bc3ebdd5f59
Rex <binhua.tein@gmail.com>
2026-02-27 01:01:19 +0800

    Fix bug

--- plugins/gameobjects/shape/utils/render/StrokePathWebGL.js
+++ plugins/gameobjects/shape/utils/render/StrokePathWebGL.js
@@ -12,76 +12,103 @@ src: {
 */
 var Utils = Phaser.Renderer.WebGL.Utils;
 
-var StrokePathWebGL = function (pipeline, src, alpha, dx, dy) {
-    var strokeTint = pipeline.strokeTint;
-    var strokeTintColor = Utils.getTintAppendFloatAlpha(src.strokeColor, src.strokeAlpha * alpha);
+var StrokePathWebGL = function (drawingContext, submitter, calcMatrix, gameObject, shapeData, alpha, dx, dy) {
+    var strokeTintColor = Utils.getTintAppendFloatAlpha(shapeData.strokeColor, shapeData.strokeAlpha * alpha);
+    var strokePath = gameObject.customRenderNodes.StrokePath || gameObject.defaultRenderNodes.StrokePath;
 
-    strokeTint.TL = strokeTintColor;
-    strokeTint.TR = strokeTintColor;
-    strokeTint.BL = strokeTintColor;
-    strokeTint.BR = strokeTintColor;
+    var lineWidth = shapeData.lineWidth;
+    var openPath = !shapeData.closePath;
+    var isDashed = shapeData.isDashed && !!shapeData.strokePathData && !!shapeData.strokePathMask;
 
-    var isDashed = src.isDashed && !!src.strokePathData;
-    var path = (!isDashed) ? src.pathData : src.strokePathData;
-    var pathLength = path.length - 1;
-    var lineWidth = src.lineWidth;
-    var halfLineWidth = lineWidth / 2;
+    // Helper method
+    var RunStrokePath = function (pointPath, pathIsOpen) {
+        if (pointPath.length < 2) {
+            return;
+        }
 
-    var px1 = path[0] - dx;
-    var py1 = path[1] - dy;
-    var px2, py2;
-
-    if (!src.closePath) {
-        pathLength -= 2;
-    }
+        strokePath.run(
+            drawingContext,
+            submitter,
+            pointPath,
+            lineWidth,
+            pathIsOpen,
+            calcMatrix,
+            strokeTintColor,
+            strokeTintColor,
+            strokeTintColor,
+            strokeTintColor
+        );
+    };
 
     if (!isDashed) {
         // Default behavior
-        for (var i = 2; i < pathLength; i += 2) {
-            px2 = path[i] - dx;
-            py2 = path[i + 1] - dy;
+        var path = shapeData.pathData;
+        if (!path || path.length < 4) {
+            return;
+        }
+        var pathLength = path.length - 1;
 
-            pipeline.batchLine(
-                px1,
-                py1,
-                px2,
-                py2,
-                halfLineWidth,
-                halfLineWidth,
-                lineWidth,
-                i - 2,
-                (src.closePath) ? (i === pathLength - 1) : false
-            );
-
-            px1 = px2;
-            py1 = py2;
+        // Don't add the last point to open paths.
+        if (openPath) {
+            pathLength -= 2;
         }
 
+        var pointPath = [];
+        for (var i = 0; i < pathLength; i += 2) {
+            pointPath.push({
+                x: path[i] - dx,
+                y: path[i + 1] - dy,
+                width: lineWidth
+            });
+        }
+
+        RunStrokePath(pointPath, openPath);
+
     } else {
-        // Draw dashed line
-        var strokePathMask = src.strokePathMask;
+        // Dashed path data is a sequence of segment endpoints with a per-segment draw mask.
+        var dashedPath = shapeData.strokePathData;
+        if (!dashedPath || dashedPath.length < 4) {
+            return;
+        }
+        var strokePathMask = shapeData.strokePathMask;
+        var dashedPathLength = dashedPath.length - 1;
+
+        if (openPath) {
+            dashedPathLength -= 2;
+        }
+
+        var px1 = dashedPath[0] - dx;
+        var py1 = dashedPath[1] - dy;
+
         var drawMaskIdx = 0;
-        var segLineIdx = 0;
-        for (var i = 2; i < pathLength; i += 2) {
-            px2 = path[i] - dx;
-            py2 = path[i + 1] - dy;
+        var pointPath = [];
 
+        for (var j = 2; j < dashedPathLength; j += 2) {
+            var px2 = dashedPath[j] - dx;
+            var py2 = dashedPath[j + 1] - dy;
+
+            // Build continuous line segments (pointPath)
             if (strokePathMask[drawMaskIdx]) {
-                pipeline.batchLine(
-                    px1,
-                    py1,
-                    px2,
-                    py2,
-                    halfLineWidth,
-                    halfLineWidth,
-                    lineWidth,
-                    segLineIdx,
-                    false
-                );
-                segLineIdx++;
+                if (
+                    pointPath.length === 0 ||
+                    pointPath[pointPath.length - 1].x !== px1 ||
+                    pointPath[pointPath.length - 1].y !== py1
+                ) {
+                    pointPath.push({
+                        x: px1,
+                        y: py1,
+                        width: lineWidth
+                    });
+                }
 
+                pointPath.push({
+                    x: px2,
+                    y: py2,
+                    width: lineWidth
+                });
             } else {
-                segLineIdx = 0;
+                RunStrokePath(pointPath, true);
+                pointPath = [];
 
             }
 
@@ -89,6 +116,8 @@ var StrokePathWebGL = function (pipeline, src, alpha, dx, dy) {
             py1 = py2;
             drawMaskIdx++;
         }
+
+        RunStrokePath(pointPath, true);
     }
 
 };
