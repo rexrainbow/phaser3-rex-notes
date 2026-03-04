3f6497a2c955fbccf425cef0f5a0b6c122f5399a
Rex <binhua.tein@gmail.com>
2026-03-04 11:37:42 +0800

    Return null if camera is undefined

--- templates/ui/gridtable/input/PointerToCellIndex.js
+++ templates/ui/gridtable/input/PointerToCellIndex.js
@@ -1,11 +1,16 @@
 // Return null if (x, y) is out of any cell, or that cell is invisible
 var PointerToCellIndex = function (table, pointer, worldX, worldY) {
+    var camera = pointer.camera;
+    if (!camera) {
+        // Why camera is undefined here?
+        return null;
+    }
+
     if (worldX === undefined) {
         worldX = pointer.worldX;
         worldY = pointer.worldY;
     }
 
-    var camera = pointer.camera;
     var x = worldX + camera.scrollX * (table.scrollFactorX - 1);
     var y = worldY + camera.scrollY * (table.scrollFactorY - 1);
     var cellIndex = table.pointToCellIndex(x, y);
