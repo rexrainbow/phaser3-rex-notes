cfa69a1bad0ca00c8274de0f7bc332d541887324
Rex <binhua.tein@gmail.com>
2026-04-14 14:57:10 +0800

    Add d.ts

---
+++ plugins/utils/mask/MaskMethods.d.ts
@@ -0,0 +1,33 @@
+/**
+ * Apply a mask source game object to a target game object.
+ *
+ * The target receives either a WebGL `MaskController` or a Canvas
+ * `GeometryMask`, depending on the current render mode.
+ *
+ * @param {Phaser.GameObjects.GameObject} gameObject - The target game object that will receive the mask.
+ * @param {Phaser.GameObjects.GameObject} maskGameObject - The game object that provides the mask.
+ * @param {boolean} [invert] - Set to `true` to invert the mask in WebGL mode.
+ * @returns {void}
+ */
+declare function SetMask(
+    gameObject: Phaser.GameObjects.GameObject,
+    invert?: boolean
+): Phaser.GameObjects.GameObject;
+
+/**
+ * Remove the current mask from a game object.
+ *
+ * This only detaches the mask from the target game object. The shared mask
+ * object owned by the mask source is kept alive until the source is destroyed.
+ *
+ * @param {Phaser.GameObjects.GameObject} gameObject - The masked game object.
+ * @returns {void}
+ */
+declare function ClearMask(
+    gameObject: Phaser.GameObjects.GameObject,
+): void;
+
+export {
+    SetMask,
+    ClearMask,
+}
