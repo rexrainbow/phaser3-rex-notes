abec22c45393fb0ca1966bc719d979f84c4f263e
Rex <binhua.tein@gmail.com>
2026-05-03 16:56:55 +0800

    Add test case

---
+++ examples/effect-properties/bloom.js
@@ -0,0 +1,47 @@
+import Phaser from 'phaser';
+import EffectPropertiesPlugin from '../../plugins/effectproperties-plugin.js';
+
+class Demo extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'examples'
+        })
+    }
+
+    preload() {
+        this.load.image('logo', 'assets/images/logo.png');
+    }
+
+    create() {
+        var image = this.add.image(400, 300, 'logo');
+        this.plugins.get('rexEffectProperties').add(image);
+
+        image.bloomOffsetX = 10;
+        image.bloomOffsetY = 10;
+        image.bloomColor = 0xff0000;
+
+    }
+
+    update() { }
+}
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    scene: Demo,
+    plugins: {
+        global: [{
+            key: 'rexEffectProperties',
+            plugin: EffectPropertiesPlugin,
+            start: true
+        }]
+    }
+};
+
+var game = new Phaser.Game(config);
