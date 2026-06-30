e0b21ab8c45f419710d2e96f74cbd5cfdafec27e
Rex <binhua.tein@gmail.com>
2026-06-30 09:59:48 +0800

    Fix bug

--- plugins/logic/eventsheets/eventsheetmanager/eventsheetgroup/StopMethods.js
+++ plugins/logic/eventsheets/eventsheetmanager/eventsheetgroup/StopMethods.js
@@ -9,20 +9,29 @@ export default {
         var eventSheetManager = this.parent;
         var blackboard = eventSheetManager.blackboard;
         var commandExecutor = eventSheetManager.commandExecutor;
+        var trees = this.pendingTrees.slice();
+
+        this.pendingTrees.length = 0;
+        this.closedTrees.length = 0;
+        this.isRunning = false;
 
         eventSheetManager.emit(EVT_GROUP_STOP, this.name, eventSheetManager, this);
 
-        var trees = this.pendingTrees;
-        for (var i = 0, cnt = trees.length; i < cnt; i++) {
-            var eventsheet = trees[i];
-            eventSheetManager.emit(EVT_EVENTSHEET_ABORT, eventsheet.title, this.name, eventSheetManager, eventsheet, this);
-            eventsheet.abort(blackboard, commandExecutor);
-            CloseEventSheet.call(this, eventSheetManager, eventsheet);
-        }
-        trees.length = 0;
+        try {
+            for (var i = 0, cnt = trees.length; i < cnt; i++) {
+                var eventsheet = trees[i];
+                if (eventsheet.roundComplete) {
+                    continue;
+                }
 
-        this.isRunning = false;
-        this.clearRunContext();
+                eventsheet.abort(blackboard, commandExecutor);
+                eventSheetManager.emit(EVT_EVENTSHEET_ABORT, eventsheet.title, this.name, eventSheetManager, eventsheet, this);
+                CloseEventSheet.call(this, eventSheetManager, eventsheet);
+            }
+        } finally {
+            this.clearRunContext();
+            this.removePendingEventSheets();
+        }
 
         return this;
     },
